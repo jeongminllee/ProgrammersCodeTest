02df38e5a758722cc76eb0c4885cac63e7e09482
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-17 20:46:50 +0900

    Update 공배수.py

--- 프로그래머스/unrated/181936. 공배수/공배수.py
+++ 프로그래머스/unrated/181936. 공배수/공배수.py
@@ -1,2 +1,16 @@
 def solution(number, n, m):
     return int(not(number % n or number % m))
+
+# def solution(num, n, m) :
+#   return int(not(num % n or num % m))
+
+# def solution(number, n, m):
+#     return int(bool(number % n == 0) & bool(number % m == 0))
+
+# def solution(number, n, m):
+#     answer = 0
+#     if number % n == 0 and number % m == 0 :
+#         answer = 1
+#     else :
+#         answer = 0
+#     return answer
