74222143f4f6f02364e8526020deca7195966bfe
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-17 20:20:22 +0900

    Update 더 크게 합치기.py

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.py
@@ -2,3 +2,6 @@ def solution(a, b):
     answer1 = str(a) + str(b)
     answer2 = str(b) + str(a)
     return int(answer1) if int(answer1) > int(answer2) else int(answer2)
+
+# def solution(a, b):
+#     return int(max(f"{a}{b}", f"{b}{a}"))
