27efcc21bd1f52be54b7589ebc2e5479d437692b
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-20 14:03:07 +0000

    [unrated] Title: 카운트 업, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.py
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.py
@@ -1,6 +1,6 @@
 def solution(start_num, end_num):
     answer = []
-    for i in range(start_num - 1, end_num) :
-        i += 1
+    for i in range(start_num, end_num + 1) :
         answer.append(i)
+        i += 1
     return answer
