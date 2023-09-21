51a528cd68952217cc739bf0ccdb577fd3268492
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-21 00:40:22 +0000

    [unrated] Title: 간단한 논리 연산, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.py
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -1,19 +1,7 @@
 def solution(x1, x2, x3, x4):
     answer = True
-    a = x1 + x2
-    b = x3 + x4
-    answer = a + b
-    if x1 == True or x2 == True :
-        a = True
-    else :
-        a = False
-    if x3 == True or x4 == True :
-        b = True
-    else :
-        b = False
-        
-    if a == True and b == True :
+    if (x1 == True or x2 == True) and (x3 == True or x4 == True) : 
         answer = True
-    else :
+    else : 
         answer = False
     return answer
