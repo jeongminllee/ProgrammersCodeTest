30c4bc28038617dde9318343b6f6d3df519e47bc
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-22 02:33:45 +0000

    [unrated] Title: 배열 만들기 1, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,11 +1,7 @@
 def solution(n, k):
     answer = []
-    a = 0
-    b = 0
-    while b < n :
-        a += 1
-        b = k * a
-        if b > n :
-            break
-        answer.append(b)
+    idx = 1
+    while k * idx <= n :
+        answer.append(k*idx)
+        idx += 1
     return answer
