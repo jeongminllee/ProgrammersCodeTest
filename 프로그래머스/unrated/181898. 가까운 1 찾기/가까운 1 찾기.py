ad8f0986a145cd5c40512ebdb09e88f5cb2fe42d
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-22 15:21:44 +0900

    Update 가까운 1 찾기.py

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -7,11 +7,11 @@ def solution(arr, idx):
 
     return answer
 
-def solution(arr, idx):
-    answer = -1
-    for i in range(idx-1, len(arr)) :
-        if arr[i] == 1 :
-            if i >= idx :
-                answer = i
-                break
-    return answer
+# def solution(arr, idx):
+#     answer = -1
+#     for i in range(idx-1, len(arr)) :
+#         if arr[i] == 1 :
+#             if i >= idx :
+#                 answer = i
+#                 break
+#     return answer
