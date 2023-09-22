d70da236dea4e67c3d1c4c50de671d4edee3a889
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-22 06:14:25 +0000

    [unrated] Title: 가까운 1 찾기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,17 +1,17 @@
 def solution(arr, idx):
-    answer = -1
-    for i in range(idx-1, len(arr)) :
-        if arr[i] == 1 :
-            if i >= idx :
-                answer = i
-                break
-    return answer
-
-def solution(arr, idx):
     answer = 0
     try:
         answer = arr.index(1, idx)
     except:
         answer = -1
 
+    return answer
+
+def solution(arr, idx):
+    answer = -1
+    for i in range(idx-1, len(arr)) :
+        if arr[i] == 1 :
+            if i >= idx :
+                answer = i
+                break
     return answer
