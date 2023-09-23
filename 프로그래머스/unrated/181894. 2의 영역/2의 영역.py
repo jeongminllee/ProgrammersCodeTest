61d39c0a2c5cd5a3376afd5dc513fb71532274ff
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-23 02:06:55 +0000

    [unrated] Title: 2의 영역, Time: 1.54 ms, Memory: 15.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
@@ -1,12 +1,12 @@
 def solution(arr):
     if 2 in arr :
-        if arr.count(2) > 1 :
+        if arr.count(2) >= 1 :
             start = arr.index(2)
             end = len(arr) - arr[::-1].index(2)
             return arr[start : end]
         
-        else :
-            idx = arr.index(2)
-            return [arr[idx]]
+        # else :
+        #     idx = arr.index(2)
+        #     return [arr[idx]]
     else : 
         return [-1]
