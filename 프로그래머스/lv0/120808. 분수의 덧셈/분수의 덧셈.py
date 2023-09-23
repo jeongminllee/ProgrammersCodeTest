38094fc3e0fdd9e36f5a91f1bf838fcc5c8bbff3
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-23 15:43:04 +0000

    [level 0] Title: 분수의 덧셈, Time: 0.29 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -7,3 +7,23 @@ def solution(numer1, denom1, numer2, denom2):
     gcd_value = math.gcd(denom, numer)
     
     return [int(numer / gcd_value), int(denom / gcd_value)]
+
+
+def solution(denum1, num1, denum2, num2):
+    answer = []
+    s = 0
+
+    denum0 = (denum1*num2) +(denum2*num1)
+    num0 = num1*num2
+
+    for i in range(min(denum0,num0),0,-1):
+        if denum0%i == 0 and num0%i == 0:
+            s = i
+            break
+
+    denum0 /= s
+    num0 /= s
+    answer.append(denum0)
+    answer.append(num0)
+
+    return answer
