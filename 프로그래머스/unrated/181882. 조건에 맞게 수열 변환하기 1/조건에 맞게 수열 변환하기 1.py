7d62178fbe0c0b2a6e096b80396b0e2fd8cf5e54
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-24 02:02:51 +0000

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 13.47 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -1,2 +1,14 @@
 def solution(arr):
     return [i*2 if i < 50 and i % 2 == 1 else(i/2 if i >= 50 and i % 2 == 0 else i) for i in arr]
+
+def solution(arr) :
+    for i, val in enumerate(arr) :
+        if val < 50 and val % 2 == 1 :
+            val *= 2
+            arr[i] = val
+        elif val >= 50 and val % 2 == 0 :
+            val /= 2
+            arr[i] = val
+        else :
+            arr[i] = val
+    return arr
