99b4ce695a82be53b54b723eef4b4f01f66d2ce4
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-25 05:22:22 +0000

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,10 @@
+def solution(price):
+    answer = 0
+    if price >= 100000 and price < 300000:
+        return int(price * 0.95)
+    elif price >= 300000 and price < 500000:
+        return int(price * 0.90)
+    elif price >= 500000 :
+        return int(price * 0.80)
+    else :
+        return price
