d2b244044b5f7c9e0186c4e83845fa373891e785
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-26 19:49:07 +0900

    Update 공 바꾸기.py

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -10,4 +10,11 @@ for i in range(M) :
     
 for i in range(N) :
     print(box[i], end = ' ')
-    
+
+
+# n, m = map(int, input().split())
+# s = [i for i in range(1, n+1)]
+# for _ in range(m):
+#     a, b = map(int, input().split())
+#     s[a-1], s[b-1] = s[b-1], s[a-1]
+# print(*s)
