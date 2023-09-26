a37be67f2cb1cadc35d019060949c7c6f63a691e
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-26 17:43:06 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,11 +1,18 @@
-def solution(num_list):
-    answer = []
-    odds = []
-    evens = []
-    for i in range(len(num_list)) :
-        if num_list[i] % 2 == 0 :
-            evens.append(num_list[i])
-        else :
-            odds.append(num_list[i])
+# def solution(num_list):
+#     answer = []
+#     odds = []
+#     evens = []
+#     for i in range(len(num_list)) :
+#         if num_list[i] % 2 == 0 :
+#             evens.append(num_list[i])
+#         else :
+#             odds.append(num_list[i])
         
-    return [len(evens), len(odds)]
+#     return [len(evens), len(odds)]
+
+
+def solution(num_list):
+    answer = [0, 0]
+    for n in num_list :
+        answer[n % 2] += 1
+    return answer
