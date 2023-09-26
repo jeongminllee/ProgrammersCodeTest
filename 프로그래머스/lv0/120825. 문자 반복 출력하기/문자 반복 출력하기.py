e04bc636e0a30093c3ccd947c0a21174ee427922
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-26 18:02:25 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -1,5 +1,9 @@
-def solution(my_string, n):
-    answer = ''
-    for i in my_string :
-        answer += n * i
-    return answer
+# def solution(my_string, n):
+#     answer = ''
+#     for i in my_string :
+#         answer += n * i
+#     return answer
+
+
+def solution(my_string, n) :
+    return ''.join(n * i for i in my_string)
