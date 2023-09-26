222d2f656bf85381f0579aafa51c0da727225d40
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-26 11:32:24 +0900

    [unrated] Title: rny_string, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.py
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.py
@@ -1,3 +1,7 @@
 def solution(rny_string):
     answer = ''.join(["rn" if i == "m" else i for i in rny_string])
     return answer
+
+
+def solution(rny_string) :
+    return rny_string.replace('m', 'rn')
