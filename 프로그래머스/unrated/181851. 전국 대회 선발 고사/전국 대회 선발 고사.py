6fe4476c9253321805635fc2e3d274bf48b100ab
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-27 18:28:33 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,8 +1,12 @@
-def solution(rank, attendance):
-    temp = []
-    for i, ranker in enumerate(rank) :
-        if attendance[i] == 1 :
-            temp.append([rank[i], i])
+# def solution(rank, attendance):
+#     temp = []
+#     for i, ranker in enumerate(rank) :
+#         if attendance[i] == 1 :
+#             temp.append([rank[i], i])
             
-    temp.sort(key = lambda v : v[0])
-    return 10000 * temp[0][1] + 100 * temp[1][1] + temp[2][1]
+#     temp.sort(key = lambda v : v[0])
+#     return 10000 * temp[0][1] + 100 * temp[1][1] + temp[2][1]
+
+def solution(rank, attendance) :
+    arr = sorted([(x, i) for i, x in enumerate(rank) if attendance[i]])
+    return 10000 * arr[0][1] + 100 * arr[1][1] + arr[2][1]
