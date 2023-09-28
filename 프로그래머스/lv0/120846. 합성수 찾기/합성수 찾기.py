c329010550bb8086d6557b53f49ebef7dc64edb6
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-28 13:46:11 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,12 +1,9 @@
 def solution(n):
-    num = []
     answer = 0
     for i in range(2, n + 1) :
-        for j in range(1, i + 1) :
+        for j in range(2, int(i ** 0.5) + 1) :
             
             if i % j == 0 :
-                num.append(i)
-                
-        if num.count(i) >= 3 :
-            answer += 1
+                answer += 1
+                break
     return answer
