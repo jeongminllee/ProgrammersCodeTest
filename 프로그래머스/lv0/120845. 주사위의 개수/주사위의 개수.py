b52484ec2df81899884826b773d74845f31d8984
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-28 12:23:37 +0900

    [level 0] Title: 주사위의 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.py
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.py
@@ -1,5 +1,4 @@
 def solution(box, n):
-    answer = 0
-    for i in range(len(box)) :
-        answer = (box[0] // n) * (box[1] // n) * (box[2] // n)
+    x, y, z = box
+    answer = (x // n) * (y // n) * (z // n)
     return answer
