642263636748adc888fc3301e037b902de9caf7b
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-28 11:08:05 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,5 +1,12 @@
-import numpy as np
+# import numpy as np
+
+# def solution(num_list, n):
+#     answer = np.array(num_list).reshape(-1, n)
+#     return answer.tolist()
 
 def solution(num_list, n):
-    answer = np.array(num_list).reshape(-1, n)
-    return answer.tolist()
+    answer = []
+    for i in range(0, len(num_list), n) :
+        answer.append(num_list[i:i + n])
+        
+    return answer
