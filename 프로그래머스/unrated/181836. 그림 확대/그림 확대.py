d69a4ed3edc8fe95ba6ea8bf8383e1be76075e7d
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-30 09:16:30 +0900

    [unrated] Title: 그림 확대, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.py
@@ -1,12 +1,20 @@
-def solution(picture, k):
-    answer = []
-    for row in picture :
-        resized = ''
+# def solution(picture, k):
+#     answer = []
+#     for row in picture :
+#         resized = ''
         
-        for pixel in row :
-            resized += pixel * k
+#         for pixel in row :
+#             resized += pixel * k
             
+#         for _ in range(k) :
+#             answer.append(resized)
+            
+#     return answer
+
+def solution(picture, k) :
+    answer = []
+    for i in range(len(picture)) :
         for _ in range(k) :
-            answer.append(resized)
+            answer.append(picture[i].replace('.', '.' * k).replace('x', 'x'*k))
             
     return answer
