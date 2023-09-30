b72b2c30b8b99dd15f0c1a792fe5689b2072e7fe
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-30 15:54:56 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -1,12 +1,21 @@
-def solution(s):
-    answer = 0
-    string = s.split(' ')
-    for i, num in enumerate(string) :
-        if num == 'Z' :
-            string[i] = '-' + string[i - 1]
-            if '--' in string[i]:
-                string[i] = string[i].replace('--', '')
+# def solution(s):
+#     answer = 0
+#     string = s.split(' ')
+#     for i, num in enumerate(string) :
+#         if num == 'Z' :
+#             string[i] = '-' + string[i - 1]
+#             if '--' in string[i]:
+#                 string[i] = string[i].replace('--', '')
 
-    for a in string :
-        answer += int(a)
-    return answer
+#     for a in string :
+#         answer += int(a)
+#     return answer
+
+def solution(s) :
+    answer = []
+    for i in s.split() :
+        if i != 'Z' :
+            answer.append(int(i))
+        else :
+            answer.pop()
+    return sum(answer)
