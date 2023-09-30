b04d0fcd43fb10c8bc933d231be598839b823e18
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-09-30 11:12:28 +0900

    [unrated] Title: 특별한 이차원 배열 1, Time: 0.26 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -1,10 +1,10 @@
-# def solution(n):
-#     return [[1 if i == j else 0 for i in range(n)] for j in range(n)]
+def solution(n):
+    return [[1 if i == j else 0 for i in range(n)] for j in range(n)]
     
-def solution(n) :
-    answer = [[0] * n for _ in range(n)]
+# def solution(n) :
+#     answer = [[0] * n for _ in range(n)]
     
-    for i in range(n) : 
-        answer[i][i] = 1
+#     for i in range(n) : 
+#         answer[i][i] = 1
         
-    return answer
+#     return answer
