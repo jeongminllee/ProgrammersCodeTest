3e6e853190a4cce5cf7d7ea35a56e4d72d860387
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-10-01 14:31:58 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.16 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,11 +1,11 @@
-# def solution(my_string):
-#     answer = ''
-#     for string in my_string :
-#         if string.islower() :
-#             answer += string.upper()
-#         else :
-#             answer += string.lower()
-#     return answer
+def solution(my_string):
+    answer = ''
+    for string in my_string :
+        if string.islower() :
+            answer += string.upper()
+        else :
+            answer += string.lower()
+    return answer
 
-def solution(my_string) :
-    return my_string.swapcase()
+# def solution(my_string) :
+#     return my_string.swapcase()
