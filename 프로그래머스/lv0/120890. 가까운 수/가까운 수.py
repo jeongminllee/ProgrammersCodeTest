1a2b0f42e18688081a9c033c24f02a075d966243
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-10-01 14:14:14 +0900

    [level 0] Title: 가까운 수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
@@ -1,1 +1,7 @@
-solution = lambda a, n:sorted(a, key=lambda x:(abs(x-n), x))[0]
+# solution = lambda a, n:sorted(a, key=lambda x:(abs(x-n), x))[0]
+
+
+def solution(array, n) :
+    array.sort(key = lambda x: (abs(x-n), x-n))
+    answer = array[0]
+    return answer
