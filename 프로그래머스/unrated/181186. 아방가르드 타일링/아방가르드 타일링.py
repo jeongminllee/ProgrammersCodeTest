cdb47de8bdb8af25711017371651cdf6be0abb70
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-10-02 21:18:59 +0900

    [unrated] Title: 아방가르드 타일링, Time: 2.50 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181186. 아방가르드 타일링/아방가르드 타일링.py
@@ -0,0 +1,48 @@
+mod = 1000000007 
+
+def matrix_product1(arr1, arr2) :
+    l = len(arr1)
+    new_arr = [[0] * l for _ in range(l)]
+    for i in range(l) :
+        for j in range(l) :
+            for k in range(l) :
+                new_arr[i][j] += arr1[i][k] * arr2[k][j]
+            new_arr[i][j] %= mod
+    return new_arr
+
+def matrix_product2(arr, lst) :
+    l = len(arr)
+    result = [0] * l
+    for i in range(l) :
+        for j in range(l) :
+            result[i] += arr[i][j] * lst[-j-1]
+            result[i] %= mod
+            
+    return result
+def solution(n) :
+    A = [1, 3, 10, 23, 62, 170]
+    
+    if n <= 6 :
+        return A[n - 1]
+    
+    arr = [[1, 2, 6, 1, 0, -1]]
+    for i in range(5) :
+        lst = [0] * 6
+        lst[i] = 1
+        arr.append(lst)
+        
+    mat = [row[:] for row in arr]
+    r_matrix = [[0] * 6 for _ in range(6)]
+    for i in range(6) :
+        r_matrix[i][i] = 1
+    
+    cnt = n - 3
+    while cnt > 0 :
+        if cnt % 2 :
+            r_matrix = matrix_product1(r_matrix, mat) 
+        mat = matrix_product1(mat, mat)
+        cnt //= 2
+        
+    result = matrix_product2(r_matrix, A)
+    
+    return result[3]
