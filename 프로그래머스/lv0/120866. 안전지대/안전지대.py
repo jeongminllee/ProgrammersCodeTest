5b40109dde9002a933f16045de79a90c3f51c5e7
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-10-06 20:03:28 +0900

    [level 0] Title: 안전지대, Time: 0.16 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.py
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.py
@@ -14,3 +14,14 @@ def solution(board):
     danger_list = danger_array.reshape(1, -1).squeeze()
     answer = Counter(danger_list)[0]
     return answer
+
+
+def solution(board) :
+    n = len(board)
+    danger = set()
+    for i, row in enumerate(board) :
+        for j, x in enumerate(row) :
+            if not x :
+                continue
+            danger.update((i + di, j + dj) for di in [-1, 0, 1] for dj in [-1, 0, 1])
+    return n * n - sum(0 <= i < n and 0 <= j < n for i, j in danger)
