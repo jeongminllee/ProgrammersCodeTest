4e7462c72c15501a5fd5f6ce9cc3bbeb4da1807e
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-10-06 18:39:47 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,3 +1,10 @@
+import re
+
 def solution(my_string):
-    s = ''.join(i if i.isdigit() else ' ' for i in my_string)
-    return sum(int(n) for n in s.split())
+    return sum([int(n) for n in re.findall('\d+', my_string)])
+
+
+
+# def solution(my_string):
+#     s = ''.join(i if i.isdigit() else ' ' for i in my_string)
+#     return sum(int(n) for n in s.split())
