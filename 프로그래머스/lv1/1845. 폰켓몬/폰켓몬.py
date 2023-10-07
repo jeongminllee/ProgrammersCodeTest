71a37ef39ad0f6532ff0618a6ca40c355354adcd
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-10-07 22:50:09 +0900

    [level 1] Title: 폰켓몬, Time: 1.46 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,11 @@
+def solution(nums):
+    # choice = ((len(nums) - 1) // 2) + 1
+    # nums 항상 짝수로만 주어짐
+    choice = len(nums) // 2
+    num = []
+    for n in nums :
+        if n not in num :
+            num.append(n) 
+        if len(num) > choice :
+            return choice
+    return len(num)
