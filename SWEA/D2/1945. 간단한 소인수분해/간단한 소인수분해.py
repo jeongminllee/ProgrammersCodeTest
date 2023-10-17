4018b5a2eaaa852f664fa06aa3bcf6df95f478a1
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-10-17 20:51:31 +0900

    [D2] Title: 간단한 소인수분해, Time: 146 ms, Memory: 58,260 KB -BaekjoonHub

---
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -0,0 +1,14 @@
+T = int(input())
+divs = [2, 3, 5, 7, 11]
+for test_case in range(1, T + 1) :
+    N = int(input())
+    
+    cnts = [0] * 5
+    for i in range(5) :
+        while N % divs[i]  == 0 :
+            cnts[i] += 1
+            N //= divs[i]
+            
+    
+    
+    print(f"#{test_case}", *cnts)
