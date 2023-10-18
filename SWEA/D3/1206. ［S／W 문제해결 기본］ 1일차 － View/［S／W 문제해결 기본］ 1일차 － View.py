68ea4bfe4dc4dc787e06bd5de3688bab751cdb32
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-10-18 16:42:24 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 160 ms, Memory: 62,016 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -4,13 +4,7 @@ for test_case in range(1, T + 1) :
     lst = list(map(int, input().split()))
     ans = 0
     for i in range(2, N - 2) :
-        mx = lst[i - 2]
-        for j in range(i-1, i + 3):
-            if j == i :
-                continue
-            else :
-                if mx < lst[j] :
-                    mx = lst[j]
+        mx = max(lst[i - 2 : i] + lst[i + 1 : i + 3])
         if lst[i] > mx :
             ans += lst[i] - mx
             
