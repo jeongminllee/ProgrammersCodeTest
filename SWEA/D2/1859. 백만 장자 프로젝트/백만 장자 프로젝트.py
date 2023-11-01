844ac6470c069406807b802ae474e8d23b7d8095
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-02 00:25:08 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,066 ms, Memory: 253,480 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -1,17 +1,24 @@
 T = int(input())
-for test_case in range(1, T+1) :
+for test_case in range(1, T + 1):
     N = int(input())
-    lst = list(map(int,input().split()))
+    # lst = list(map(int, input().split()))
 
-    ans = s = 0
-    while s < N :
-        i_max = s
-        for i in range(s + 1, N) :
-            if lst[i_max] < lst[i] :
-                i_max = i
-        for i in range(s, i_max) :
-            ans += (lst[i_max] - lst[i])
+    # ans = s = 0
+    # while s<N:
+    #     i_max = s
+    #     for i in range(s+1, N):
+    #         if lst[i_max] < lst[i]:
+    #             i_max = i
+    #     for i in range(s, i_max):
+    #         ans += (lst[i_max] - lst[i])
+    #     s = i_max + 1
 
-        s = i_max + 1
+    lst = list(map(int, input().split()))
+    ans = mx = 0
+    for n in lst[::-1]:
+        if mx < n:
+            mx = n
+        else:
+            ans += (mx - n)
 
-    print(F"#{test_case} {ans}")
+    print(f'#{test_case} {ans}')
