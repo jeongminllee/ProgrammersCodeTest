9909056c31ebf033f81b85243f5b61e9f0c5e740
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-05 20:42:47 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,43 +1,24 @@
-class StackDef:
-    def push(stack, value):
-        stack.append(value)
-        return stack
-    
-    def pop(stack, result):
-        if len(stack) == 0:
-            result.append(-1)
-        else:
-            result.append(int(stack[-1]))
-            stack.pop()
-        return stack, result
-    
-    def size(stack, result):
-        result.append(len(stack))
-        return stack, result
-    
-    def empty(stack, result):
-        if len(stack) == 0:
-            result.append(1)
-        else:
-            result.append(0)
-        return stack, result
-    
-    def top(stack, result):
-        if len(stack) == 0:
-            result.append(-1)
-        else:
-            result.append(int(stack[-1]))
-        return stack, result
+import sys
+input = sys.stdin.readline
 
 
-stack = []
-result = []
-for i in range(int(input())):
-    inputValue = input().split(" ")
-    if len(inputValue) == 1:
-        stack, result = getattr(StackDef, inputValue[0])(stack, result)
-    else:
-        stack = getattr(StackDef, inputValue[0])(stack, inputValue[1])
-    
-for j in result:
-    print(j)
+def sol():
+    stack = []
+    N = int(input())
+
+    for n in range(N):
+        command = input().rstrip()
+        if command == "pop":
+            print(stack.pop() if stack else -1)
+        elif command == "size":
+            print(len(stack))
+        elif command == "empty":
+            print(+(not stack))
+        elif command == "top":
+            print(stack[-1] if stack else -1)
+        else:
+            _, x = command.split()
+            stack.append(x)
+
+
+sol()
