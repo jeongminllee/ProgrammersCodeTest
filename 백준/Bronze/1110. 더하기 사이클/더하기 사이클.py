e57b134ee2c7f2401e917047cf9507e5f89c0980
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-05 12:27:36 +0900

    [Bronze I] Title: 더하기 사이클, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,15 +1,14 @@
 N = int(input())
-N_input = N
-x = []
-x.append(N)
+a = N // 10
+b = N % 10
+i = 0
+
 while True :
-    a = N // 10
-    b = N % 10
-    c = (a + b) % 10
-    d = b * 10 + c
-    if d == N_input :
-        print(len(x))
+    c = b * 10 + (a + b) % 10
+    a = c // 10
+    b = c % 10
+    i += 1
+    if c == N :
         break
-    else :
-        x.append(d)
-        N = d
+
+print(i)
