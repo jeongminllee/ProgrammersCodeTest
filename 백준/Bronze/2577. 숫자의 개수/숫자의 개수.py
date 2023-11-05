f4ee1d2e13e40d7a5a0c0d2c5fdbd6ae8fd4a6c0
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-05 12:48:34 +0900

    [Bronze II] Title: 숫자의 개수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,19 +1,28 @@
+# A = int(input())
+# B = int(input())
+# C = int(input())
+
+# n = A * B * C
+# n_lst = list(str(n))
+# # print(n_lst)
+#
+# cnt_lst = [0] * 10
+# # print(cnt_lst)
+#
+# for i in range(10) :
+#     for n in n_lst :
+#         if i == int(n) :
+#             cnt_lst[i] += 1
+#             # print(cnt_lst)
+#
+# for cnt in cnt_lst :
+#     print(cnt)
+
 A = int(input())
 B = int(input())
 C = int(input())
 
 n = A * B * C
-n_lst = list(str(n))
-# print(n_lst)
-
-cnt_lst = [0] * 10
-# print(cnt_lst)
 
 for i in range(10) :
-    for n in n_lst :
-        if i == int(n) :
-            cnt_lst[i] += 1
-            # print(cnt_lst)
-
-for cnt in cnt_lst :
-    print(cnt)
+    print(str(n).count(str(i)))
