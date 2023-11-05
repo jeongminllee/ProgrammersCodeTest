5cd49563bf7e513d255396e423d98c8645854fc9
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-05 18:56:13 +0900

    [Bronze II] Title: OX퀴즈, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -1,14 +1,13 @@
 T = int(input())
 
-for i in range(T) :
-    o = 0
-    s = 0
-    lst = list(map(str, input()))
-    for e in range(len(lst)) :
-        if lst[e] == 'O' :
-            o += 1
-            s += o
+for _ in range(T) :
+    score, k = 0, 0
+    lst = input().rstrip()
+    for j in lst :
+        if j == 'O' :
+            k += 1
         else :
-            o = 0
+            k = 0
 
-    print(s)
+        score += k
+    print(score)
