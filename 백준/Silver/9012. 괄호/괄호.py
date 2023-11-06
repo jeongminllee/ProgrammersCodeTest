862fa8f49121a61083e59f08426c00497ca1a739
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-07 07:37:32 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,19 @@
+T = int(input())
+
+for t in range(T) :
+    stack = []
+    a = input()
+    for j in a :
+        if j == '(' :
+            stack.append(j)
+        elif j == ')' :
+            if stack :
+                stack.pop()
+            else :
+                print("NO")
+                break
+    else :
+        if not stack :
+            print("YES")
+        else :
+            print("NO")
