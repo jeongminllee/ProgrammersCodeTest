415cb42e09a2935f1fb81d41f713c0751f2706b8
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-06 22:15:23 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,10 +1,10 @@
-def solution(brown, yellow) :
-    tile = brown + yellow
-    rows = [(tile) // c for c in range(3, (tile) // 3 + 1) if tile % c == 0]
-    for row in rows :
-        col = tile // row
-        if 2*(row + col - 2) == brown :
-            return [row, col]
+# def solution(brown, yellow) :
+#     tile = brown + yellow
+#     rows = [(tile) // c for c in range(3, (tile) // 3 + 1) if tile % c == 0]
+#     for row in rows :
+#         col = tile // row
+#         if 2*(row + col - 2) == brown :
+#             return [row, col]
             
 
 # 가로 >= 세로
@@ -15,3 +15,8 @@ def solution(brown, yellow) :
 # 옐로우 = tile - 옐로우
 # print(len([1, 2, 3, 4, 6, 8, 12, 16, 24, 48]))
 #
+def solution(brown, yellow) :
+    for i in range(1, int(yellow**(1/2)) + 1) :
+        if yellow % i == 0 :
+            if 2*(i + yellow//i) == brown - 4 :
+                return [yellow//i+2, i+2]
