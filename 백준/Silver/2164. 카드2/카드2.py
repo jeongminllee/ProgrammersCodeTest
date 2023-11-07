e5cbe7fccc11abb55a97a463263a948e5fbc7a75
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-08 00:02:57 +0900

    [Silver IV] Title: 카드2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,10 +1,4 @@
-N = int(input())
-s = 2
-while True :
-    if (N == 1 or N == 2) :
-        print(N)
-        break
-    s *= 2
-    if s >= N :
-        print((N - (s // 2)) * 2)
-        break
+n, m = int(input()), 1
+while m < n :
+    m *= 2
+print(2 * n - m)
