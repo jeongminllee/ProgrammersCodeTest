032ed5866471f30c1eb0aafcbdaa1d064bb969e9
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-08 16:20:11 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,22 +1,10 @@
 T = int(input())
 
 for _ in range(T) :
-    coin = [0] * 4
     N = int(input())
-    while N != 0 :
-        if N >= 25 :
-            N -= 25
-            coin[0] += 1
+    q, N=divmod(N, 25)
+    d, N=divmod(N, 10)
+    n, N=divmod(N, 5)
+    p = N
 
-        elif N >= 10 :
-            N -= 10
-            coin[1] += 1
-
-        elif N >= 5 :
-            N -= 5
-            coin[2] += 1
-
-        elif N >= 1 :
-            N -= 1
-            coin[3] += 1
-    print(coin[0], coin[1], coin[2], coin[3])
+    print(q, d, n, p)
