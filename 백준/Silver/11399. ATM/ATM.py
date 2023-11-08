2af61fcf3058f5ed18bdd59fc975c82400bf115a
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-08 17:29:16 +0900

    [Silver IV] Title: ATM, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -2,6 +2,6 @@ T = int(input())
 lst = list(map(int, input().split()))
 lst.sort()
 answer = 0
-for i in range(1, T + 1) :
-    answer += lst[i - 1] * (T - i + 1)
+for i in range(1, len(lst) + 1) :
+    answer += lst[i - 1] * (len(lst) - i + 1)
 print(answer)
