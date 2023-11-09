7c2daeb1c5f3fea721a1ed549815e6c7a1cacb58
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-09 10:53:25 +0900

    [level 3] Title: 네트워크, Time: 0.50 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,28 @@
+def solution(n, computers):
+    answer = 0
+    visited = [False for _ in range(n)]
+    for com in range(n) :
+        if visited[com] == False :
+            DFS(n, computers, com, visited)
+            # BFS(n, computers, com, visited)
+            answer += 1
+    return answer
+
+def DFS(n, computers, com, visited) :
+    visited[com] = True
+    for connect in range(n) :
+        if connect != com and computers[com][connect] == 1 : # 연결된 컴퓨터
+            if visited[connect] == False :
+                DFS(n, computers, connect, visited)
+
+def BFS(n, computers, com, visited) :
+    visited[com] = True
+    queue = []
+    queue.append(com)
+    while len(queue) != 0 :
+        com = queue.pop(0)
+        visited[com] = True
+        for connect in range(n) :
+            if connect != com and computers[com][connect] == 1:
+                if visited[connect] == False :
+                    queue.append(connect)
