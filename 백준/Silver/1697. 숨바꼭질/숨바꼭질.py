8eb431a77be42b33c1e76cb15b2bbf38d5b37a38
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-09 15:33:45 +0900

    [Silver I] Title: 숨바꼭질, Time: 1244 ms, Memory: 38824 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,24 @@
+def bfs(s, e) :
+    # [1] q 생성, v[] 생성
+    q = []
+    v = [0] * 200001
+
+    # [2] 초기 데이터 삽입, v[] 초기화
+    q.append(s)
+    v[s] = 1
+
+    while q:
+        c = q.pop(0)
+        if c == e :
+            return v[e] - 1
+        # 3방향, 범위내(0~200000), 미방문
+        for n in (c - 1, c + 1, c * 2) :
+            if 0 <= n <= 200000 and v[n] == 0 :
+                q.append(n)
+                v[n] = v[c] + 1
+
+    return -1
+
+N, K = map(int, input().split())
+ans = bfs(N, K)
+print(ans)
