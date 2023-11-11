986e98f7b42c27b2de4ad8f94fd0601eb65d9dcb
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-11 22:54:51 +0900

    [Silver IV] Title: 색종이 - 2, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
@@ -0,0 +1,21 @@
+def count(arr) :
+    cnt = 0
+    for lst in arr :
+        for i in range(1, len(lst)) :
+            if lst[i - 1] != lst[i] :
+                cnt += 1
+    return cnt
+
+N = int(input())
+
+arr = [[0] * 102 for _ in range(102)]
+for _ in range(N) :
+    # [1] 해당 영역을 1로 표시
+    sj, si = map(int, input().split())
+    for i in range(si, si + 10) :
+        for j in range(sj, sj + 10) :
+            arr[i][j] = 1
+
+arr_t = list(zip(*arr)) # 전치행렬 : 수정 필요시 list(map(lst, zip(*arr)))
+ans = count(arr) + count(arr_t)
+print(ans)
