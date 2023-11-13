efbd69b78b45d8d7f0cec4cd488b6f436e59cc98
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-13 11:23:39 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 176 ms, Memory: 114016 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -1,25 +1,39 @@
 from collections import deque
 
+# 1000m 이동 가능
+# 좌표 형태로 저장 -> lst
+# lst = (1000,0) (1000, 1000) N = 2
+# ci, cj 기준으로 n개의 편의점 이동 가능 확인
+# v[] N = 2
+
+# 조건
+# for i (N)
+# if v[i] == 0 : ## 미방문
+# 범위 내, |cj - nj| + |ci - ni|
+# Q 삽입, v[] 표시
 def bfs(sj, si, ej, ei) :
+    # Q 생성, 필요 변수 등 생성
     q = deque()
     v = [0] * N
 
+    # Q에 초기데이터 삽입, si, sj는 v에 표시X
     q.append((sj, si))
 
     while q :
         cj, ci = q.popleft()
 
-        if abs(cj - ej) + abs(ci - ei) <= 1000 :    
+        if abs(cj - ej) + abs(ci - ei) <= 1000 :    # 목적지까지 도달 가능
             return 'happy'
 
+        # 미방문 모든 편의점 좌표 : 범위 내인지 체크
         for i in range(N) :
-            if v[i] == 0 :  
+            if v[i] == 0 :  # 미방문 편의점
                 nj, ni = lst[i]
-                if abs(cj-nj) + abs(ci-ni) <= 1000 :    
+                if abs(cj-nj) + abs(ci-ni) <= 1000 :    # 범위 내
                     q.append((nj, ni))
                     v[i] = 1
     return 'sad'
-                    
+
 T = int(input())
 for _ in range(T) :
     N = int(input())
