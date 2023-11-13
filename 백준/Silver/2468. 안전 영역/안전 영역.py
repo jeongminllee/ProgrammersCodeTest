2b636920ffe413fd928646f8cea644e4fb29828a
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-13 10:36:13 +0900

    [Silver I] Title: 안전 영역, Time: 276 ms, Memory: 116032 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,46 +1,31 @@
 from collections import deque
 
+def bfs(si, sj, h) :
+    q = deque()
+    q.append((si, sj))
+    v[si][sj] = 1
+
+    while q :
+        ci, cj = q.popleft()
+        # 네 방향, 범위 내, 미방문, 높이>h
+        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)) :
+            ni, nj = ci+di, cj+dj
+            if 0 <= ni < N and 0 <= nj < N and v[ni][nj] == 0 and arr[ni][nj] > h :
+                q.append((ni, nj))
+                v[ni][nj] = 1
+def solve(h) :  # h 높이에 대해 잠기지 않는 영역 개수 리턴
+    cnt = 0
+    for i in range(N) :
+        for j in range(N) :
+            if v[i][j] == 0 and arr[i][j] > h :
+                bfs(i, j, h)
+                cnt += 1
+    return cnt
+
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
-maxNum = 0
-
-for i in range(N) :
-    for j in range(N) :
-        if arr[i][j] > maxNum :
-            maxNum = arr[i][j]
-
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-def bfs(a, b, val, visited) :
-    q = deque()
-    q.append((a, b))
-    visited[a][b] = 1
-
-    while q:
-        x, y = q.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0 <= nx < N and 0 <= ny < N :
-                if arr[nx][ny] > val and visited[nx][ny] == 0 :
-                    visited[nx][ny] = 1
-                    q.append((nx, ny))
-
 ans = 0
-for i in range(maxNum) :
-    visited = [[0] * N for _ in range(N)]
-    cnt = 0
-
-    for j in range(N) :
-        for k in range(N) :
-            if arr[j][k] > i and visited[j][k] == 0 :
-                bfs(j, k, i, visited)
-                cnt += 1
-
-    if ans < cnt :
-        ans = cnt
-
+for h in range(100) :   # 높이 0 ~ 99까지 물 높이 지정
+    v = [[0] * N for _ in range(N)]
+    ans = max(ans, solve(h))
 print(ans)
