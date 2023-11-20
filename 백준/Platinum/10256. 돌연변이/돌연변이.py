48c564eeb68cacc6c0e64b1c113a071887a75e6e
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-20 22:19:38 +0900

    [Platinum II] Title: 돌연변이, Time: 5408 ms, Memory: 248400 KB -BaekjoonHub

--- 백준/Platinum/10256. 돌연변이/돌연변이.py
+++ 백준/Platinum/10256. 돌연변이/돌연변이.py
@@ -2,89 +2,86 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-class Trie :
+class Trie() :
     def __init__(self):
         self.next = [None] * 4
         self.fail = None
         self.output = 0
 
-def insert(trie, s, idx):
-    if idx >= len(s) :
-        trie.output = 1
-        return
-    x = ord(s[idx]) - ord('0')
-    if not trie.next[x] :
-        trie.next[x] = Trie()
-    insert(trie.next[x], s, idx + 1)
+    def ConstFail(self):
+        q = deque()
+        q.append(self)
+        self.fail = self
 
-def construct_fail(root) :
-    q = deque()
-    root.fail = root
-    q.append(root)
-    while q :
-        cur = q.popleft()
-        for i in range(4) :
-            nxt = cur.next[i]
-            if not nxt :
-                continue
-            if root == cur :
-                nxt.fail = root
-            else :
-                tmp = cur.fail
-                while tmp != root and not tmp.next[i] :
-                    tmp = tmp.fail
-                if tmp.next[i] :
-                    tmp = tmp.next[i]
-                nxt.fail = tmp
-            nxt.output += nxt.fail.output
-            q.append(nxt)
+        while q :
+            cur = q.popleft()
 
-def solve(s, root) :
-    ret = 0
-    cur = root
-    for char in s :
-        nxt = ord(char) - ord('0')
-        while cur != root and not cur.next[nxt] :
-            cur = cur.fail
-        if cur.next[nxt] :
-            cur = cur.next[nxt]
-        ret += cur.output
-    return ret
+            for i in range(4) :
+                childNode = cur.next[i]
+                if not childNode :
+                    continue
 
+                if cur == self :
+                    childNode.fail = self
+
+                else :
+                    f = cur.fail
+
+                    while f != self and not f.next[i] :
+                        f = f.fail
+                    if f.next[i] :
+                        f = f.next[i]
+
+                    childNode.fail = f
+                q.append(childNode)
+    def Add(self, pattern):
+        cur = self
+
+        for i in range(len(pattern)) :
+            for j in range(i + 1, len(pattern), 1) :
+                fixedCur = cur
+
+                for idx in range(i, len(pattern)) :
+                    p = pattern[idx] if (idx < i or idx > j) else pattern[j - (idx - i)]
+                    if not fixedCur.next[p] :
+                        fixedCur.next[p] = Trie()
+                    fixedCur = fixedCur.next[p]
+                fixedCur.output = 1
+
+            if not cur.next[pattern[i]] :
+                cur.next[pattern[i]] = Trie()
+
+            cur = cur.next[pattern[i]]
+            if i == len(pattern) - 1 :
+                cur.output = 1
+
+    def Search(self, pattern):
+        res = 0
+        cur = self
+        for p in pattern :
+            while cur != self and not cur.next[p] :
+                cur = cur.fail
+
+            if cur.next[p] :
+                cur = cur.next[p]
+
+            if cur.output :
+                res += 1
+
+        return res
+
+cvDict = {"A" : 0, "C" : 1, "G" : 2, "T" : 3}
+
+def conv(string) :
+    return [cvDict[s] for s in string]
 
 T = int(input())
 for _ in range(T) :
     N, M = map(int, input().split())
-    s = list(input().rstrip())
-    m = list(input().rstrip())
+    s = conv(input().rstrip())
+    m = conv(input().rstrip())
 
     root = Trie()
-    for i in range(N) :
-        if s[i] == 'A' :
-            s[i] = '0'
-        elif s[i] == 'C' :
-            s[i] = '1'
-        elif s[i] == 'G' :
-            s[i] = '2'
-        else :
-            s[i] = '3'
-
-    for i in range(M) :
-        if m[i] == 'A' :
-            m[i] = '0'
-        elif m[i] == 'C' :
-            m[i] = '1'
-        elif m[i] == 'G' :
-            m[i] = '2'
-        else :
-            m[i] = '3'
-
-    insert(root, m, 0)
-    for i in range(M + 1) :
-        for j in range(i + 2, M + 1) :
-            m[i : j] = reversed(m[i : j])
-            insert(root, m, 0)
-            m[i : j] = reversed(m[i : j])
-
-    construct_fail(root)
-    print(solve(s, root))
+    root.Add(m)
+    root.ConstFail()
+    print(root.Search(s))
