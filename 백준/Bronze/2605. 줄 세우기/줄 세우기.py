44f723dd3aaba1e6ce47716baeb078b8c6e99c02
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-22 13:14:01 +0900

    [Bronze II] Title: 줄 세우기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.py
@@ -0,0 +1,10 @@
+N = int(input())
+lst = list(map(int, input().split()))
+alst = [n for n in range(1, N + 1)]
+
+for i in range(N) :        
+    n, t = lst[i], alst[i]  
+    for j in range(i, i - n, -1) :
+        alst[j] = alst[j - 1]
+    alst[i - n] = t
+print(*alst)
