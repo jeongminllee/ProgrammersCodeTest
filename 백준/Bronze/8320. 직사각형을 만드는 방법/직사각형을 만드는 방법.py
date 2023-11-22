a8d4c023ca1c1fc86b47ae4f98674ebb0c9062e7
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-22 12:42:14 +0900

    [Bronze II] Title: 직사각형을 만드는 방법, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.py
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.py
@@ -1,9 +1,8 @@
 N = int(input())
-
-ans = 0
-for i in range(1, N + 1) :
-    for j in range(i, N + 1) :
-        if i * j <= N :
-            ans += 1
-
+ans = N
+for i in range(2, N) :
+    n = N // i - (i - 1)
+    if n < 1 :
+        break
+    ans += n
 print(ans)
