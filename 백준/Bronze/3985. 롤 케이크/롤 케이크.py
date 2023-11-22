13b2bbc89043632b96eadfbda87863b7ea295428
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-11-22 13:48:02 +0900

    [Bronze I] Title: 롤 케이크, Time: 128 ms, Memory: 110404 KB -BaekjoonHub

---
+++ 백준/Bronze/3985. 롤 케이크/롤 케이크.py
@@ -0,0 +1,17 @@
+L = int(input())
+N = int(input())
+lst = [1] * (L + 1)
+mx1 = mx1_i = mx2 = mx2_i = 0
+for i in range(1, N + 1) :  
+    s, e = map(int, input().split())
+    if mx1 < (e - s + 1) :  
+        mx1, mx1_i = (e - s + 1), i
+
+    cnt = sum(lst[s : e + 1])  
+    if mx2 < cnt :
+        mx2, mx2_i = cnt, i
+
+    lst[s : e + 1] = [0] * (e - s + 1)
+
+print(mx1_i)
+print(mx2_i)
