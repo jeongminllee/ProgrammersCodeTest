735af37ef45a8b33da963f89a3da472c0b1d15df
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-12-15 05:49:17 +0900

    [Silver I] Title: 포도주 시식, Time: 140 ms, Memory: 110884 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,11 +1,12 @@
 n = int(input())
-lst = [int(input()) for _ in range(n)] + [0]
+lst = [0] + [int(input()) for _ in range(n)]
 dp = [0] * (n + 1)
+
 
 dp[0] = lst[0]
 dp[1] = sum(lst[:2])
 
-for i in range(2, n) :
+for i in range(2, n + 1) :
     dp[i] = max(dp[i - 1], dp[i - 2] + lst[i], dp[i - 3] + lst[i - 1] + lst[i])
 
 print(max(dp))
