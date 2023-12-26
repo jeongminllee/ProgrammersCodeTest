9f513e2c171db23f73a8dde6c81af96f7c0a4813
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-12-26 15:40:29 +0900

    [Silver V] Title: 기상캐스터, Time: 124 ms, Memory: 110832 KB -BaekjoonHub

---
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.py
@@ -0,0 +1,18 @@
+H, W = map(int, input().split())
+arr = [input() for _ in range(H)]
+v = [[-1] * W for _ in range(H)]
+
+for i in range(H) :
+    cnt = -1                    
+    for j in range(W) :
+        if arr[i][j] == 'c' :   
+            cnt = 0
+
+        else :                  
+            if cnt >= 0 :
+                cnt += 1
+
+        v[i][j] = cnt
+
+for lst in v :
+    print(*lst)
