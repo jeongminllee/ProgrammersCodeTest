37b805289748602ef7b8fc136cf55f1688d26f3b
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2023-12-26 16:48:24 +0900

    [Silver IV] Title: 설탕 배달, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,21 +1,14 @@
-T = int(input())
+N = int(input())
 
-cnt = 0
-
-if T % 5 == 0 :
-    print(T // 5)
-else :
+def delivery(N) :
     cnt = 0
-    while T > 0 :
-        T -= 3
+    while N >= 0 :
+        if N % 5 == 0 :
+            cnt += N // 5
+            print(cnt)
+            return
+        N -= 3
         cnt += 1
-        if T % 5 == 0 :
-            cnt += T // 5
-            print(cnt)
-            break
-        elif T == 1 or T == 2 :
-            print(-1)
-            break
-        elif T == 0 :
-            print(cnt)
-            break
+    print(-1)
+
+delivery(N)
