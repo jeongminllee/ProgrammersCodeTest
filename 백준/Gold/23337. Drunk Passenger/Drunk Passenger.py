d6067c80c6b8af87a01bb563ae95eb745ad361b4
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-02-08 05:53:20 +0900

    [Gold V] Title: Drunk Passenger, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/23337. Drunk Passenger/Drunk Passenger.py
+++ 백준/Gold/23337. Drunk Passenger/Drunk Passenger.py
@@ -1,11 +1,8 @@
-from decimal import Decimal, getcontext
-getcontext().prec = 12
-
 n = int(input())
 dp = [0.0 for _ in range(n+1)]
-dp[1] = Decimal(1.0) / Decimal(n-1)
+dp[1] = 1 / (n - 1)
 
 for i in range(2, n):
-    dp[i] = dp[i-1] + dp[i-1] * (Decimal(1.0) / Decimal(n-i+1))
+    dp[i] = dp[i-1] + dp[i-1] * (1 / (n-i+1))
 
 print(float(dp[n-1]))
