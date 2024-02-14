64a02478ec630091c399b891e7a78d9c2d738b34
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-02-15 04:11:33 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,40 +1,19 @@
-def find(parent, i) :
-    if parent[i] == i :
-        return i
-    parent[i] = find(parent, parent[i])
-    return parent[i]
-
-def union(parent, rank, x, y) :
-    xroot = find(parent, x)
-    yroot = find(parent, y)
-
-    if rank[xroot] < rank[yroot] :
-        parent[xroot] = yroot
-    elif rank[xroot] > rank[yroot] :
-        parent[yroot] = xroot
+def ancestor(node, parents) :
+    if parents[node] == node :
+        return node
     else :
-        parent[yroot] = xroot
-        rank[xroot] += 1
-def solution(n, costs):
-    costs.sort(key=lambda x:x[2])
+        return ancestor(parents[node], parents)
+def solution(n, costs) :
+    answer = 0
+    edges = sorted([(x[2], x[0], x[1]) for x in costs])
+    parents = [i for i in range(n)]
+    bridges = 0
+    for w, f, t in edges :
+        if ancestor(f, parents) != ancestor(t, parents) :
+            answer += w
+            parents[ancestor(f, parents)] = t
+            bridges += 1
 
-    parent = [i for i in range(n)]
-
-    rank = [0] * n
-
-    min_cost = 0    
-    edges = 0       
-
-    for edge in costs :
-        if edges == n - 1:
+        if bridges == n - 1 :
             break
-
-        x = find(parent, edge[0])
-        y = find(parent, edge[1])
-
-        if x != y :
-            union(parent, rank, x, y)
-            min_cost += edge[2]
-            edges += 1
-
-    return min_cost
+    return answer
