d6fbc6873c097ea2d6668e1e517668d7b4467b58
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-02-16 16:13:47 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,17 @@
+def solution(want, number, discount):
+    answer = 0
+    want_dict = {}
+    for i in range(len(want)) :
+        want_dict[want[i]] = number[i]
+    
+    for i in range(len(discount) - 9) :
+        discount_10d = {}
+        
+        for j in range(i, i + 10) :
+            if discount[j] in want_dict :
+                discount_10d[discount[j]] = discount_10d.get(discount[j], 0) + 1
+            
+            if discount_10d == want_dict :
+                answer += 1
+    
+    return answer
