fb28dc178704fc3b9121c010bcf9657628623a0c
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-02-19 18:30:56 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -0,0 +1,15 @@
+import math
+T = int(input())
+for _ in range(T) :
+    x, y = map(int, input().split())
+    d = y - x
+    if d <= 3 :
+        print(d)
+    else :
+        n = int(math.sqrt(d))
+        if d == n ** 2 :
+            print(2 * n - 1)
+        elif n ** 2 < d <= n * (n + 1) :
+            print(2 * n)
+        else :
+            print(2 * n + 1)
