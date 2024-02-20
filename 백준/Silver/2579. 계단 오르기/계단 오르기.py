158ea1ac94a89d6d8235c2230700c42cc72ad62b
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-02-20 22:44:11 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,12 +1,15 @@
-n = int(input())
-arr = [0] + list(int(input()) for _ in range(n))
-dp = [0] * (n + 1)
+T = int(input())
+arr = [0]
 
-dp[1] = arr[1]
-if n >= 2 :
-    dp[2] = arr[1] + arr[2]
+for _ in range(T) :
+    x = int(input())
+    arr.append(x)
+g = [0, 0]
+h = [0, arr[1]]
 
-for i in range(3, n + 1) :
-    dp[i] = max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i])
 
-print(dp[-1])
+for i in range(2, T + 1) :
+    g.append(h[i-1] + arr[i])
+    h.append(max(g[i-2], h[i-2]) + arr[i])
+
+print(max(g[T], h[T]))
