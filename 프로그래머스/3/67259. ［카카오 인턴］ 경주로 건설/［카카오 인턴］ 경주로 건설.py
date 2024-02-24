28d40fab51f62bd2afbc1dd0a5f91d8b2d140653
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-02-25 05:56:22 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -15,4 +15,57 @@ def solution(board):
                 if costs[nx][ny][new_direction] > new_cost:
                     costs[nx][ny][new_direction] = new_cost
                     queue.append(((nx, ny), new_cost, new_direction))
+    print(costs)
     return min(costs[-1][-1])
+
+
+# def solution(board) :
+#     # 1. 주어진 좌표가 보드의 범위 내에 있는지 확인
+#     def is_valid(x, y) :
+#         return 0 <= x < n and 0 <= y < n
+
+#     # 2. 주어진 좌표가 차단되었거나 이동할 수 있는지 확인
+#     def is_blocked(x, y) :
+#         return (x, y) == (0, 0) or not is_valid(x, y) or board[x][y] == 1
+
+#     # 3. 이전 방향과 현재 방향에 따라 비용 계산
+#     def calculate_cost(direction, prev_direction, cost) :
+#         if prev_direction == -1 or (prev_direction - direction) % 2 == 0 :
+#             return cost + 100
+#         else :
+#             return cost + 600
+
+#     # 4. 주어진 좌표와 방향이 아직 방문하지 않았거나 새 비용이 더 적은 경우
+#     def isShouldUpdate(x, y, direction, new_cost) :
+#         return visited[x][y][direction] == 0 or visited[x][y][direction] > new_cost
+
+#     queue = deque([((0, 0), -1, 0)])
+#     n = len(board)
+#     directions = [(0, -1), (-1, 0), (0, 1), (1, 0)]
+#     visited = [[[0 for _ in range(4)] for _ in range(n)] for _ in range(n)]
+#     answer = float('inf')
+
+#     # 5. 큐가 빌 때까지 반복
+#     while queue :
+#         (x, y), prev_direction, cost = queue.popleft()
+
+#         # 6. 가능한 모든 방향에 대해 반복
+#         for direction, (dx, dy) in enumerate(directions) :
+#             nx, ny = x + dx, y + dy
+
+#             # 7. 이동할 수 없는 좌표는 건너뛰기
+#             if is_blocked(nx, ny) :
+#                 continue
+
+#             new_cost = calculate_cost(direction, prev_direction, cost)
+
+#             # 8. 도착지에 도달한 경우 최소 비용 업데이트
+#             if (nx, ny) == (n - 1, n - 1) :
+#                 answer = min(answer ,new_cost)
+
+#             # 9. 좌표와 방향이 아직 방문하지 않았거나 새 비용이 더 작은 경우 큐에 추가
+#             elif isShouldUpdate(nx, ny, direction, new_cost) :
+#                 queue.append(((nx, ny), direction, new_cost))
+#                 visited[nx][ny][direction] = new_cost
+
+#     return answer
