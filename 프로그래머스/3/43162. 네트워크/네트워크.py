f0a8fb3e0d3db8abc8a633206842bbd36cff0198
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-02-24 23:09:56 +0900

    [level 3] Title: 네트워크, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,28 +1,43 @@
-def solution(n, computers):
+def dfs(computers, visited, node) :
+    visited[node] = 1
+    for idx, connected in enumerate(computers[node]) :
+        if connected and not visited[idx] :
+            dfs(computers, visited, idx)
+
+def solution(n, computers) :
     answer = 0
-    visited = [False for _ in range(n)]
-    for com in range(n) :
-        if visited[com] == False :
-            DFS(n, computers, com, visited)
-            # BFS(n, computers, com, visited)
+    visited = [0] * n
+    for i in range(n) :
+        if not visited[i] :
+            dfs(computers, visited, i)
             answer += 1
     return answer
-
-def DFS(n, computers, com, visited) :
-    visited[com] = True
-    for connect in range(n) :
-        if connect != com and computers[com][connect] == 1 : # 연결된 컴퓨터
-            if visited[connect] == False :
-                DFS(n, computers, connect, visited)
-
-def BFS(n, computers, com, visited) :
-    visited[com] = True
-    queue = []
-    queue.append(com)
-    while len(queue) != 0 :
-        com = queue.pop(0)
-        visited[com] = True
-        for connect in range(n) :
-            if connect != com and computers[com][connect] == 1:
-                if visited[connect] == False :
-                    queue.append(connect)
+#
+# def solution(n, computers):
+#     answer = 0
+#     visited = [False for _ in range(n)]
+#     for com in range(n) :
+#         if visited[com] == False :
+#             DFS(n, computers, com, visited)
+#             # BFS(n, computers, com, visited)
+#             answer += 1
+#     return answer
+#
+# def DFS(n, computers, com, visited) :
+#     visited[com] = True
+#     for connect in range(n) :
+#         if connect != com and computers[com][connect] == 1 : # 연결된 컴퓨터
+#             if visited[connect] == False :
+#                 DFS(n, computers, connect, visited)
+#
+# def BFS(n, computers, com, visited) :
+#     visited[com] = True
+#     queue = []
+#     queue.append(com)
+#     while len(queue) != 0 :
+#         com = queue.pop(0)
+#         visited[com] = True
+#         for connect in range(n) :
+#             if connect != com and computers[com][connect] == 1:
+#                 if visited[connect] == False :
+#                     queue.append(connect)
