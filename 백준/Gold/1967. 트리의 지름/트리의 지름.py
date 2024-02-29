4eef655569f61388a5e089f4aa0e8c6cda8767a5
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-02-29 15:32:39 +0900

    [Gold IV] Title: 트리의 지름, Time: 152 ms, Memory: 112616 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,29 +1,26 @@
-import sys
-sys.setrecursionlimit(10**4)
-
 n = int(input())
-arr = [[] for _ in range(n + 1)]
-
-# p, c, w = 부모 노드, 자식 노드, 가중치
-for _ in range(1, n) :  
+graph = [[] for _ in range(n + 1)]
+for _ in range(1, n) :
     p, c, w = map(int, input().split())
-    arr[p].append((c, w))
-    arr[c].append((p, w))
+    graph[p].append((c, w))
+    graph[c].append((p, w))
 
-lst = [-1] * (n + 1)
-lst[1] = 0
+def dfs(n, e, flag) :
+    distance = [-1] * (n + 1)
+    stack = [(e, 0)]
+    while stack :
+        e, d = stack.pop()
+        distance[e] = d
+        for node, weight in graph[e] :
+            if distance[node] == -1 :
+                stack.append((node, d + weight))
 
-def dfs(cv, cw) :
-    for nv, nw in arr[cv] :
-        if lst[nv] == -1 :
-            lst[nv] = cw + nw
-            dfs(nv, cw + nw)
+    if flag :
+        x = 0
+        for i in range(1, n + 1) :
+            if distance[x] < distance[i] :
+                x = i
+        return x
+    return max(distance)
 
-dfs(1, 0)
-ans = lst.index(max(lst))
-lst = [-1] * (n + 1)
-lst[ans] = 0
-
-dfs(ans, 0)
-
-print(max(lst))
+print(dfs(n, dfs(n, 1, True), False))
