27377cb6ee18c3e6bca27206f6d91c2461db2407
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-01 00:22:53 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.50 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -10,3 +10,12 @@ def solution(left, right):
         else :
             answer -= i
     return answer
+
+# def solution(left, right):
+#     answer = 0
+#     for i in range(left,right+1):
+#         if int(i**0.5)==i**0.5:
+#             answer -= i
+#         else:
+#             answer += i
+#     return answer
