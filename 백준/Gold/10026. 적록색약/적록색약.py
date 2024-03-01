1fea9d5fef4ab480756d97e41b5fc4160c9db9b1
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-02 02:36:40 +0900

    [Gold V] Title: 적록색약, Time: 180 ms, Memory: 114988 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -36,3 +36,47 @@ for i in range(n) :
             b_cnt += 1
 
 print(n_cnt, b_cnt)
+'''
+sys.setrecursionlimit(10 ** 4)
+n = int(input())
+arr = [list(input().rstrip()) for _ in range(n)]
+v = [[0] * n for _ in range(n)]
+
+n_cnt = b_cnt = 0
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def dfs(x, y) :
+    v[x][y] = 1
+    cur = arr[x][y]
+
+    for k in range(4) :
+        nx = x + dx[k]
+        ny = y + dy[k]
+
+        if 0 <= nx < n and 0 <= ny < n and arr[nx][ny] == cur and v[nx][ny] == 0 :
+            dfs(nx, ny)
+
+for i in range(n) :
+    for j in range(n) :
+        if v[i][j] == 0 :
+            dfs(i, j)
+            n_cnt += 1
+
+# 적록색약
+for i in range(n) :
+    for j in range(n) :
+        if arr[i][j] == 'R' :
+            arr[i][j] = 'G'
+
+v = [[0] * n for _ in range(n)]
+
+for i in range(n) :
+    for j in range(n) :
+        if v[i][j] == 0 :
+            dfs(i, j)
+            b_cnt += 1
+
+print(n_cnt, b_cnt)
+'''
