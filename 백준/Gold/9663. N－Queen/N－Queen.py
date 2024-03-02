cad25e5e6d33252da3a0966a7ffa527915fbc79a
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-02 09:54:53 +0900

    [Gold IV] Title: N-Queen, Time: 6540 ms, Memory: 120284 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,11 +1,11 @@
 def dfs(n) :
     global ans
-    if n == N :     
+    if n == N :
         ans += 1
         return
-
+    
     for j in range(N) :
-        if v1[j] == v2[n + j] == v3[n - j] == 0 :   
+        if v1[j] == v2[n + j] == v3[n - j] == 0 :
             v1[j] = v2[n + j] = v3[n - j] = 1
             dfs(n + 1)
             v1[j] = v2[n + j] = v3[n - j] = 0
