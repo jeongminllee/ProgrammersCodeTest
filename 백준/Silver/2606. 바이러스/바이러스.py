45bbfbcf9eb167b3ce5802f54a0680043ee82ad7
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-04 12:50:16 +0900

    [Silver III] Title: 바이러스, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,22 +1,21 @@
 def dfs(c) :
-    global ans
-    ans += 1
+    global cnt
+    cnt += 1
     v[c] = 1
 
     for n in adj[c] :
         if not v[n] :
             dfs(n)
 
-N = int(input())
-M = int(input())
-
+N = int(input())    # 컴퓨터의 수
+pairs = int(input())# 컴퓨터 쌍의 수
 adj = [[] for _ in range(N + 1)]
-for _ in range(M) :
+for _ in range(pairs) :
     s, e = map(int, input().split())
     adj[s].append(e)
     adj[e].append(s)
 
-ans = 0
+cnt = 0
 v = [0] * (N + 1)
 dfs(1)
-print(ans - 1)
+print(cnt - 1)
