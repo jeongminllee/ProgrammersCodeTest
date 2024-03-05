80b7c7fa0d21b42a0c104056591b553538e36900
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-05 19:06:18 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,18 +1,42 @@
-answer = 0
-
-def dfs(k, cnt, dungeons, v) :
-    global answer
-    answer = max(cnt, answer)
-
-    for i in range(len(dungeons)) :
-        if not v[i] and k >= dungeons[i][0] :
-            v[i] = 1
-            dfs(k - dungeons[i][1], cnt + 1, dungeons, v)
-            v[i] = 0
+from itertools import permutations
 
 def solution(k, dungeons) :
-    global answer
-    v = [0] * len(dungeons)
-    dfs(k, 0, dungeons, v)
-    return answer
-    
+    mx = 0
+    for i in permutations(dungeons) :
+        fatigue = k
+        cnt = 0
+        for dungeon in i :
+            if fatigue >= dungeon[0] :
+                fatigue -= dungeon[1]
+                cnt += 1
+
+        mx = max(cnt, mx)
+    return mx
+
+# def solution(k, dungeons) :
+#     mx = 0
+#     n = len(dungeons)
+#     v = [0] * n
+#     order = [0] * n
+
+#     def dfs(d) :
+#         nonlocal mx
+#         if d == n :
+#             fatigue = k
+#             cnt = 0
+#             for i in order :
+#                 if fatigue >= dungeons[i][0] :
+#                     fatigue -= dungeons[i][1]
+#                     cnt += 1
+#             mx = max(cnt, mx)
+
+#         else :
+#             for i in range(n) :
+#                 if not v[i] :
+#                     v[i] = 1
+#                     order[d] = i
+#                     dfs(d + 1)
+#                     v[i] = 0
+
+#     dfs(0)
+#     return mx
