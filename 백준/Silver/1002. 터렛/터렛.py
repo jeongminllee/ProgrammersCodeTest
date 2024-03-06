d2d52a8228c53d2bd324b4a674d620304ae584a4
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-06 20:41:37 +0900

    [Silver III] Title: 터렛, Time: 120 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.py
+++ 백준/Silver/1002. 터렛/터렛.py
@@ -1,17 +1,14 @@
 T = int(input())
 
 for _ in range(T) :
-    x1,y1,r1,x2,y2,r2 = map(int, input().split())
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    d = (x1 - x2) ** 2 + (y1 - y2) ** 2
     if x1 == x2 and y1 == y2 and r1 == r2 :
         print(-1)
-    elif x1 == x2 and y1 == y2 and r1 != r2 :
-        print(0)
     else :
-        if ((((x1 - x2) ** 2) + ((y1 - y2) ** 2)) == (r1 - r2) ** 2 or
-            ((x1 - x2) ** 2) + ((y1 - y2) ** 2) == (r1 + r2) ** 2):
-            print(1)
-        elif (r1 - r2) ** 2 < (x1 - x2) ** 2 + (y1 - y2) ** 2 < (r1 + r2) ** 2 :
+        if  (r1 - r2) ** 2 < d < (r1 + r2) ** 2:
             print(2)
-        elif ((x1 - x2) ** 2 + (y1 - y2) ** 2 > (r1 + r2) ** 2 or
-            (x1 - x2) ** 2 + (y1 - y2) ** 2 < (r1 - r2) ** 2):
+        elif (r1 - r2) ** 2 == d  or d == (r1 + r2) ** 2 :
+            print(1)
+        else :
             print(0)
