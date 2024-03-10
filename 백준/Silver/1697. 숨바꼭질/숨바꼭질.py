6d20ea86e3c255d3dab0e680bdf02b3a6c526afa
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-10 11:34:35 +0900

    [Silver I] Title: 숨바꼭질, Time: 156 ms, Memory: 117332 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,23 +1,23 @@
+from collections import deque
+
 def bfs(s, e) :
-    # 1. q 생성, v[] 생성
-    q = []
+    q = deque()
     v = [0] * 200001
 
-    # 2. 초기 데이터 삽입, v[] 초기화
     q.append(s)
     v[s] = 1
 
     while q :
-        c = q.pop(0)
+        c = q.popleft()
         if c == e :
             return v[e] - 1
-        # 3방향, 범위내 (0~200000), 미방문
+
         for n in (c - 1, c + 1, c * 2) :
             if 0 <= n <= 200000 and v[n] == 0 :
                 q.append(n)
                 v[n] = v[c] + 1
 
-    return - 1
+    return -1
 
 n, k = map(int, input().split())
 print(bfs(n, k))
