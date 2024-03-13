b98d0bb6ffe8bb14cc6cb57d091cba2f009dcfef
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-13 20:16:09 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 456 ms, Memory: 142104 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -1,37 +1,24 @@
-import sys
-sys.setrecursionlimit(10 ** 5)
-from collections import defaultdict
+def dfs(s) :
+    stack = []
+    stack.append(s)
 
-def dfs(graph, start, visited, order, counter):
-    visited[start] = True
-    order[start] = counter[0]
-    counter[0] += 1
+    cnt = 1
+    while stack :
+        node = stack.pop()
+        if not visited[node] :
+            visited[node] = cnt
+            cnt += 1
+            stack += sorted(graph[node], reverse=True)
+            
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+visited = [0] * (N + 1)
+for _ in range(M) :
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-    for next_node in sorted(graph[start]):
-        if not visited[next_node]:
-            dfs(graph, next_node, visited, order, counter)
+dfs(R)
 
-def main():
-    N, M, R = map(int, input().split())
-    graph = defaultdict(list)
-
-    # 그래프 구성
-    for _ in range(M):
-        u, v = map(int, input().split())
-        graph[u].append(v)
-        graph[v].append(u)
-
-    visited = [False] * (N + 1)
-    order = [0] * (N + 1)
-    counter = [1]  # 방문 순서를 기록하기 위한 카운터, 리스트 형태로 선언하여 참조 전달을 사용
-
-    # 깊이 우선 탐색 시작
-    dfs(graph, R, visited, order, counter)
-
-    # 방문 순서 출력
-    for i in range(1, N + 1):
-        print(order[i])
-
-
-if __name__ == "__main__":
-    main()
+for n in range(1, N + 1) :
+    print(visited[n])
