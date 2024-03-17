b5e3c7d3e199e1680e9c932b2aa4f382ee8f6053
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-17 16:12:46 +0900

    [Silver IV] Title: ATM, Time: 120 ms, Memory: 110404 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,8 +1,8 @@
-N = int(input())
-arr = list(map(int, input().split()))
-arr = sorted(arr)
-lst = [0] * N
-answer = 0
-for i in range(len(arr)) :
-    lst[i] = sum(arr[:i + 1])
-print(sum(lst))
+n = int(input())
+lst = list(map(int, input().split()))
+ans = [0] * n
+lst.sort()
+
+for i in range(n) :
+    ans[i] = sum(lst[:i + 1])
+print(sum(ans))
