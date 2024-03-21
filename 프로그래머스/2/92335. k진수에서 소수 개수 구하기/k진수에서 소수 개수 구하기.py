024fc385efc0af68338316aa209373396186fd1e
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-21 20:34:39 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,3 +1,29 @@
-def solution(n, k):
-    answer = -1
-    return answer
+def solution(n, k) :
+    word = ''
+    # n을 k진수로 변환
+    while n :
+        word = str(n % k) + word
+        n //= k
+
+    # k진수로 변환된 문자열에서 '0'을 기준으로 나누어 리스트를 만듦
+    word = word.split('0')
+
+    # 리스트의 각 요소에 대해 소수인지 아닌지 검사
+    cnt = 0
+    for w in word :
+        if len(w) == 0 :
+            continue
+        if int(w) < 2 :
+            continue
+
+    # 소수 검사
+    # 2부터 해당 숫자의 제곱근까지 나눠보며 나누어 떨어지는 수가 있는지 확인
+        prinum = True
+        for i in range(2, int(int(w) ** 0.5) + 1) :
+            if int(w) % i == 0 :
+                prinum = False
+                break
+    # 나누어 떨어지는 수가 없다면 소수로 판단하고 cnt += 1
+        if prinum :
+            cnt += 1
+    return cnt
