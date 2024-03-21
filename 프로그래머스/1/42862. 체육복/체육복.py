f0abe6ac126d44477a2de07c0683d22ab8b0d7c5
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-21 16:02:26 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -3,34 +3,18 @@ def solution(n, lost, reserve):
     lost = set(lost)
     reserve = set(reserve)
     
-    reserve_and_lost = lost & reserve
-    lost -= reserve_and_lost
-    reserve -= reserve_and_lost
+    # 여분이 있는데 도난 당한 경우
+    lost_and_reserve = lost & reserve
+    lost -= lost_and_reserve
+    reserve -= lost_and_reserve
     
+    # 잃어버린 학생한테 빌려줌.
     for i in reserve :
-        if i - 1 in lost :
-            lost.remove(i - 1)
+        if i - 1 in lost : 
+            lost.remove(i - 1) 
         elif i + 1 in lost :
             lost.remove(i + 1)
             
+    # 그럼에도 없는 학생들 수업에서 제외
     answer -= len(lost)
     return answer
-
-# def solution(n, lost, reserve):
-#     lost_set = set(lost)
-#     reserve_set = set(reserve)
-#     answer = n
-
-#     #여분이 있는데 도난당한 경우
-#     reserve_and_lost = lost_set & reserve_set
-#     lost_set -= reserve_and_lost
-#     reserve_set -= reserve_and_lost
-
-#     for x in reserve_set:
-#         if x-1 in lost_set:
-#             lost_set.remove(x-1)
-#         elif x+1 in lost_set:
-#             lost_set.remove(x+1)
-
-#     answer -= len(lost_set)
-#     return answer
