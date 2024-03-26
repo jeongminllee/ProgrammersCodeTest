d5c7f8e7631b1cdd765d0aa930f69843a934cc1c
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-27 00:22:35 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 4828 ms, Memory: 129552 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -9,4 +9,4 @@ for a in range(1, N + 1) :
 for _ in range(M) :
     x1, y1, x2, y2 = map(int, input().split())
     ans = sm[x2][y2] - sm[x1 - 1][y2] - sm[x2][y1 - 1] + sm[x1 - 1][y1 - 1]
-    print(ans)
+    print(str(ans))
