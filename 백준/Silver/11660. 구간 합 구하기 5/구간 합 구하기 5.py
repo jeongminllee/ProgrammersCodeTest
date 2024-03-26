41bc0987328d0cb72d2f3570b7627afa2469e155
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-27 00:05:42 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 4952 ms, Memory: 129552 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+sm = [[0] * (N + 1) for _ in range(N + 1)]
+for a in range(1, N + 1) :
+    for b in range(1, N + 1):
+        sm[a][b] = sm[a - 1][b] + sm[a][b - 1] - sm[a - 1][b - 1] + arr[a - 1][b - 1]
+
+for _ in range(M) :
+    x1, y1, x2, y2 = map(int, input().split())
+    ans = sm[x2][y2] - sm[x1 - 1][y2] - sm[x2][y1 - 1] + sm[x1 - 1][y1 - 1]
+    print(ans)
