7b958221916be07778d58df765fae16cc4bf224b
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-30 21:41:44 +0900

    [level 2] Title: 삼각 달팽이, Time: 1614.95 ms, Memory: 44.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -0,0 +1,25 @@
+def solution(n):
+    answer = [[0 for j in range(1, i + 1)] for i in range(1, n + 1)]    # 삼각형 구조 만들기
+
+    x, y = -1, 0    # 좌표 초기화 => 처음 시작은 아래로 내려가기 때문에 x = -1
+    num = 1
+
+    for i in range(n) : # 방향
+        for j in range(i, n) :  # 좌표 구하기
+            if i % 3 == 0 : # 하
+                x +=1
+            elif i % 3 == 1 :   # 우
+                y += 1
+            else :  # 상
+                x -= 1
+                y -= 1
+            answer[x][y] = num
+            num += 1
+
+    return sum(answer, [])
+
+# answer = 삼각형 배열
+# x, y = 좌표 값
+# num = 대입 숫자
+# i = 방향값
+# j = i방향에 대한 대입 횟수
