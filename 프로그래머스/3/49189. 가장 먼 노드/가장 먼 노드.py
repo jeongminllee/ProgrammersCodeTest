403c484758d460b45e51d208e281b3c7128a03b2
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-30 22:53:07 +0900

    [level 3] Title: 가장 먼 노드, Time: 7252.51 ms, Memory: 20 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,6 +1,20 @@
 # 1번 노드가 루트 노드 => 에서 가장 먼 노드를 찾아라
 from collections import deque
 
+def bfs(graph, start, distance) :
+    q = deque([1])  # BFS를 위한 queue, 출발 노드 = 1
+    distance[1] = 0  # 출발 노드의 최단 거리를 0으로
+
+    # BFS 수행
+    while q:
+        curr = q.popleft()  # 현재 노드
+
+        # 현재 노드에서 이동할 수 있는 모든 노드 확인
+        for i in graph[curr]:
+            if distance[i] == -1:  # 아직 방문하지 않은 노드라면,
+                q.append(i)  # q에 추가
+                distance[i] = distance[curr] + 1  # 최단 거리 갱신
+
 def solution(n, edge):
     answer = 0
 
@@ -14,18 +28,7 @@ def solution(n, edge):
         graph[e[0]].append(e[1])
         graph[e[1]].append(e[0])    # 양방향
 
-    q = deque([1])  # BFS를 위한 queue, 출발 노드 = 1
-    distance[1] = 0 # 출발 노드의 최단 거리를 0으로
-
-    # BFS 수행
-    while q :
-        curr = q.popleft()  # 현재 노드
-
-        # 현재 노드에서 이동할 수 있는 모든 노드 확인
-        for i in graph[curr] :
-            if distance[i] == -1 :  # 아직 방문하지 않은 노드라면,
-                q.append(i) # q에 추가
-                distance[i] = distance[curr] + 1    # 최단 거리 갱신
+    bfs(graph, 1, distance)
 
     # 가장 멀리 떨어진 노드 개수 구하기
     for d in distance :
