416834b62f2212488fc487b7002b21a8534e4619
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-03-31 12:57:45 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,11 +1,11 @@
 def solution(num):
     answer = 0
     while num != 1 :
-        if num % 2 == 0 :
+        if num % 2 == 0 :   # 짝수라면 2로 나눔
             num //= 2
-        else :
+        else :              # 홀수라면 3 * n + 1
             num = 3 * num + 1
-        answer += 1
-        if answer >= 500 :
-            return -1
-    return answer
+        answer += 1         # 횟수 추가
+        if answer >= 500 :  # 횟수가 500번 넘으면
+            return -1       # -1 리턴
+    return answer           # 500번 넘지 않으면 횟수 리턴
