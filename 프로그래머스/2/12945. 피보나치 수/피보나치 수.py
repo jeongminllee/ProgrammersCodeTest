12bbd5fbab832cb0faf67d2ed2469b010efbde9b
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-04-02 16:00:51 +0900

    [level 2] Title: 피보나치 수, Time: 492.55 ms, Memory: 439 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,8 +1,18 @@
 def solution(n):
     if n <= 1 :
         return n
-    a, b = 0, 1
-    for _ in range(n) :
-        a, b = b, (a + b)
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    for i in range(2, n + 1) :
+        dp[i] = (dp[i - 1] + dp[i - 2])
+
+    return dp[n]%1234567
+
+# def solution(n):
+#     if n <= 1 :
+#         return n
+#     a, b = 0, 1
+#     for _ in range(n) :
+#         a, b = b, (a + b)
         
-    return a % 1234567
+#     return a % 1234567
