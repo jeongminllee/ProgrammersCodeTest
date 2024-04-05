e9de3e702d42d840ca7d9ebba8eb9365efa5a048
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-04-05 20:07:55 +0900

    [Silver V] Title: CD, Time: 2360 ms, Memory: 152456 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.py
+++ 백준/Silver/4158. CD/CD.py
@@ -2,14 +2,19 @@ while 1 :
     N, M = map(int, input().split())
     if N == 0 and M == 0 :
         break
-    dic = {}
+    arr1 = [int(input()) for _ in range(N)]
+    arr2 = [int(input()) for _ in range(M)]
     cnt = 0
-    for _ in range(N) :
-        cd = int(input())
-        dic[cd] = 1
+    for cd in arr2 :
+        s, e = 0, N - 1
+        while s <= e :
+            mid = (s + e) // 2
 
-    for _ in range(M) :
-        cd = int(input())
-        if cd in dic :
-            cnt += 1
+            if arr1[mid] == cd :
+                cnt += 1
+                break
+            elif arr1[mid] > cd :
+                e = mid - 1
+            elif arr1[mid] < cd :
+                s = mid + 1
     print(cnt)
