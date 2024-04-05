8d5f5748b274ad3b43ff8702b761a9e9ac9d89c3
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-04-05 20:12:39 +0900

    [Silver V] Title: CD, Time: 2348 ms, Memory: 152440 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.py
+++ 백준/Silver/4158. CD/CD.py
@@ -18,3 +18,21 @@ while 1 :
             elif arr1[mid] < cd :
                 s = mid + 1
     print(cnt)
+    
+''' dictionary 
+while 1 :
+    N, M = map(int, input().split())
+    if N == 0 and M == 0 :
+        break
+    dic = {}
+    cnt = 0
+    for _ in range(N) :
+        cd = int(input())
+        dic[cd] = 1
+
+    for _ in range(M) :
+        cd = int(input())
+        if cd in dic :
+            cnt += 1
+    print(cnt)
+'''
