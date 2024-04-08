509e87d342e011842c6151402de7116a4dbb9f88
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-04-08 17:43:57 +0900

    [Silver III] Title: 합 구하기, Time: 192 ms, Memory: 122480 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.py
+++ 백준/Silver/11441. 합 구하기/합 구하기.py
@@ -2,8 +2,13 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-lst = list(map(int, input().split()))
+lst = [0] + list(map(int, input().split()))
 M = int(input())
-for m in range(M) :
+sumlst = [0] * (N + 1)
+
+for i in range(1, N+1) :
+    sumlst[i] = sumlst[i-1] + lst[i]
+
+for _ in range(M) :
     i, j = map(int, input().split())
-    print(sum(lst[i-1:j]))
+    print(sumlst[j] - sumlst[i-1])
