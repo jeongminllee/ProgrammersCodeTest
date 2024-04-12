812015b4aabcab8ecd61e98a74cf7fcda400a488
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-04-12 20:26:23 +0900

    [level 2] Title: 롤케이크 자르기, Time: 648.07 ms, Memory: 51.2 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,17 +1,21 @@
 from collections import Counter
 
+# topping 배열을 입력으로 받아서, 피자를 나눌 수 있는 경우의 수를 반환하는 함수
 def solution(topping):
-    split_cnt = 0
-    topping_cnt = Counter(topping)
-    half_topping_set = set()
-    for t in topping :
-        half_topping_set.add(t)
-        topping_cnt[t] -= 1
-
-        if topping_cnt[t] == 0 :
-            topping_cnt.pop(t)
-
-        if len(half_topping_set) == len(topping_cnt) :
-            split_cnt += 1
+    split_cnt = 0  # 피자를 나눌 수 있는 경우의 수를 저장할 변수
+    topping_cnt = Counter(topping)  # 각 토핑의 개수를 계산하여 저장
+    half_topping_set = set()  # 현재까지 고려된 토핑의 종류를 저장할 집합
     
-    return split_cnt
+    # 피자 토핑 배열을 순회
+    for t in topping:
+        half_topping_set.add(t)  # 현재 토핑을 집합에 추가
+        topping_cnt[t] -= 1  # 현재 토핑의 개수를 하나 줄임
+
+        if topping_cnt[t] == 0:  # 만약 현재 토핑의 개수가 0이 되면,
+            topping_cnt.pop(t)  # 해당 토핑을 카운터에서 제거
+
+        # 왼쪽 부분(현재까지 고려된 토핑의 종류)과 오른쪽 부분(나머지 토핑의 종류)의 토핑 종류 수가 같다면
+        if len(half_topping_set) == len(topping_cnt):
+            split_cnt += 1  # 나눌 수 있는 경우의 수를 하나 증가시킴
+            
+    return split_cnt  # 나눌 수 있는 경우의 수 반환
