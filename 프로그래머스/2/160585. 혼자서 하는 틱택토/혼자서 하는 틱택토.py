f3371c6239fb7dcfbf22febd344e86e4e163c419
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-04-13 21:52:58 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
@@ -1,41 +1,40 @@
-def check(board, t) :
+def check(board, t):
     win_conditions = []
     # 가로 승리 조건
-    for i in range(3) :
+    for i in range(3):
         win_conditions.append([(i, j) for j in range(3)])
     # 세로 승리 조건
-    for j in range(3) :
+    for j in range(3):
         win_conditions.append([(i, j) for i in range(3)])
-    # 대각 승리 조건
-    win_conditions.append([(i,i) for i in range(3)])
-    win_conditions.append([(i,2-i) for i in range(3)])
-    
-    # 
-    for condition in win_conditions :
-        if all(board[x][y] == t for x, y in condition) :
+    # 대각선 승리 조건
+    win_conditions.append([(i, i) for i in range(3)])
+    win_conditions.append([(i, 2 - i) for i in range(3)])
+
+    # 승리 조건 충족 여부 확인
+    for condition in win_conditions:
+        if all(board[x][y] == t for x, y in condition):
             return True
     return False
 
 
 def solution(board):
-    # 문자열 배열 => 리스트
-    board = [list(row) for row in board]
-    ocnt = sum(row.count("O") for row in board)
-    xcnt = sum(row.count("X") for row in board)
-    
-    # "O" "X" 개수 확인
-    if not (ocnt == xcnt or ocnt == xcnt + 1) :
+    board = [list(row) for row in board]  # 문자열 배열을 리스트로 변환
+    ocnt = sum(row.count("O") for row in board)  # "O"의 개수
+    xcnt = sum(row.count("X") for row in board)  # "X"의 개수
+
+    # "O"와 "X"의 개수가 규칙에 부합하는지 확인
+    if not (ocnt == xcnt or ocnt == xcnt + 1):
         return 0
-    
-    owin = check(board, "O")
-    xwin = check(board, "X")
-    
-    # 승리 조건 확인
-    if owin and xwin :  # 동시 승리 불가능
+
+    owin = check(board, "O")  # "O"의 승리 조건 충족 여부
+    xwin = check(board, "X")  # "X"의 승리 조건 충족 여부
+
+    # 승리 조건 검증
+    if owin and xwin:  # 두 플레이어가 동시에 승리하는 것은 불가능
         return 0
-    if owin and ocnt != xcnt + 1:  # O가 이겼을 떄
+    if owin and ocnt != xcnt + 1:  # "O"가 이겼다면, "O"의 개수는 "X"의 개수보다 하나 더 많아야 함
         return 0
-    if xwin and ocnt != xcnt :  # X가 이겼을때
+    if xwin and ocnt != xcnt:  # "X"가 이겼다면, "O"와 "X"의 개수는 동일해야 함
         return 0
-    
-    return 1
+
+    return 1  # 위의 모든 조건을 만족하지 않으면 규칙에 부합하는 게임판 상태로 간주
