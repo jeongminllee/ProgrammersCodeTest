cfd5ca0df86027517f1dbe075e26d8a7f21ca530
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-04-18 23:38:25 +0900

    [level 3] Title: 고고학 최고의 발견, Time: 8989.51 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/131702. 고고학 최고의 발견/고고학 최고의 발견.py
+++ 프로그래머스/3/131702. 고고학 최고의 발견/고고학 최고의 발견.py
@@ -1,36 +1,45 @@
+import copy
+
+# 상하좌우 및 현재 위치에 대한 변화량
+dx = [0, -1, 1, 0, 0]
+dy = [0, 0, 0, 1, -1]
+
+# 특정 위치의 시계와 인접한 시계를 회전시키는 함수
+def rotate(x, y, lst, rt):
+    n = len(lst)  # 시계 배열의 크기
+    for k in range(5):  # 현재 위치 및 상하좌우 위치에 대해 반복
+        nx, ny = x + dx[k], y + dy[k]
+        if 0 <= nx < n and 0 <= ny < n:  # 유효한 위치인지 확인
+            lst[ny][nx] = (lst[ny][nx] + rt) % 4  # 시계를 rt만큼 회전
+
+# 최소 회전 횟수를 찾는 함수
 def solution(clockHands):
-    answer = float('inf')
+    answer = float('inf')  # 최소 회전 횟수를 저장할 변수, 초기값은 무한대
+    n = len(clockHands)  # 시계 배열의 크기
 
-    # 회전을 구현한 함수
-    def plus(r, c, num):
-        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1), (0, 0)]:
-            nr, nc = r + dr, c + dc
-            if 0 <= nr < n and 0 <= nc < n:
-                newclockHands[nr][nc] = (newclockHands[nr][nc] + num) % 4
-        return
+    # 첫 번째 열에 대해 가능한 모든 회전 조합을 시도
+    for i in range(4 ** n):
+        tmp = 0  # 현재 조합에 대한 회전 횟수
+        tmp_clock = copy.deepcopy(clockHands)  # 시계 배열의 복사본 생성
+        for j in range(n):  # 첫 번째 열에 대해 반복
+            rt = i % 4 ** (j + 1) // 4 ** j  # j번째 시계의 회전 횟수 계산
+            if rt == 0:
+                continue
 
-    n = len(clockHands)
-    for i in range(1 << (2 * n)):
-        newclockHands = [clockHands[i][:] for i in range(n)]
-        cnt = 0
-        for c in range(n):
-            a = '1' if i & (1 << 2 * c) else '0'
-            b = '1' if i & (1 << 2 * c + 1) else '0'
-            plus(0, c, int(b + a, base=2))
-            cnt += int(b + a, base=2)
+            rotate(j, 0, tmp_clock, rt)  # j번째 시계와 인접한 시계들 회전
+            tmp += rt  # 회전 횟수 추가
 
-        for line in range(1, n):
-            if cnt >= answer:
-                break
+        # 두 번째 열부터 마지막 열까지 현재 조건을 만족하는지 확인하며 회전
+        for y in range(1, n):
+            for x in range(n):
+                if tmp_clock[y - 1][x] == 0:  # 이전 열의 시계가 12시를 가리키면 패스
+                    continue
+                rt = 4 - tmp_clock[y - 1][x]  # 필요한 회전 횟수 계산
+                rotate(x, y, tmp_clock, rt)  # 회전
+                tmp += rt  # 회전 횟수 추가
 
-            for c in range(n):
-                num = newclockHands[line - 1][c]
-                if num:
-                    plus(line, c, 4 - num)
-                    cnt += 4 - num
+        # 마지막 행이 모두 12시를 가리키는지 확인
+        if sum(tmp_clock[-1]) == 0:
+            answer = min(answer, tmp)  # 최소 회전 횟수 업데이트
 
-        if newclockHands[-1] == ([0] * n):
-            if cnt < answer:
-                answer = cnt
-
-    return answer
+    return answer  # 최소 회전 횟수 반환
