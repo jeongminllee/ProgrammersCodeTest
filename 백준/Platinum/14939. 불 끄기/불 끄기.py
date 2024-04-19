f1e02855379018d6263cadfe71be3027aed6ef49
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-04-19 17:08:04 +0900

    [Platinum IV] Title: 불 끄기, Time: 240 ms, Memory: 114680 KB -BaekjoonHub

--- 백준/Platinum/14939. 불 끄기/불 끄기.py
+++ 백준/Platinum/14939. 불 끄기/불 끄기.py
@@ -1,41 +1,38 @@
 import copy
+
 dx = [0, -1, 1, 0, 0]
 dy = [0, 0, 0, -1, 1]
 
-arr = [list(input().rstrip()) for _ in range(10)]
-v = [[0] * 10 for _ in range(10)]
+def isValid(arr, x, y) :
+    for d in range(5) :
+        nx, ny = x + dx[d], y + dy[d]
+        if 0 <= nx < 10 and 0 <= ny < 10 :
+            arr[nx][ny] = not arr[nx][ny]
 
-ans = float('inf')
-
-for i in range(10) :
-    for j in range(10) :
-        if arr[i][j] == "O" :
-            v[i][j] = 1
-
-# 첫 번째 줄의 모든 경우의 수
-for ck in range(1 << 10) :
-    visitied = copy.deepcopy(v)
-    cnt = 0
-    # 첫 줄에 10개 전구 하나씩 탐색
-    for k in range(10) :
-        # j번째 스위치를 누른다면 +1
-        if ck & (1 << k) :
-            cnt += 1
-            # 맨 윗줄 전구 살피기
-            for d in range(5) :
-                nx, ny = 0 + dx[d], k + dy[d]
-                if 0 <= nx < 10 and 0 <= ny < 10 :
-                    visitied[nx][ny] = not visitied[nx][ny]
-
-    for x in range(1, 10) :
-        for y in range(10) :
-            if visitied[x-1][y] == 1 :
+def solve(arr) :
+    ans = float('inf')
+    for first_row in range(1<<10) :   # 10개 전구에 대한 모든 조작 경우의 수
+        cnt = 0
+        narr = [copy.deepcopy(row) for row in arr]  # arr 상태 복사
+        for i in range(10) :
+            if first_row & (1 << i) :   # 첫 줄의 i번째 전구를 조작하는 경우
+                isValid(narr, 0, i)
                 cnt += 1
-                for d in range(5) :
-                    nx, ny = x + dx[d], y + dy[d]
-                    if 0 <= nx < 10 and 0 <= ny < 10 :
-                        visitied[nx][ny] = not visitied[nx][ny]
 
-    if all(c == 0 for c in visitied[-1]) :
-        ans = min(ans, cnt)
-print(ans if ans < 101 else -1)
+        for i in range(1, 10) : # 나머지 줄 처리
+            for j in range(10) :
+                if narr[i-1][j] :   # 위 전구가 켜져 있으면 해당 위치의 전구 조작
+                    isValid(narr, i, j)
+                    cnt += 1
+        
+        if all(not val for row in narr for val in row) :    # 모든 전구가 꺼져 있는지 확인
+            ans = min(ans, cnt)
+
+    return ans if ans != float('inf') else -1
+
+arr = []
+for _ in range(10) :
+    row = list(map(lambda x: x == 'O', input().strip()))
+    arr.append(row)
+
+print(solve(arr))
