4e84f3ba20d7a4006ab46978b8e12c8cf98c5e07
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-04-21 14:43:36 +0900

    [Silver II] Title: 최대 힙, Time: 192 ms, Memory: 113484 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,14 +1,56 @@
 import sys
-import heapq
 
-heap = []
-n = int(sys.stdin.readline())
-for _ in range(n):
-  m = int(sys.stdin.readline())
-  if m == 0:
-    if len(heap) == 0:
-      print(0)
-    else:
-      print((-1)*heapq.heappop(heap))
+PIN = sys.stdin.readline
+POUT = sys.stdout.write
+
+class heap:
+  def __init__(self):
+    self.array = []
+    self.size = 0
+
+  def top(self) -> int:
+    return self.array[0]
+
+  def push(self, data : int) -> None:
+    self.array.append(data)
+    idx = self.size
+    self.size += 1
+    # up heap
+    while (idx > 0 and self.array[idx // 2] < self.array[idx]):
+      self.array[idx // 2] , self.array[idx] = self.array[idx], self.array[idx // 2]
+      idx = idx // 2
+  
+  def pop(self) -> int:
+    if self.size == 0:
+      return 0
+    self.array[0], self.array[self.size - 1] = self.array[self.size - 1], self.array[0]
+    ret : int = self.array[self.size - 1]
+    self.array.pop()
+    self.size -= 1
+    idx : int = 0
+    # down heap
+    while (idx << 1) < self.size:
+      left_child_idx = idx << 1
+      right_child_idx = left_child_idx | 1
+      maximam = idx
+
+      if left_child_idx < self.size and self.array[left_child_idx] > self.array[maximam]:
+        maximam = left_child_idx
+      if right_child_idx < self.size and self.array[right_child_idx] > self.array[maximam]:
+        maximam = right_child_idx
+
+      if maximam != idx:
+        self.array[idx], self.array[maximam] = self.array[maximam], self.array[idx]
+        idx = maximam        
+      else:
+        break
+    return ret
+      
+N = int(PIN().rstrip())
+H = heap()
+for _ in range(N):
+  x = int(PIN().rstrip())
+  if x == 0:
+    POUT(str(H.pop()) + '\n')
   else:
-    heapq.heappush(heap, (-1)*m)
+    H.push(x)
