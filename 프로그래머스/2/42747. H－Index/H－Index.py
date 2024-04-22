815f5a9503365ee5ed4f92dbdebb02b7341e79d6
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-04-22 22:25:24 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,8 +1,5 @@
 def solution(citations):
-    answer = 0
-    n = len(citations)
-    citations.sort(reverse = True)
-    for i in range(n) :
-        if citations[i] >= i + 1 :
-            answer += 1
-    return answer
+    citations.sort(reverse=True) # 인용 횟수를 내림차순으로 정렬
+    # 각 논문에 대해, 인덱스와 인용 횟수 중 더 작은 값을 선택하고, 그 중 최댓값을 H-index로 결정
+    answer = max(map(min, enumerate(citations, start=1)))
+    return answer # 계산된 H-index 반환
