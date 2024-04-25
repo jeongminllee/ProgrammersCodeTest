6bcf78010ac692ba444a0dc37659329aab3b5648
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-04-26 01:14:16 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 22.77 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,17 +1,26 @@
 def solution(cap, n, deliveries, pickups):
-    answer = 0
+    # 배달과 수거 배열을 뒤집음 = 먼 위치부터 배달을 완료 해야 여러번 반복하지 않음
     deliveries.reverse()
     pickups.reverse()
-    
-    cnt_de = 0
-    cnt_pick = 0
-    
+    answer = 0  # 총 이동 거리
+
+    # 현재까지 처리해야 할 배달 및 수거 상자의 수
+    deli_cnt = 0
+    pick_cnt = 0
+
+    # 모든 집을 순회
     for i in range(n) :
-        cnt_de += deliveries[i]
-        cnt_pick += pickups[i]
-        
-        while cnt_de > 0 or cnt_pick > 0 :
-            cnt_de -= cap
-            cnt_pick -= cap
+        # i번째 집에서 배달할 상자의 수와 수거할 상자의 수를 누적
+        deli_cnt += deliveries[i]
+        pick_cnt += pickups[i]
+    
+    # 현재 집에서 처리해야 할 배달 또는 수거가 남아있는 경우
+        while deli_cnt > 0 or pick_cnt > 0 :
+            # 트럭의 용량만큼 배달 또는 수거를 처리
+            # 처리할 수 있는 최대량을 초과하는 경우, 초과분은 다음 순회 때 처리
+            deli_cnt -= cap
+            pick_cnt -= cap
+            # 물류창고에서 현재 위치까지 왕복 거리를 계산하여 총 이동 거리에 더함
             answer += (n - i) * 2
+
     return answer
