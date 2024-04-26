ff44128028fbdc0eb761d4f6a74885fd553053cb
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-04-26 20:30:33 +0900

    [Silver I] Title: 회의실 배정, Time: 420 ms, Memory: 127312 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,13 +1,11 @@
-import sys
-input = sys.stdin.readline
-
 T = int(input())
 arr = [list(map(int, input().split())) for _ in range(T)]
-arr.sort(key=lambda x:(x[1], x[0]))
+arr.sort(key=lambda x:(x[1],x[0]))
 cnt = 0
 last = 0
 for s, e in arr :
     if last <= s :
         cnt += 1
         last = e
+
 print(cnt)
