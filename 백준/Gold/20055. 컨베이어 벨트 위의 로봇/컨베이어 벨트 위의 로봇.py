45620d6f3204283ac9aa46b92e62dc7202f4e5ab
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-05-03 12:53:35 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 224 ms, Memory: 110708 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -2,15 +2,15 @@ N, K = map(int, input().split())
 lst = list(map(int, input().split()))
 robot = [0]*N
 
-ans = 0
+ans = cnt = 0
 while True :
     ans += 1
     # [1] 벨트, 로봇 회전 + [N-1]로봇 내림
-    # lst = [lst[-1]] + lst[:-1]  # 364ms : 느림
+    # lst = [lst[-1]] + lst[:-1]  # 364ms
     # robot = [0] + robot[:-1]
     # robot[N-1] = 0
 
-    lst.insert(0,lst.pop())
+    lst.insert(0,lst.pop()) # 268ms
     robot.pop()
     robot.insert(0,0)
     robot[N-1] = 0
@@ -19,14 +19,23 @@ while True :
     for i in range(N-2, 0, -1) :
         if robot[i] == 1 and robot[i+1]==0 and lst[i+1] > 0 :
             robot[i], robot[i+1] = 0, 1
-            lst[i+1] -= 1
+            lst[i+1] -= 1    # 내구도가 감소해서 0이 되면 cnt += 1
+
+            if lst[i+1] == 0 :
+                cnt += 1
 
     # [3] 0자리 내구도 > 0 이면 로봇 올림
     if lst[0] > 0 :
         robot[0] = 1
         lst[0] -= 1
 
+        if lst[0] == 0:
+            cnt += 1
+
     # [4] 0인 개수 >= K 이면 탈출
-    if lst.count(0) >= K :
+    # if lst.count(0) >= K :      # 268ms 매번 0의 개수를 카운트 : 느리다..
+    #     break
+    if cnt >= K :
         break
+
 print(ans)
