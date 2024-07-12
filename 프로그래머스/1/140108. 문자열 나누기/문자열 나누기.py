4e0b5d7833676a3303ddb6da6d914542717a8ec9
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-07-13 08:38:58 +0900

    [level 1] Title: 문자열 나누기, Time: 3.82 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -0,0 +1,24 @@
+def solution(s):
+    answer = 0
+    
+    while s :
+        x = s[0]
+        x_cnt = 0
+        other_cnt = 0
+        
+        for i, char in enumerate(s) :
+            if char == x :
+                x_cnt += 1
+            else :
+                other_cnt += 1
+                
+            if x_cnt == other_cnt :
+                answer += 1
+                s = s[i+1:]
+                break
+                
+        else :
+            answer += 1
+            break
+            
+    return answer
