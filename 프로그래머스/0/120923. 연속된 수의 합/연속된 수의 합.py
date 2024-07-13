9543eaced34b86d939792fe238a69e4f2b7b14bc
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-07-13 09:43:30 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,7 +1,15 @@
-def solution(num, total):
-    lst = [i for i in range(-101, 101)]
+# def solution(num, total):
+#     d = 0
+#     for i in range(num) :
+#         d += i
+#     start = (total - d) // num
+#     answer = [i for i in range(start, start + num)]
+        
+#     return answer
 
-    for i in lst :
-        answer = lst[i : i + num]
-        if sum(answer) == total :
-            return answer
+def solution(num, total):
+    # 연속된 숫자의 시작값을 계산
+    start = (total - sum(range(num))) // num
+    
+    # 시작값부터 num개의 연속된 숫자를 리스트로 만들어 반환
+    return list(range(start, start + num))
