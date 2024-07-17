ed23cf2c5ed27996e310a8340a58fae2d4ea3956
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-07-17 23:17:15 +0900

    [Silver III] Title: 영식이의 손가락, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1614. 영식이의 손가락/영식이의 손가락.py
+++ 백준/Silver/1614. 영식이의 손가락/영식이의 손가락.py
@@ -6,6 +6,11 @@ lst = [(6, 2), (4, 4), (2, 6)]
 if n == 1 or n == 5 :
     print(start + 8 * m)
 elif n == 2 or n == 3 or n == 4 :
-    for i in range(m) :
-        start += lst[n-2][i%2]
+    if m % 2 == 0 :
+        start += 8 * (m // 2)
+
+    else :
+        start += 8 * (m // 2 + 1) - lst[n-2][1]
+    # for i in range(m) :
+    #     start += lst[n-2][i%2]
     print(start)
