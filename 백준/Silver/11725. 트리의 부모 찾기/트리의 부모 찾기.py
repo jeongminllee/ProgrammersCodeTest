108ba6209d56c8703f9d409c0493df33a5b8c1bb
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-07-18 15:17:17 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 312 ms, Memory: 127808 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,29 +1,27 @@
 from collections import deque
 
-def bfs(s) :
+def find_parents(tree, N, s) :
     q = deque()
     q.append(s)
 
-    visited[s] = 1
-
     while q :
         curr = q.popleft()
-
         for child in tree[curr] :
             if visited[child] == 0 :
                 visited[child] = curr
                 q.append(child)
 
+    return visited[2:]
+
 N = int(input())
 tree = [[] for _ in range(N + 1)]
-for _ in range(N-1) :
+for _ in range(N - 1) :
     a, b = map(int, input().split())
     tree[a].append(b)
     tree[b].append(a)
 
 visited = [0] * (N + 1)
 
-bfs(1)
-parents = visited[2:]
-for i in parents:
+parents = find_parents(tree, N, 1)
+for i in parents :
     print(i)
