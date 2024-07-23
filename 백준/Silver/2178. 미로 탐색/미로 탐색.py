1e7ee64e17c0621f8f6f3834f4e2a9ac717ab379
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-07-23 23:42:49 +0900

    [Silver I] Title: 미로 탐색, Time: 140 ms, Memory: 114156 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,25 +1,24 @@
-def bfs(si, sj, ei, ej) :
-    q = []
-    v = [[0] * M for _ in range(N)]
-
-    q.append((si, sj))
-    v[si][sj] = 1
-
-    while q :
-        ci, cj = q.pop(0)
-        if (ci, cj) == (ei, ej) :
-            return v[ei][ej]
-
-        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)) :
-            ni, nj = ci + di, cj + dj
-            if 0 <= ni < N and 0 <= nj < M and arr[ni][nj] == 1 and v[ni][nj] == 0 :
-                q.append((ni, nj))
-                v[ni][nj] = v[ci][cj] + 1
-
+from collections import deque
 
 N, M = map(int, input().split())
-arr = [list(map(int, input())) for _ in range(N)]
+arr = [list(map(int,input())) for _ in range(N)]
+v = [[0] * M for _ in range(N)]
 
-ans = bfs(0, 0, N - 1, M - 1)
+q = deque()
+q.append((0, 0))
+v[0][0] = 1
 
-print(ans)
+while q :
+    i, j = q.popleft()
+
+    if i == N - 1 and j == M - 1 :
+        break
+
+    for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)) :
+        ni, nj = i + di, j + dj
+
+        if 0 <= ni < N and 0 <= nj < M and arr[ni][nj] == 1 and v[ni][nj] == 0 :
+            v[ni][nj] = v[i][j] + 1
+            q.append((ni, nj))
+
+print(v[-1][-1])
