9fefa30eedbf2a20d4cbe0c1f0607ada5ad3708b
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-07-24 01:28:54 +0900

    [Gold III] Title: 내리막 길, Time: 212 ms, Memory: 144692 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,26 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-m, n = map(int, input().split())
-arr = [list(map(int, input().split())) for i in range(m)]
-dp = [[-1]*n for i in range(m)]
-dx = [0, 1, 0, -1]
-dy = [1, 0 , -1, 0]
-
-def is_range(x, y, now):
-    return 0 <= x < m and 0 <= y < n and arr[x][y] < now
-
-def solution(x, y):
-    if x == m-1 and y == n-1:
+def dfs(i, j) :
+    # 도착 지점에 도달하면 1을 리턴(이건 OK)
+    if (i, j) == (N-1, M-1) :
         return 1
-    if dp[x][y] == -1:
-        dp[x][y] = 0
-        for i in range(4):
-            dr_x, dr_y = x + dx[i], y + dy[i]
-            if is_range(dr_x, dr_y, arr[x][y]):
-                dp[x][y] += solution(dr_x, dr_y)
-    
-    return dp[x][y]
-        
-    
-print(solution(0, 0))
+
+    # 이미 방문한 적이 있다면 그 위치에서 출발하는 경우의 수를 리턴(이건 무슨 말이지? 분기점 잡는건가)
+    if v[i][j] != -1 :
+        return v[i][j]
+
+    cnt = 0
+    for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)) :
+        ni, nj = i + di, j + dj
+
+        if 0 <= ni < N and 0 <= nj < M and arr[ni][nj] < arr[i][j] :
+            cnt += dfs(ni, nj)
+
+    v[i][j] = cnt
+    return v[i][j]
+
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+v = [[-1] * M for _ in range(N)]
+print(dfs(0, 0))
