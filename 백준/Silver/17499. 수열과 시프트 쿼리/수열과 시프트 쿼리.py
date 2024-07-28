f002c4273e0dd6e2141862217166f6aaee6bda5e
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-07-28 17:24:41 +0900

    [Silver III] Title: 수열과 시프트 쿼리, Time: 316 ms, Memory: 144012 KB -BaekjoonHub

---
+++ 백준/Silver/17499. 수열과 시프트 쿼리/수열과 시프트 쿼리.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N, Q = map(int, input().split())
+lst = list(map(int, input().split()))
+p = 0
+for _ in range(Q) :
+    query = list(map(int, input().split()))
+    if query[0] == 1 :
+        lst[(p + query[1] - 1) % N] += query[2]
+
+    elif query[0] == 2 :
+        p -= query[1]
+
+    elif query[0] == 3 :
+        p += query[1]
+
+print(*(lst[p%N:] + lst[:p%N]))
