10065a9bd892f3653687104b6d1a1c36a7043ff2
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-01 18:37:12 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.54 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,69 +1,27 @@
 from collections import deque
 
-def solution(maps) :
+di = [-1, 1, 0, 0]
+dj = [0, 0, -1, 1]    
+                
+def solution(maps):
     n, m = len(maps), len(maps[0])
-    direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-    dist = [[-1] * m for _ in range(n)]
-
-    def bfs(start) :
-        q = deque([start])
-        dist[start[0]][start[1]] = 1
-
-        while q :
-            cur = q.popleft()
-
-            for d in direction :
-                x, y = cur[0] + d[0], cur[1] + d[1]
-
-                if x < 0 or x >= n or y < 0 or y >= m :
-                    continue
-                if maps[x][y] == 0 :
-                    continue
-
-                if dist[x][y] == -1 :
-                    q.append([x, y])
-                    dist[x][y] = dist[cur[0]][cur[1]] + 1
-
-        return dist
-    bfs([0, 0])
-
-    return dist[n - 1][m - 1]
-
-# from heapq import heappush, heappop
-# import sys
-# 
-# 
-# def DFS(maps, cr, cc, R, C):
-#     cv = maps[cr][cc]
-#     for rd, cd in ((-1, 0), (0, 1), (1, 0), (0, -1)):  # URDL
-#         nr, nc = cr + rd, cc + cd
-#         if nr in range(R) and nc in range(C) and cv + 1 < maps[nr][nc]:
-#             maps[nr][nc] = cv + 1
-#             DFS(maps, nr, nc, R, C)
-# 
-# 
-# def BFS(maps, cr, cc, R, C):
-#     QUEUE = []
-#     heappush(QUEUE, (1, 0, 0))  # 몇칸, row, col
-# 
-#     while QUEUE:
-#         cv, cr, cc = heappop(QUEUE)
-#         for rd, cd in ((-1, 0), (0, 1), (1, 0), (0, -1)):
-#             nr, nc = cr + rd, cc + cd
-#             if nr in range(R) and nc in range(C) and maps[nr][nc]:
-#                 if cv + 1 < maps[nr][nc]:
-#                     maps[nr][nc] = cv + 1
-#                     heappush(QUEUE, (maps[nr][nc], nr, nc))
-# 
-# 
-# def solution(maps):
-#     R, C = len(maps), len(maps[0])
-#     maps = [[float("inf") if maps[r][c] else 0 for c in range(C)] for r in range(R)]
-#     maps[0][0] = 1
-# 
-#     sys.setrecursionlimit(10000)
-#     DFS(maps, 0, 0, R, C)
-#     # BFS(maps, 0, 0, R, C)
-# 
-#     return -1 if maps[R - 1][C - 1] == float("inf") else maps[R - 1][C - 1]
+    q = deque()
+    v = [[0] * m for _ in range(n)]
+    
+    q.append((0, 0))
+    v[0][0] = 1
+    
+    while q :
+        ci, cj = q.popleft()
+        if (ci, cj) == (n-1, m-1) :
+            return v[ci][cj]
+        
+        for d in range(4) :
+            ni, nj = ci + di[d], cj + dj[d]
+            if 0<=ni<n and 0<=nj<m and maps[ni][nj] == 1 and v[ni][nj] == 0 :
+                v[ni][nj] = v[ci][cj] + 1
+                q.append((ni, nj))
+                
+    return -1
+    
+    
