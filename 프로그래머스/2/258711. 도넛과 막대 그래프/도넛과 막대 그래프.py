4595a28368239b537b9c502e3aea56ff1ed342a5
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-02 18:48:16 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 1036.60 ms, Memory: 166 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -1,95 +1,39 @@
-from typing import List, Dict
+from collections import defaultdict
 
-import sys
-sys.setrecursionlimit(1000000 * 2)
+def solution(edges):
+    # 각 노드의 진입 차수와 진출 차수를 저장할 딕셔너리 초기화
+    in_degree = defaultdict(int)
+    out_degree = defaultdict(int)
+    # 모든 고유한 노드를 저장할 집합
+    nodes = set()
 
+    # 간선 정보를 순회하며 각 노드의 진입/진출 차수 계산 및 노드 집합 업데이트
+    for a, b in edges:
+        out_degree[a] += 1
+        in_degree[b] += 1
+        nodes.add(a)
+        nodes.add(b)
 
-class Vertex:
-    def __init__(self, n):
-        self.n: int = n
-        self.visited: bool = False
-        self.edge_incoming: List[Edge] = []
-        self.edge_outgoing: List[Edge] = []
+    # 생성된 정점과 각 그래프 유형의 수를 저장할 변수 초기화
+    created_node = None
+    donut = bar = eight = 0
 
-class Edge:
-    def __init__(self, v_fr: Vertex, v_to: Vertex):
-        self.v_fr = v_fr
-        self.v_to = v_to
-        v_fr.edge_outgoing.append(self)
-        v_to.edge_incoming.append(self)
+    # 모든 노드를 순회하며 각 그래프 유형 분류
+    for node in nodes:
+        if out_degree[node] >= 2 and in_degree[node] == 0:
+            # 진출 차수 >= 2이고 진입 차수 = 0인 노드는 생성된 정점
+            created_node = node
+        elif out_degree[node] == 0:
+            # 진출 차수 = 0인 노드는 막대 모양 그래프의 끝점
+            bar += 1
+        elif out_degree[node] == 2:
+            # 진출 차수 = 2인 노드는 8자 모양 그래프의 중심점
+            eight += 1
 
-def build_vertexes(vertexes: Dict, edges: List[List[int]]):
-    for n_fr, n_to in edges:
-        if n_fr in vertexes.keys():
-            pass
-        else:
-            vertexes[n_fr] = Vertex(n_fr)
+    # 도넛 모양 그래프의 수 계산
+    # 생성된 정점의 진출 차수에서 막대와 8자 모양 그래프의 수를 뺀 값
+    donut = out_degree[created_node] - bar - eight
 
-        if n_to in vertexes.keys():
-            pass
-        else:
-            vertexes[n_to] = Vertex(n_to)
-
-        v_fr = vertexes[n_fr]
-        v_to = vertexes[n_to]
-        Edge(v_fr, v_to)
-
-def find_jung_vertex(vertexes: Dict) -> Vertex:
-    candi = []
-
-    for vertex in vertexes.values():
-        if len(vertex.edge_incoming) == 0 \
-            and len(vertex.edge_outgoing) > 0:
-            candi.append(vertex)
-
-    candi.sort(key = lambda v: len(v.edge_outgoing), reverse=True)
-
-    return candi[0]
-
-graph_donut: int = 0
-graph_macda: int = 0
-graph_palja: int = 0
-
-early_stop: bool = False
-
-def travel(v: Vertex):
-    global graph_donut, graph_macda, graph_palja
-    global early_stop
-
-    if early_stop:
-        return
-
-    if len(v.edge_outgoing) == 0:
-        early_stop = True
-        graph_macda += 1
-        return
-
-    if len(v.edge_incoming) > 1 and len(v.edge_outgoing) > 1:
-        early_stop = True
-        graph_palja += 1
-        return
-
-    if v.visited:
-        early_stop = True
-        graph_donut += 1
-        return
-
-    v.visited = True
-
-    for edge in v.edge_outgoing:
-        travel(edge.v_to)
-
-
-def solution(edges: List[List[int]]):
-    global early_stop
-    vertexes: Dict = {}
-
-    build_vertexes(vertexes, edges)
-    v_jung = find_jung_vertex(vertexes)
-
-    for edge in v_jung.edge_outgoing:
-        edge.v_to.edge_incoming.remove(edge)
-        travel(edge.v_to)
-        early_stop = False
-
-    return [v_jung.n, graph_donut, graph_macda, graph_palja]
+    # 문제에서 요구하는 순서대로 결과 반환
+    # [생성된 정점의 번호, 도넛 모양 그래프 수, 막대 모양 그래프 수, 8자 모양 그래프 수]
+    return [created_node, donut, bar, eight]
