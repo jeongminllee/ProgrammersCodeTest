8ccfa10a09554f3275abc05c6f1a705d43c23fe8
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-02 17:48:32 +0900

    [level 3] Title: 양과 늑대, Time: 12.32 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,23 +1,49 @@
 def solution(info, edges):
-    answer = 0
-    v = [0] * len(info)
-    
+    visited = [0] * len(info)
+    answer = []
+
     def dfs(sheep, wolf) :
-        nonlocal answer
         if sheep > wolf :
-            answer = max(answer, sheep)
+            answer.append(sheep)
         else :
             return
-        
-        for prev, curr in edges :
-            if v[prev] and not v[curr] :
-                v[curr] = 1
-                if info[curr] == 0 :
+
+        for p, c in edges :
+            if visited[p] and not visited[c] :
+                visited[c] = 1
+                if info[c] == 0 :
                     dfs(sheep + 1, wolf)
                 else :
                     dfs(sheep, wolf + 1)
-                v[curr] = 0
-    
-    v[0] = 1
+                visited[c] = 0
+
+    visited[0] = 1
     dfs(1, 0)
-    return answer
+
+    return max(answer)
+
+# from collections import deque
+
+# def solution(info, edges):
+#     tree = [[] for _ in range(len(info))]
+#     for edge in edges :
+#         tree[edge[0]].append(edge[1])
+
+#     sheep = 0
+
+#     q = deque()
+#     q.append((0, 1, 0, set()))
+
+#     while q:
+#         curr, sheep_cnt, wolf_cnt, visited = q.popleft()
+#         sheep = max(sheep, sheep_cnt)
+#         visited.update(tree[curr])
+
+#         for next_node in visited :
+#             if info[next_node] :
+#                 if sheep_cnt != wolf_cnt + 1 :
+#                     q.append((next_node, sheep_cnt, wolf_cnt + 1, visited - {next_node}))
+#             else :
+#                 q.append((next_node, sheep_cnt + 1, wolf_cnt, visited - {next_node}))
+
+#     return sheep
