8778a32009aefc34d7ebcc951d53b7513a048450
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-04 15:57:16 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 21.48 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,22 +1,45 @@
 from collections import deque
 
-def solution(board):
-    N = len(board)
-    costs = [[[float('inf')] * 2 for _ in range(N)] for _ in range(N)]  # 각 위치의 최소 비용을 저장하는 3차원 배열
-    costs[0][0] = [0, 0]
-    queue = deque([((0, 0), 0, -1)])  # ((x, y), cost, direction) (-1: 시작, 0: 수평, 1: 수직)
+# 상 좌 하 우 => 이거를 i % 2 = 0, 1 일때를 구별해도 좋지 않을까?
+di = [-1, 0, 1, 0]
+dj = [0, -1, 0, 1]
 
-    while queue:
-        (x, y), cost, direction = queue.popleft()
-        for dx, dy, new_direction in ((0, 1, 0), (1, 0, 1), (0, -1, 0), (-1, 0, 1)):  # 우, 하, 좌, 상 순서
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == 0:
-                new_cost = cost + 600 if direction != new_direction and direction != -1 else cost + 100  # 방향이 바뀌면 코너 비용 추가
-                if costs[nx][ny][new_direction] > new_cost:
-                    costs[nx][ny][new_direction] = new_cost
-                    queue.append(((nx, ny), new_cost, new_direction))
-    print(costs)
+def solution(board) :
+    n = len(board)
+    costs = [[[float('inf')] * 2 for _ in range(n)] for _ in range(n)]
+
+    q = deque()
+    q.append(((0, 0), 0, -1))   # (x, y), cost, direction
+    costs[0][0] = [0,0]
+
+    while q :
+        (i, j), cost, direction = q.popleft()
+        for d in range(4) :
+            ni, nj = i + di[d], j + dj[d]
+            new_direction = d % 2
+            if 0 <= ni < n and 0 <= nj < n and board[ni][nj] == 0 :
+                new_cost = cost + 600 if direction != new_direction and direction != -1 else cost + 100
+                if costs[ni][nj][new_direction] > new_cost :
+                    costs[ni][nj][new_direction] = new_cost
+                    q.append(((ni, nj), new_cost, new_direction))
     return min(costs[-1][-1])
+
+# def solution(board):
+#     N = len(board)
+#     costs = [[[float('inf')] * 2 for _ in range(N)] for _ in range(N)]  # 각 위치의 최소 비용을 저장하는 3차원 배열
+#     costs[0][0] = [0, 0]
+#     queue = deque([((0, 0), 0, -1)])  # ((x, y), cost, direction) (-1: 시작, 0: 수평, 1: 수직)
+
+#     while queue:
+#         (x, y), cost, direction = queue.popleft()
+#         for dx, dy, new_direction in ((0, 1, 0), (1, 0, 1), (0, -1, 0), (-1, 0, 1)):  # 우, 하, 좌, 상 순서
+#             nx, ny = x + dx, y + dy
+#             if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == 0:
+#                 new_cost = cost + 600 if direction != new_direction and direction != -1 else cost + 100  # 방향이 바뀌면 코너 비용 추가
+#                 if costs[nx][ny][new_direction] > new_cost:
+#                     costs[nx][ny][new_direction] = new_cost
+#                     queue.append(((nx, ny), new_cost, new_direction))
+#     return min(costs[-1][-1])
 
 
 # def solution(board) :
