2e01e4195fe0404604baa70298866698eb5b4bd0
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-11 20:24:44 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.03 ms
+메모리: 10.2 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 3일 0:43:50
+2024년 08월 11일 20:24:57
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -2,14 +2,14 @@ def solution(n, words):
     # 순서 1, 2, 3, 1, 2, 3 으로 돌아가니까 
     # % 연산을 이용해서 len(words) % n 을 하면 순서가 나오고 
     # // 연산으로 차례를 return
-    used_words = set()
+    used_words = list()
     prev_word = words[0][0]
     
     for i, word in enumerate(words) :
         if word in used_words or word[0] != prev_word :
             return [(i % n) + 1, (i // n) + 1]
         
-        used_words.add(word)
+        used_words.append(word)
         prev_word = word[-1]
     
     return [0, 0]
