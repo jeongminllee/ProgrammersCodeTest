e45a5aba8328f9b14ae3d2adc298eb13e36c840c
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-12 00:48:10 +0900

    [Silver V] Title: 분수찾기, Time: 92 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,17 @@
+X = int(input())
+n = 1
+
+while X > n :
+    X -= n
+    n += 1
+
+# 짝수일 경우
+if n % 2 == 0 :
+    a = X
+    b = n - X + 1
+
+else :
+    a = n - X + 1
+    b = X
+
+print(f"{a}/{b}")
