a27ee57f6ce4387bc1c464e73028deaee135de56
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-12 01:11:52 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 444 ms, Memory: 122728 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,9 @@
+N = int(input())
+arr = list()
+for _ in range(N) :
+    x, y = map(int, input().split())
+    arr.append((x, y))
+
+arr.sort(key=lambda x:(x[0], x[1]))
+for x, y in arr :
+    print(x, y)
