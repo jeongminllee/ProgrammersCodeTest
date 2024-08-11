ea85606a47953d5401e8357ecf4f1aba7d88b773
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-12 03:46:23 +0900

    [Silver III] Title: N과 M (4), Time: 136 ms, Memory: 112608 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+
+answer = []
+
+def dfs(depth, idx, n, m):
+    if depth == m:
+        print(' '.join(map(str, answer)))
+        return
+    for i in range(idx, n + 1):
+            answer.append(i)
+            dfs(depth + 1, i, n, m)
+            answer.pop()
+
+dfs(0, 1, n, m)
