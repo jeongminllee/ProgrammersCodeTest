65fae3e33ccbb2f112c9aae0c83e497444304f0f
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-16 04:41:45 +0900

    [Silver V] Title: 집합, Time: 932 ms, Memory: 130340 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -2,32 +2,24 @@ import sys
 input = sys.stdin.readline
 
 M = int(input())
-S = set()
+S = 0
 
 for _ in range(M) :
     cmd = input().rstrip()
     if cmd == 'all' :
-        S = set(i for i in range(1, 21))
+        S = (1 << 20) - 1
 
     elif cmd == 'empty' :
-        S.clear()
+        S = 0
 
     else :
         c, num = cmd.split(' ')
+        n = int(num) - 1
         if c == "add" :
-            S.add(int(num))
+            S |= (1 << n)
         elif c == "remove" :
-            if int(num) not in S :
-                continue
-            else :
-                S.remove(int(num))
+            S &= ~(1 << n)
         elif c == "check" :
-            if int(num) in S :
-                print('1')
-            else :
-                print('0')
+            print(1 if S & (1 << n) else 0)
         elif c == 'toggle' :
-            if int(num) not in S :
-                S.add(int(num))
-            else :
-                S.remove(int(num))
+            S ^= (1 << n)
