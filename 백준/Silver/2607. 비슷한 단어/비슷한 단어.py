26d094bfa80af87ab278efcc609b3d5f654898b3
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-18 18:48:44 +0900

    [Silver II] Title: 비슷한 단어, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
@@ -0,0 +1,18 @@
+N = int(input())
+target = list(input())
+res = 0
+
+for _ in range(N - 1) :
+    compare = target[:]
+    word = input()
+    cnt = 0
+
+    for w in word :
+        if w in compare :
+            compare.remove(w)
+        else :
+            cnt += 1
+
+    if cnt < 2 and len(compare) < 2 :
+        res += 1
+print(res)
