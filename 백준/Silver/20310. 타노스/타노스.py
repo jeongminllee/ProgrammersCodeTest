c0aaba77582453197318142fa2e8055eb9d6e09b
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-19 00:17:31 +0900

    [Silver III] Title: 타노스, Time: 88 ms, Memory: 108080 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,6 +1,25 @@
-S = input()
+S = list(input())
 
-S_0 = S.count('0') // 2
-S_1 = S.count('1') // 2
+n = S.count('0')
+m = S.count('1')
 
-print('0' * S_0 + '1' * S_1)
+cnt_0 = 0
+cnt_1 = 0
+
+for i in S :
+    if cnt_1 == m//2 :
+        break
+    if i == '1' :
+        S.remove(i)
+        cnt_1 += 1
+
+S = S[::-1]
+for i in S :
+    if cnt_0 == n//2 :
+        break
+    if i == '0' :
+        S.remove(i)
+        cnt_0 += 1
+
+for i in S[::-1] :
+    print(i, end='')
