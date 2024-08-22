b73fe60d287fe370e5a6cff041cd215b2aa3447a
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-22 21:53:36 +0900

    [Gold V] Title: A와 B 2, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -0,0 +1,19 @@
+S = list(input())
+T = list(input())
+
+def dfs(t) :
+    if t == S :
+        print(1)
+        exit()
+
+    if len(t) == 0 :
+        return 0
+
+    if t[-1] == 'A' :   # 마지막이 A면
+        dfs(t[:-1])     # 제거해서 재귀
+
+    if t[0] == 'B' :    # 처음이 B이면
+        dfs(t[1:][::-1])    # B 제거하고, 뒤집어서 재귀
+
+dfs(T)
+print(0)
