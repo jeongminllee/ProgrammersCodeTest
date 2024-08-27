f3d0cd978b06325111696b4a4b9ee236e33f479d
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-27 12:30:52 +0900

    [Gold V] Title: 빌런 호석, Time: 144 ms, Memory: 110592 KB -BaekjoonHub

--- 백준/Gold/22251. 빌런 호석/빌런 호석.py
+++ 백준/Gold/22251. 빌런 호석/빌런 호석.py
@@ -1,35 +1,28 @@
+# N : 층수, K : 디스플레이에 나타나는 층 자리 수, P : 반전시킬 수, X : 실제 멈춰있는 층
 N, K, P, X = map(int, input().split())
-nums = {
-    '0' : [1, 1, 1, 1, 1, 1, 0],
-    '1' : [0, 1, 1, 0, 0, 0, 0],
-    '2' : [1, 1, 0, 1, 1, 0, 1],
-    '3' : [1, 1, 1, 1, 0, 0, 1],
-    '4' : [0, 1, 1, 0, 0, 1, 1],
-    '5' : [1, 0, 1, 1, 0, 1, 1],
-    '6' : [1, 0, 1, 1, 1, 1, 1],
-    '7' : [1, 1, 1, 0, 0, 0, 0],
-    '8' : [1, 1, 1, 1, 1, 1, 1],
-    '9' : [1, 1, 1, 1, 0, 1, 1]
-}
-
-X = str(X).zfill(K)
-x_transform = [nums[x] for x in X]
+nums = [
+    [0, 4, 3, 3, 4, 3, 2, 3, 1, 2], # 0 to n
+    [4, 0, 5, 3, 2, 5, 6, 1, 5, 4], # 1 to n
+    [3, 5, 0, 2, 5, 4, 3, 4, 2, 3], # 2 to n
+    [3, 3, 2, 0, 3, 2, 3, 2, 2, 1], # 3 to n
+    [4, 2, 5, 3, 0, 3, 4, 3, 3, 2], # 4 to n
+    [3, 5, 4, 2, 3, 0, 1, 4, 2, 1], # 5 to n
+    [2, 6, 3, 3, 4, 1, 0, 5, 1, 2], # 6 to n
+    [3, 1, 4, 2, 3, 4, 5, 0, 4, 3], # 7 to n
+    [1, 5, 2, 2, 3, 2, 1, 4, 0, 1], # 8 to n
+    [2, 4, 3, 1, 2, 1, 2, 3, 1, 0]  # 9 to n
+]
 
 ans = 0
-for n in range(1, N+1) :
-    elevator = str(n).zfill(K)
-    ele_transform = [nums[e] for e in elevator]
+for target in range(1, N+1) :
+    now = X
+    cnt = 0
+    for _ in range(K) :
+        cnt += nums[now % 10][target % 10]
+        now //= 10
+        target //= 10
 
-    diff_cnt = 0
-    for x, e in zip(x_transform, ele_transform) :
-        for i, j in zip(x, e) :
-            if i != j :
-                diff_cnt += 1
-
-        if diff_cnt > P :
-            break
-
-    if diff_cnt <= P and diff_cnt > 0 :
+    if 1 <= cnt <= P :
         ans += 1
 
 print(ans)
