ca986e09e21cc7329680de85b04b072dee698f85
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-27 12:11:23 +0900

    [Gold V] Title: 빌런 호석, Time: 756 ms, Memory: 111516 KB -BaekjoonHub

---
+++ 백준/Gold/22251. 빌런 호석/빌런 호석.py
@@ -0,0 +1,35 @@
+N, K, P, X = map(int, input().split())
+nums = {
+    '0' : [1, 1, 1, 1, 1, 1, 0],
+    '1' : [0, 1, 1, 0, 0, 0, 0],
+    '2' : [1, 1, 0, 1, 1, 0, 1],
+    '3' : [1, 1, 1, 1, 0, 0, 1],
+    '4' : [0, 1, 1, 0, 0, 1, 1],
+    '5' : [1, 0, 1, 1, 0, 1, 1],
+    '6' : [1, 0, 1, 1, 1, 1, 1],
+    '7' : [1, 1, 1, 0, 0, 0, 0],
+    '8' : [1, 1, 1, 1, 1, 1, 1],
+    '9' : [1, 1, 1, 1, 0, 1, 1]
+}
+
+X = str(X).zfill(K)
+x_transform = [nums[x] for x in X]
+
+ans = 0
+for n in range(1, N+1) :
+    elevator = str(n).zfill(K)
+    ele_transform = [nums[e] for e in elevator]
+
+    diff_cnt = 0
+    for x, e in zip(x_transform, ele_transform) :
+        for i, j in zip(x, e) :
+            if i != j :
+                diff_cnt += 1
+
+        if diff_cnt > P :
+            break
+
+    if diff_cnt <= P and diff_cnt > 0 :
+        ans += 1
+
+print(ans)
