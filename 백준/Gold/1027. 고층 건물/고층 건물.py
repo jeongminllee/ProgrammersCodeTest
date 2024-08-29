3b9813a1b23d9fe4b8b258773cc8fb2f196931bd
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-29 22:59:10 +0900

    [Gold IV] Title: 고층 건물, Time: 88 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/1027. 고층 건물/고층 건물.py
+++ 백준/Gold/1027. 고층 건물/고층 건물.py
@@ -1,6 +1,3 @@
-def slope(x1, y1, x2, y2) :
-    return (y2 - y1) / (x2 - x1)
-
 N = int(input())
 lst = list(map(int, input().split()))
 
@@ -9,11 +6,11 @@ res = [0] * N
 for i in range(N-1) :
     max_slope = -float('inf')
     for j in range(i+1, N) :
-        slope_ = slope(i, lst[i], j, lst[j])
+        slope = (lst[j] - lst[i]) / (j - i)
 
-        if slope_ <= max_slope :
+        if slope <= max_slope :
             continue
-        max_slope = max(max_slope, slope_)
+        max_slope = max(max_slope, slope)
         res[i] += 1
         res[j] += 1
 
