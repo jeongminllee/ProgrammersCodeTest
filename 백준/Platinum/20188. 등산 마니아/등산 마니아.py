b8db603d2230ad27cb95e5360562040ee3a761c5
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-08-31 00:04:00 +0900

    [Platinum V] Title: 등산 마니아, Time: 580 ms, Memory: 200100 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/20188. 등산 마니아/등산 마니아.py
+++ 백준/Platinum/20188. 등산 마니아/등산 마니아.py
@@ -1,27 +1,37 @@
-from collections import defaultdict
-import sys
-sys.setrecursionlimit(10**8)
-input = sys.stdin.readline
+N=int(input())
 
-def dfs(cur, parent) :
-    global ans
-    for child in graph[cur] :
-        if child != parent :
-            dfs(child, cur)
-            dp[cur] += dp[child] + 1
+adj=[[] for _ in range(N+1)]
+#adj[v]:v와 간선으로 연결된 정점들의 모임
+for _ in range(N-1):
+    a,b=map(int,input().split())
+    adj[a].append(b)
+    adj[b].append(a)
 
-    val = (dp[cur] + 1) * (N - dp[cur] - 1) + (dp[cur] * (dp[cur] + 1) // 2)
+p=[0 for _ in range(N+1)]#p[V]:V의 부모 정점 번호
+sz=[1 for _ in range(N+1)]
+#sz[V]:V를 루트로 하는 서브트리의 크기
 
-    if cur != 1 :
-        ans += val
+stack=[1]
+dfsord=[]
+while stack:
+    cur=stack.pop()
+    dfsord.append(cur)
+    for nxt in adj[cur]:
+        if nxt == p[cur]:
+            continue
+        p[nxt]=cur
+        stack.append(nxt)
 
-N = int(input())
-dp = [0] * (N + 1)
-graph = defaultdict(list)
-for _ in range(N-1) :
-    u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
-ans = 0
-dfs(1, 0)
+while dfsord:
+    v=dfsord.pop()
+    sz[p[v]]+=sz[v]
+            
+
+ans=0
+MM=N*(N-1)//2
+for i in range(2,N+1):
+    t=N-sz[i] 
+    #루트와 연결 했을 때 i와 i의 부모를 잇는 간선을 지나지 않는 정점의 수
+    ans+= MM-t*(t-1)//2
+
 print(ans)
