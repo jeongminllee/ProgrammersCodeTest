1ffb3cc88a0591ee77304aba510eabfcd1e647f1
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-09-03 15:29:39 +0900

    [Bronze III] Title: ZOAC 4, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.py
@@ -5,3 +5,18 @@ col = math.ceil(W/(M + 1))
 
 ans = row * col
 print(ans)
+
+'''
+H, W, N, M = map(int, input().split())
+
+row = H // (N + 1)
+col = W // (M + 1)
+
+if H % (N + 1) != 0 :
+    row += 1
+if W % (M + 1) != 0 :
+    col += 1
+
+ans = row * col
+print(ans)
+'''
