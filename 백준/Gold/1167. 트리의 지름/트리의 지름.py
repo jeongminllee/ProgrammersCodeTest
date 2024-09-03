2656a8af84d2ce31e0be67dfe498887f1a228cea
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-09-03 18:48:58 +0900

    [Gold II] Title: 트리의 지름, Time: 280 ms, Memory: 146100 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -1,10 +1,13 @@
-import sys
-sys.setrecursionlimit(10**6)
 def dfs(s, arr, v) :
-    for (next_node, dist) in arr[s] :
-        if v[next_node] == -1 :
-            v[next_node] = v[s] + dist
-            dfs(next_node, arr, v)
+    stack = [(s, 0)]
+    v[s] = 0
+
+    while stack :
+        node, dist = stack.pop()
+        for next_node, next_dist in arr[node] :
+            if v[next_node] == -1 :
+                v[next_node] = dist + next_dist
+                stack.append((next_node, v[next_node]))
 
 n = int(input())
 arr = [[] for _ in range(n + 1)]
