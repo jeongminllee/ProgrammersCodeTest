8ffc34edc5af5ce1828c6e78d16351bad73ea61f
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-09-03 18:25:44 +0900

    [Bronze I] Title: 단어 공부, Time: 140 ms, Memory: 114192 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,4 +1,24 @@
 S = input().upper()
+char_count = {}
+
+# Counter(S)
+for char in S :
+    char_count[char] = char_count.get(char, 0) + 1
+
+# 가장 많이 나온 문자의 개수 찾기
+max_count = max(char_count.values())
+
+# 가장 많이 나온 문자들 찾기
+most_common = [char for char, cnt in char_count.items() if cnt == max_count]
+
+# 결과 출력
+if len(most_common) > 1 :
+    print("?")
+else :
+    print(most_common[0])
+    
+'''
+S = input().upper()
 
 unique_S = list(set(S))
 
@@ -14,3 +34,4 @@ if cnt_list.count(max(cnt_list)) > 1 :
 else :
     max_index = cnt_list.index(max(cnt_list))
     print(unique_S[max_index])
+    '''
