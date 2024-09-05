7d07d7689762fd4ee9344469a6456eb56c170d4d
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-09-05 15:45:07 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 88 ms, Memory: 109108 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
@@ -1,37 +1,42 @@
-aeiou = ('a', 'e', 'i', 'o', 'u')
-
+aeiou = ('a','e','i','o','u')
 while True :
-    cmd = input()
-    if cmd == 'end' :
+    S = input()
+    if S == 'end' :
         break
 
-    x = y = cnt = 0
+    # x : 1번 조건, y : 2번 조건, z : 3번 조건
+    x = y = z = 0
+
+    # 1. 모음이 하나 이상 등장하는지
     for i in aeiou :
-        if i in cmd :
-            cnt += 1
+        if i in S :
+            x += 1
 
-    if cnt < 1 :
-        print(f'<{cmd}> is not acceptable.')
+    if x < 1 :
+        print(f'<{S}> is not acceptable.')
         continue
 
-    for i in range(len(cmd) - 2) :
-        if cmd[i] in aeiou and cmd[i+1] in aeiou and cmd[i+2] in aeiou or \
-                not(cmd[i] in aeiou) and not(cmd[i + 1] in aeiou) and not(cmd[i + 2] in aeiou) :
-            x = 1
-
-    if x == 1 :
-        print(f'<{cmd}> is not acceptable.')
-        continue
-
-    for i in range(len(cmd) - 1) :
-        if cmd[i] == cmd[i + 1] :
-            if cmd[i] == 'e' or cmd[i] == 'o' :
-                continue
-            else :
-                y = 1
+    # 2. 연속해서 모음 또는 자음이 3개 등장하는지
+    for i in range(len(S) - 2) :
+        if (S[i] in aeiou and S[i+1] in aeiou and S[i+2] in aeiou) or \
+            (not(S[i] in aeiou) and not(S[i+1] in aeiou) and not(S[i+2] in aeiou)):
+            y = 1
 
     if y == 1 :
-        print(f'<{cmd}> is not acceptable.')
+        print(f'<{S}> is not acceptable.')
         continue
 
-    print(f'<{cmd}> is acceptable.')
+    # 3. 같은 글자가 연속해서 2번 등장하는지, ee oo 는 제외
+    for i in range(len(S) - 1) :
+        if S[i] == S[i + 1] :
+            if S[i] == 'e' or S[i] == 'o' :
+                continue
+            else :
+                z = 1
+
+    if z == 1 :
+        print(f'<{S}> is not acceptable.')
+        continue
+    
+    # 3가지 제한사항을 다 통과하였다면
+    print(f'<{S}> is acceptable.')
