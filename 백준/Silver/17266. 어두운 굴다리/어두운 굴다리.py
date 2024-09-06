83ace398b6f81df156f53f961310bae7cbe9c926
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-09-06 21:45:54 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 116 ms, Memory: 123716 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
@@ -1,22 +1,25 @@
-def bs(li, m):
-    if li[1]-li[0] > m:
-        return 0
-    if li[-1]-li[-2] > m:
-        return 0
-    for i in range(1, len(li)-2):
-        if (li[i+1]-li[i])/2 > m:
-            return 0
-    return 1
+def binary_search(arr, mid) :
+    if arr[1] - arr[0] > mid :
+        return False
+    if arr[-1] - arr[-2] > mid :
+        return False
+    for i in range(1, len(arr) - 2) :
+        if (arr[i + 1] - arr[i]) / 2 > mid :
+            return False
+    return True
 
-N, M = int(input()), int(input())
-li = [0] + list(map(int, input().split())) + [N]
+N = int(input())
+M = int(input())
+arr = [0] + list(map(int, input().split())) + [N]
+
 s, e = 0, N
-res = 0
-while s <= e:
-    m = (s+e)//2
-    if bs(li, m):
-        e = m-1
-        res = m
-    else:
-        s = m+1
-print(res)
+ans = 0
+while s <= e :
+    mid = (s + e) // 2
+    if binary_search(arr, mid) :
+        e = mid - 1
+        ans = mid
+    else :
+        s = mid + 1
+
+print(ans)
