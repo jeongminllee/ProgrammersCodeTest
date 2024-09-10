a1c696da7ba6e0d4ebd6fe44132098952f5f7f63
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-09-10 16:10:27 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 428 ms, Memory: 139596 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -1,9 +1,9 @@
 N, M = map(int, input().split())
-words = {}
-lst = []
+words = {}  # Counter 할 딕셔너리 생성
+lst = []    # 단어장
 for _ in range(N) :
     S = input()
-    if len(S) < M :
+    if len(S) < M : # S의 길이가 M 미만이면
         continue
 
     if S not in words :
@@ -14,6 +14,9 @@ for _ in range(N) :
 for word, val in words.items() :
     lst.append((word, val))
 
+# 1. 등장 횟수가 많을수록 
+# 2. 단어 길이가 길수록 
+# 3. 알파벳 순으로
 lst.sort(key=lambda x:(-x[1], -len(x[0]), x[0]))
 
 for w in lst :
