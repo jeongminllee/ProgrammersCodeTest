945e8954b5d3f4c0bb658467a1f30036475d24dc
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-09-10 16:07:06 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 444 ms, Memory: 139596 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -1,16 +1,20 @@
 N, M = map(int, input().split())
-pad = {}
-
-for i in range(N) :
-    words = input()
-    if len(words) < M :
+words = {}
+lst = []
+for _ in range(N) :
+    S = input()
+    if len(S) < M :
         continue
 
-    if words in pad :
-        pad[words] += 1
+    if S not in words :
+        words[S] = 1
     else :
-        pad[words] = 1
+        words[S] += 1
 
-arr = [i for i in pad.keys()]
-arr.sort(key=lambda x:(-pad[x], -len(x), x))
-print(*arr, sep='\n')
+for word, val in words.items() :
+    lst.append((word, val))
+
+lst.sort(key=lambda x:(-x[1], -len(x[0]), x[0]))
+
+for w in lst :
+    print(w[0])
