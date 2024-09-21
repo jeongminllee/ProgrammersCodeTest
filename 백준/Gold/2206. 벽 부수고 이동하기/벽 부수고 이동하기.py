dadcfdbcb73c1cb5e457c699fccfc1a1266e5477
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-09-22 03:04:52 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1320 ms, Memory: 277176 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,36 +1,30 @@
 from collections import deque
 
-di = [-1, 1, 0, 0]
-dj = [0, 0, -1, 1]
-
-def bfs(N, M, board, visited) :
+def bfs() :
     q = deque()
-    q.append((0, 0, 0)) # i, j, br
-    visited[0][0][0] = 1
+    q.append((0, 0, 0))
+    v = [[[0] * 2 for _ in range(M)] for _ in range(N)]
+    v[0][0][0] = 1
 
     while q :
         ci, cj, br = q.popleft()
-        
-        # 목적지까지 도달했을 경우
+
         if (ci, cj) == (N-1, M-1) :
-            return visited[ci][cj][br]
+            return v[ci][cj][br]
 
-        for d in range(4) :
-            ni, nj = ci + di[d], cj + dj[d]
-
-            if 0 <= ni < N and 0 <= nj < M :
-                if board[ni][nj] == 0 and visited[ni][nj][br] == 0 :
-                    visited[ni][nj][br] = visited[ci][cj][br] + 1
+        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)) :
+            ni, nj = ci + di, cj + dj
+            if 0 <= ni < N and 0 <= nj < M and v[ni][nj][br] == 0 :
+                if board[ni][nj] == 0 :
+                    v[ni][nj][br] = v[ci][cj][br] + 1
                     q.append((ni, nj, br))
                 elif board[ni][nj] == 1 and br == 0 :
-                    visited[ni][nj][1] = visited[ci][cj][0] + 1
+                    v[ni][nj][1] = v[ci][cj][0] + 1
                     q.append((ni, nj, 1))
 
-    # 다 돌지 못했을 경우
     return -1
 
 
 N, M = map(int, input().split())
 board = [list(map(int, input())) for _ in range(N)]
-visited = [[[0] * 2 for _ in range(M)] for _ in range(N)]
-print(bfs(N, M, board, visited))
+print(bfs())
