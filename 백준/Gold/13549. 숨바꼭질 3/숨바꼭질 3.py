25f8a803266c8dbb39c2d6ee4803f468a1c68441
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-11-13 20:06:12 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 115032 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,23 +1,27 @@
 from collections import deque
 
-n, k = map(int, input().split())
-q = deque()
-q.append(n)
-v = [-1 for _ in range(100001)]
-v[n] = 0
+def bfs(s, e) :
+    # 1. q 생성, v[] 생성
+    q = deque()
+    v = [-1] * 100001
 
-while q :
-    s = q.popleft()
-    if s == k :
-        print(v[s])
-        break
-    if 0 <= s - 1 < 100001 and v[s - 1] == -1 :
-        v[s - 1] = v[s] + 1
-        q.append(s - 1)
-    if 0 < s * 2 < 100001 and v[s * 2] == -1 :
-        v[s * 2] = v[s]
-        q.append(s * 2)
-    if 0 <= s + 1 < 100001 and v[s + 1] == -1 :
-        v[s + 1] = v[s] + 1
-        q.append(s + 1)
-        
+    # 2. 초기 데이터 삽입, v[] 초기화
+    q.append(s)
+    v[s] = 0
+
+    while q :
+        c = q.popleft()
+        if c == e :
+            return v[e]
+        if 0 <= c - 1 < 100001 and v[c - 1] == - 1 :
+            v[c - 1] = v[c] + 1
+            q.append(c - 1)
+        if 0 < c * 2 < 100001 and v[c * 2] == -1 :
+            v[c * 2] = v[c]
+            q.append(c * 2)
+        if 0 <= c + 1 < 100001 and v[c + 1] == -1 :
+            v[c + 1] = v[c] + 1
+            q.append(c + 1)
+
+n, k = map(int, input().split())
+print(bfs(n, k))
