dd9cdbbeb628f8ba02e319d41a952ff6a5d41cc2
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-11-13 18:34:22 +0900

    [Gold V] Title: 평범한 배낭, Time: 268 ms, Memory: 189644 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,19 +1,25 @@
+# 입력 받기
 n, k = map(int, input().split())
+
 items = [[0, 0]]
-dp = [[0] * (k + 1) for _ in range(n + 1)]
 
 for _ in range(n) :
     items.append(list(map(int, input().split())))
 
-for i in range(1, n + 1) :
-    for j in range(1, k + 1) :
-        w = items[i][0]
-        p = items[i][1]
+# dp[i][j] : i번째 물건까지 고려했을 때, 무게 weight에서의 최대 가치
+dp = [[0] * (k + 1) for _ in range(n + 1)]
 
-        if j < w :
+# 각 물건에 대해
+for i in range(1, n + 1) :
+    weight, value = items[i]
+    # 각 무게에 대해
+    for j in range(1, k + 1) :
+        if j < weight :     # 현재 물건을 넣을 수 없는 경우
             dp[i][j] = dp[i - 1][j]
 
-        else :
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + p)
+        else :  # 현재 물건을 넣을 수 있는 경우
+            # 현재 물건을 넣지 않는 경우와 넣는 경우 중 최댓값 선택
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight] + value)
 
+# 결과 출력
 print(dp[n][k])
