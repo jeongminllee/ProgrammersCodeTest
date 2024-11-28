3f8b39356c343f5275aaf2c8f7d09c780921550b
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-11-29 00:18:37 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 352 ms, Memory: 138968 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 # N : 과목의 수, M : 선수 조건의 수
 N, M = map(int, input().split())
 
@@ -8,29 +6,28 @@ graph = [[] for _ in range(N + 1)]
 in_degree = [0] * (N + 1)
 
 # 선수 조건 입력 처리
-for _ in range(M):
+for _ in range(M) :
     A, B = map(int, input().split())
     graph[A].append(B)
     in_degree[B] += 1
 
 # 위상 정렬 수행
-queue = deque()
-result = [0] * (N + 1)
+q = []
+res = [0] * (N + 1)
 
 # 진입 차수가 0인 노드부터 시작
-for i in range(1, N + 1):
-    if in_degree[i] == 0:
-        queue.append(i)
-        result[i] = 1  # 첫 학기부터 수강 가능
+for i in range(1, N + 1) :
+    if in_degree[i] == 0 :
+        q.append(i)
+        res[i] = 1  # 첫 학기부터 수강 가능
 
-while queue:
-    current = queue.popleft()
-    
-    for neighbor in graph[current]:
+while q :
+    curr = q.pop(0)
+
+    for neighbor in graph[curr] :
         in_degree[neighbor] -= 1
-        if in_degree[neighbor] == 0:
-            queue.append(neighbor)
-            result[neighbor] = result[current] + 1
+        if in_degree[neighbor] == 0 :
+            q.append(neighbor)
+            res[neighbor] = res[curr] + 1
 
-# 결과 출력
-print(*result[1:])
+print(*res[1:])
