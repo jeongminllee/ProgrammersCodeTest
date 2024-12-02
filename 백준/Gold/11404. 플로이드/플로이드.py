6aab29e38fc9e601c78f9fce6275b671a83f4885
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-12-02 23:16:41 +0900

    [Gold IV] Title: 플로이드, Time: 216 ms, Memory: 121752 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,38 @@
+INF = float('inf')
+
+def floyd_warshall(bus) :
+    distance = [[INF] * n for _ in range(n)]
+
+    for i in range(n) :
+        for j in range(n) :
+            if i == j :
+                distance[i][j] = 0
+
+    for a, b, c in bus :
+        distance[a-1][b-1] = min(distance[a-1][b-1], c)
+
+    for k in range(n) :
+        for i in range(n) :
+            for j in range(n) :
+                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
+
+    return distance
+
+# 도시의 개수
+n = int(input())
+
+# 버스의 개수
+m = int(input())
+
+# 버스 정보
+bus = [list(map(int, input().split())) for _ in range(m)]
+
+res = floyd_warshall(bus)
+
+for i in range(n) :
+    for j in range(n) :
+        if res[i][j] == INF :
+            print('0', end=' ')
+        else :
+            print(res[i][j], end=' ')
+    print()
