3a3741b86db606d7c8bf3917af1443df79f7d1f1
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2024-12-05 20:39:38 +0900

    [Gold IV] Title: 알고스팟, Time: 140 ms, Memory: 114148 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -1,25 +1,27 @@
-import heapq
+from collections import deque
 
 # 이동 벡터
 di = [-1, 1, 0, 0]
 dj = [0, 0, -1, 1]
 
 def bfs(si, sj) :
-    q = []
-    heapq.heappush(q, (si, sj, 0))
+    q = deque()
+    q.append((si ,sj))
     v[si][sj] = 0
 
     while q :
-        ci, cj, cost = heapq.heappop(q)
+        ci, cj = q.popleft()
 
         for d in range(4) :
             ni, nj = ci + di[d], cj + dj[d]
             # 여기 좀 헷갈림 특히 괄호 안.
-            if 0 <= ni < N and 0 <= nj < M :
-                if board[ni][nj] + cost < v[ni][nj] :
-                    v[ni][nj] = cost + board[ni][nj]
-                    heapq.heappush(q, (ni, nj, v[ni][nj]))
-
+            if 0 <= ni < N and 0 <= nj < M and v[ni][nj] == -1 :
+                if board[ni][nj] == 0 :
+                    v[ni][nj] = v[ci][cj]
+                    q.appendleft((ni, nj))
+                else :
+                    v[ni][nj] = v[ci][cj] + 1
+                    q.append((ni, nj))
 
     return v
 
@@ -29,7 +31,7 @@ M, N = map(int, input().split())
 # board
 board = [list(map(int, input().rstrip())) for _ in range(N)]
 
-v = [[1e10] * M for _ in range(N)]
+v = [[-1] * M for _ in range(N)]
 
 # 정답
 ans = bfs(0, 0)
