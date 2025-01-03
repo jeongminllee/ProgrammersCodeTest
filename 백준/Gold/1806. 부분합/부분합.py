36147afed465886c1291ddfed43c7d076bef634d
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-01-03 20:06:08 +0900

    [Gold IV] Title: 부분합, Time: 112 ms, Memory: 121100 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,22 +1,24 @@
-N, S = map(int, input().split())
-lst = list(map(int,input().split()))
+N, M = map(int, input().split())
+lst = list(map(int, input().split()))
 
 left, right = 0, 0
-num = 0
-mn_length = 1e9
+target = 0
+length = N + 1
 
 while True :
-    if S <= num :
-        mn_length = min(mn_length, right - left)
-        num -= lst[left]
+    if target >= M :
+        length = min(length, right - left)
+        target -= lst[left]
         left += 1
+
     elif right == N :
         break
+
     else :
-        num += lst[right]
+        target += lst[right]
         right += 1
 
-if mn_length == 1e9 :
+if length == N + 1 :
     print(0)
 else :
-    print(mn_length)
+    print(length)
