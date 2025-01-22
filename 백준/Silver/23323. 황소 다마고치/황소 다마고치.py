d9edfa29020b39b5fd42bdb751f2ed94ec37f39c
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-01-22 18:14:05 +0900

    [Silver II] Title: 황소 다마고치, Time: 160 ms, Memory: 110760 KB -BaekjoonHub

---
+++ 백준/Silver/23323. 황소 다마고치/황소 다마고치.py
@@ -0,0 +1,12 @@
+def main() :
+    # 항소의 초기 체력, 가지고 있는 먹이 개수
+    n, m = map(int, input().split())
+
+    while n > 0 :
+        m += 1
+        n //= 2
+    print(m)
+
+T = int(input())
+for _ in range(T) :
+    main()
