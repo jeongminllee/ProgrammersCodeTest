e58915b6e85402f69356d43b705055796083cbe9
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-01-22 14:27:12 +0900

    [level 3] Title: 이중우선순위큐, Time: 98.73 ms, Memory: 21 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,31 +1,45 @@
 import heapq
+from collections import defaultdict
 
 def solution(operations):
-    # 최소힙과 최대힙을 구하는 리스트 생성
-    # 힙은 기본적으로 최소힙을 지향하기 때문에 최대힙을 따로 구현
-    heap = []
     max_heap = []
+    min_heap = []
+    num_cnt = defaultdict(int)
     
     for operation in operations :
         op, num = operation.split()
         num = int(num)
         
-        if op == "I" :  # op가 "I"이면
-            # 최소힙, 최대힙 추가
-            # 최대힙은 음수로 힙을 넣어서 구현
-            heapq.heappush(heap, num)
+        if op == "I" :
             heapq.heappush(max_heap, (-num, num))
-        else :  # op가 "D"
-            if not heap :   # 힙이 비어있으면
+            heapq.heappush(min_heap, num)
+            num_cnt[num] += 1
+
+        else :
+            if not min_heap :
                 pass
-            elif num == 1 : # "D 1"
-                max_value = heapq.heappop(max_heap)[1]
-                heap.remove(max_value)
-            elif num == -1 :# "D -1"
-                min_value = heapq.heappop(heap)
-                max_heap.remove((-min_value, min_value))
-                
-    if heap :   # 힙에 남아있으면 [최대힙, 최소힙]
-        return [heapq.heappop(max_heap)[1], heapq.heappop(heap)]
-    else :  # 힙이 비어있으면
-        return [0,0]
+            
+            elif num == 1 :
+                while max_heap and num_cnt[max_heap[0][1]] == 0 :
+                    heapq.heappop(max_heap)
+                if max_heap :
+                    max_value = heapq.heappop(max_heap)[1]
+                    num_cnt[max_value] -= 1
+                    
+            else :
+                while min_heap and num_cnt[min_heap[0]] == 0:
+                    heapq.heappop(min_heap)
+                if min_heap :
+                    min_value = heapq.heappop(min_heap)
+                    num_cnt[min_value] -= 1
+    
+    while max_heap and num_cnt[max_heap[0][1]] == 0 :
+        heapq.heappop(max_heap)
+    while min_heap and num_cnt[min_heap[0]] == 0:
+        heapq.heappop(min_heap)
+        
+    if not min_heap :
+        return [0, 0]
+
+    else :
+        return [heapq.heappop(max_heap)[1], heapq.heappop(min_heap)]
