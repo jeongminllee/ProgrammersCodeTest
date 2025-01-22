4c06aa27b4ae5e980820e9aad7b1e5319fa58a09
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-01-23 06:18:31 +0900

    [Gold V] Title: 급상승, Time: 468 ms, Memory: 254960 KB -BaekjoonHub

--- 백준/Gold/23978. 급상승/급상승.py
+++ 백준/Gold/23978. 급상승/급상승.py
@@ -1,34 +1,28 @@
-def calculate_total_money(N, dates, X):
-    total = 0
-    for i in range(N) :
-        current_date = dates[i]
-        next_date = dates[i + 1] if i < N - 1 else float('inf')
-        days = next_date - current_date
+def sum_val(x) :
+    return (x * (x + 1)) // 2
 
-        if days >= X :
-            # X 부터 1까지의 합 : X * (X+1) // 2
-            total += (X * (X + 1)) // 2
-        else :
-            # X 부터 (X - days + 1) 까지의 합
-            # 첫 항 : X, 마지막 항 : (X - days + 1), 항의 개수 : days
-            first = X
-            last = X - days + 1
-            total += (first + last) * days // 2
+def can(target, nums, K):
+    res = sum_val(target)
+    for num in nums :
 
-    return total
+        res += sum_val(target) - sum_val(max(0, target - num))
+        if res >= K :
+            return True
+
+    return res >= K
 
 def find_minimum_X(N, K, dates):
     left = 1
     right = K  # 최대값으로 K를 설정
+    diff = [dates[i+1] - dates[i] for i in range(N-1)][::-1]
 
     # 이분 탐색
-    while left < right:
+    while left <= right:
         mid = (left + right) // 2
-        total_money = calculate_total_money(N, dates, mid)
 
-        if total_money >= K:
-            right = mid
-        else:
+        if can(mid, diff, K) :
+            right = mid - 1
+        else :
             left = mid + 1
 
     return left
@@ -41,5 +35,5 @@ def main() :
     # 결과 출력
     result = find_minimum_X(N, K, dates)
     print(result)
-
+    
 main()
