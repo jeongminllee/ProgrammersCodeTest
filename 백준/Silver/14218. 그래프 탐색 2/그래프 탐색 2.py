5e424cce6301154d3526e0d90979530535996b49
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-02-03 19:31:14 +0900

    Update 그래프 탐색 2.py

--- 백준/Silver/14218. 그래프 탐색 2/그래프 탐색 2.py
+++ 백준/Silver/14218. 그래프 탐색 2/그래프 탐색 2.py
@@ -3,34 +3,32 @@ from collections import deque
 def bfs(s) :
     q = deque()
     q.append(s)
-
-    visited = [-1] * (n + 1)
-    visited[s] = 0
+    distances = [-1] * (n + 1)
+    distances[s] = 0
 
     while q :
         c = q.popleft()
-        for nxt in edges[c] :
-            if visited[nxt] == -1 :
-                q.append(nxt)
-                visited[nxt] = visited[c] + 1
 
-    return visited[1:]
+        for nxt in edges[c] :
+            dist = distances[c] + 1
+            if distances[nxt] == -1 :
+                distances[nxt] = dist
+                q.append(nxt)
+
+    return distances[1:]
+
 
 n, m = map(int, input().split())
 edges = [[] for _ in range(n + 1)]
-
 for _ in range(m) :
-    a, b = map(int, input().split())
-    edges[a].append(b)
-    edges[b].append(a)
+    u, v = map(int, input().split())
+    edges[u].append(v)
+    edges[v].append(u)
 
-t = int(input())
-distances = [-1] * (n + 1)
-distances[1] = 0
-
-for _ in range(t) :
-    a, b = map(int, input().split())
-    edges[a].append(b)
-    edges[b].append(a)
-    res = bfs(1)
+bridges = int(input())
+for _ in range(bridges) :
+    u, v = map(int, input().split())
+    edges[u].append(v)
+    edges[v].append(u)
+    res = bfs(s=1)
     print(*res)
