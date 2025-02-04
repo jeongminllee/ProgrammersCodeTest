a28967e9e186a0b8c6219aaf2b7da20820b57f40
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-02-04 21:13:46 +0900

    [Gold IV] Title: DSLR, Time: 5676 ms, Memory: 215456 KB -BaekjoonHub

---
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+def main() :
+    A, B = map(int, input().split())
+
+    visited = [0] * 10001
+
+    q = deque()
+    q.append([A, ''])
+    visited[A] = 1
+
+    while q :
+        num, command = q.popleft()
+
+        if num == B:
+            print(command)
+            break
+
+        d = num * 2 % 10000
+        if not visited[d] :
+            visited[d] = 1
+            q.append([d, command + 'D'])
+
+        s = (num - 1) % 10000
+        if not visited[s] :
+            visited[s] = 1
+            q.append([s, command + 'S'])
+
+        l = num // 1000 + (num % 1000) * 10
+        if not visited[l] :
+            visited[l] = 1
+            q.append([l, command + 'L'])
+
+        r = num // 10 + (num % 10) * 1000
+        if not visited[r] :
+            visited[r] = 1
+            q.append([r, command + 'R'])
+
+
+T = int(input())
+for _ in range(T) :
+    main()
