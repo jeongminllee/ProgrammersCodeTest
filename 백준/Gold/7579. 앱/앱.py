5ba5ddc1a975c3bb764b0d5699591d8d342156de
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-02-06 00:28:06 +0900

    [Gold III] Title: 앱, Time: 120 ms, Memory: 116640 KB -BaekjoonHub

---
+++ 백준/Gold/7579. 앱/앱.py
@@ -0,0 +1,29 @@
+def main() :
+    N, M = map(int, input().split())
+    memories = [0] + list(map(int, input().split()))
+    costs = [0] + list(map(int, input().split()))
+
+    # items = []
+    #
+    # for (memory, price) in zip(memories, costs) :
+    #     items.append((memory, price))
+    #
+    # items.sort(key=lambda x:x[0])
+    
+    cost_max = sum(costs)
+    dp = [[0] * (cost_max + 1) for _ in range(N + 1)]
+    answer = cost_max + 1
+
+    for idx_app in range(1, N + 1) :
+        for cost_curr in range(cost_max + 1) :
+            if cost_curr < costs[idx_app] :
+                dp[idx_app][cost_curr] = dp[idx_app - 1][cost_curr]
+            else :
+                dp[idx_app][cost_curr] = max(dp[idx_app - 1][cost_curr - costs[idx_app]] + memories[idx_app], dp[idx_app - 1][cost_curr])
+
+            if dp[idx_app][cost_curr] >= M :
+                answer = min(answer, cost_curr)
+
+    print(answer)
+
+main()
