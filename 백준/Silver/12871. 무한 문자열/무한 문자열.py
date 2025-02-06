c604f37ca2ded3f427c50ca0d84d85c66aaccb48
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-02-06 22:21:09 +0900

    [Silver V] Title: 무한 문자열, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/12871. 무한 문자열/무한 문자열.py
+++ 백준/Silver/12871. 무한 문자열/무한 문자열.py
@@ -1,31 +1,6 @@
-def gcd(a, b) :
-    while b:
-        a, b = b, a%b
-    return a
-def lcm(a, b) :
-    return abs(a * b) // gcd(a, b)
-
 def main() :
-    s = input()
-    t = input()
+    a = input()
+    b = input()
+    print(int(a+b == b+a))
 
-    ls = len(s)
-    lt = len(t)
-
-    nums = lcm(ls, lt)
-    if nums == -1 :
-        return 0
-
-    else :
-        cnt_s = nums // ls
-        cnt_t = nums // lt
-
-        fs = s * cnt_s
-        ft = t * cnt_t
-
-        if fs == ft :
-            return 1
-        else :
-            return 0
-
-print(main())
+main()
