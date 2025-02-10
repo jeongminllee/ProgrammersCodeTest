d8ac98a56dbd623c0548c5fe679bc08405be2110
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-02-10 16:56:37 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1016 ms, Memory: 238560 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,7 +1,3 @@
-import sys
-input = sys.stdin.readline 
-sys.setrecursionlimit(10 ** 5)
-
 def main() :
     n = int(input())
     arr = [0] + list(map(int, input().split()))
@@ -10,27 +6,35 @@ def main() :
     finished = [0] * (n + 1)
     cnt = 0
 
-    def dfs(v) :
-        nonlocal cnt
-        visited[v] = 1
-        nxt_v = arr[v]
-        # 아직 방문하지 않은 학생이면 DFS 재귀 호출
-        if not visited[nxt_v] :
-            dfs(nxt_v)
+    for i in range(1, n + 1) :
+        if visited[i] :
+            continue
 
-        # 이미 방문한 학생인데, 아직 DFS 처리가 끝나지 않았다면 사이클(팀)을 발견
-        elif not finished[nxt_v] :
-            tmp = nxt_v
-            # 사이클에 포함되는 학생들을 센다
-            while tmp != v :
-                cnt += 1
-                tmp = arr[tmp]
-            cnt += 1    # 현재 v도 사이클에 포함
-        finished[v] = 1 # DFS 처리 완료
+        # 각 DFS마다 현재 경로를 기록
+        stack = []
+        cur = i
 
-    for i in range(1, n + 1):
-        if not visited[i] :
-            dfs(i)
+        while True :
+            visited[cur] = 1
+            stack.append(cur)
+            nxt = arr[cur]
+
+            # 아직 방문하지 않았다면 계속 진행
+            if not visited[nxt] :
+                cur = nxt
+                continue
+
+            # 방문한 적은 있지만, DFS 처리가 끝나지 않은 경우 => 현재 경로에 있으므로 사이클 발견
+            if not finished[nxt] :
+                # 스택에서 nxt가 처음 등장한 인덱스를 찾고, 그 이후의 노드들이 사이클에 속함
+                idx = stack.index(nxt)
+                cnt += len(stack) - idx
+            # 더 이상 진행할 수 없으므로 탈출
+            break
+
+        # 현재 DFS 경로에 있는 모든 노드에 대해 finished 처리
+        for node in stack :
+            finished[node] = 1
 
     print(n - cnt)
 
