1f6b556f2290089b35c4b42c79dcf90c2801deec
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-02-18 17:14:32 +0900

    [Gold V] Title: 보드게임, Time: 560 ms, Memory: 214792 KB -BaekjoonHub

--- 백준/Gold/32249. 보드게임/보드게임.py
+++ 백준/Gold/32249. 보드게임/보드게임.py
@@ -6,43 +6,23 @@ def sol_32249() :
     Alice = [list(input().rstrip()) for _ in range(N)]
     Bob = [list(input().rstrip()) for _ in range(N)]
 
-    AcntB = sum(i == 'B' for j in Alice for i in j)
-    sB = {i for i in range(M) if Alice[-1][i]=='B'}
-
-    BcntA = sum(i == 'A' for j in Bob for i in j)
-    sA = {i for i in range(M) if Bob[-1][i] == 'A'}
-
-    def check_cond() :
-        Aturn = 1 + (AcntB - bool(sB)) * 2
-        Bturn = (1 + BcntA-bool(sA)) * 2
-        print(['Bob', 'Alice'][Aturn < Bturn])
-
-    check_cond()
+    alice_bottom_b_cnt = Alice[-1].count('B')
+    bob_bottom_a_cnt = Bob[-1].count('A')
+    print('Alice' if alice_bottom_b_cnt > 0 or bob_bottom_a_cnt == 0 else 'Bob')
 
     q = int(input())
     for _ in range(q) :
-        r1, c1, r2, c2 = map(int, input().split())
-        r1, c1, r2, c2 = r1-1, c1-1, r2-1, c2-1
-        achange = Alice[r1][c1]
-        bchange = Bob[r2][c2]
-        if achange != bchange :
-            Bob[r2][c2] = achange
-            Alice[r1][c1] = bchange
-            if achange == 'B' :
-                AcntB -= 1
-                if r1 == N - 1 :
-                    sB.remove(c1)
-                BcntA -= 1
-                if r2 == N - 1 :
-                    sA.remove(c2)
+        r1, c1, r2, c2 = [int(x) - 1 for x in input().split()]
+        alice_card, bob_card = Alice[r1][c1], Bob[r2][c2]
+        if alice_card != bob_card :
+            diff = 1 if alice_card == 'A' else -1
+            if r1 == N - 1 :
+                alice_bottom_b_cnt += diff
+            if r2 == N - 1 :
+                bob_bottom_a_cnt += diff
 
-            else :
-                BcntA += 1
-                if r2 == N - 1 :
-                    sA.add(c2)
-                AcntB += 1
-                if r1 == N - 1 :
-                    sB.add(c1)
+        Alice[r1][c1], Bob[r2][c2] = bob_card, alice_card
 
-        check_cond()
+        print('Alice' if alice_bottom_b_cnt > 0 or bob_bottom_a_cnt == 0 else 'Bob')
+
 sol_32249()
