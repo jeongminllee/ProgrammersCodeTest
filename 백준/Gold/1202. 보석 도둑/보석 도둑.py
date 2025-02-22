20ed375aef9a468836594ba68e7d99694d1e0746
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-02-22 16:49:50 +0900

    [Gold II] Title: 보석 도둑, Time: 888 ms, Memory: 172992 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,25 +1,31 @@
 import sys
-import heapq
 input = sys.stdin.readline
+
+import heapq
 
 N, K = map(int, input().split())
 jewels = []
 bags = []
+
 for _ in range(N) :
     m, v = map(int, input().split())
-    heapq.heappush(jewels, (m, v))   # 힙에 푸시
+    heapq.heappush(jewels, (m, v))
 
 for _ in range(K) :
-    bags.append(int(input()))      # 가방 무게 정보
+    bags.append(int(input()))
+
 bags.sort()
 
-total_val, capable_jewels = 0, []
+total_val = 0
+capable_jewels = []
+
 for bag in bags :
-    while jewels and bag >= jewels[0][0] :      # bag의 무게가 jewels의 무게보다 더 클 경우
-        m, v = heapq.heappop(jewels)            # 힙에서 팝
-        heapq.heappush(capable_jewels, -v)      # 힙에 푸시
+    while jewels and bag >= jewels[0][0] :
+        m, v = heapq.heappop(jewels)
+        heapq.heappush(capable_jewels, -v)
     if capable_jewels :
-        total_val -= heapq.heappop(capable_jewels)  # total_val 갱신
+        total_val -= heapq.heappop(capable_jewels)
     elif not jewels :
         break
+
 print(total_val)
