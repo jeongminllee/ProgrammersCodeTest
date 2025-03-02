91e68acfd2f0b10e869187627a746110a3f4bc4c
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-03-02 16:09:19 +0900

    [Gold II] Title: 보석 도둑, Time: 824 ms, Memory: 172512 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -7,23 +7,25 @@ def sol_1202() :
     N, K = map(int, input().split())
     jewels = []
     bags = []
+
     for _ in range(N) :
         m, v = map(int, input().split())
         heapq.heappush(jewels, (m, v))  # 힙에 푸시
 
     for _ in range(K) :
-        bags.append(int(input()))   # 가방 무게 정보
+        bags.append(int(input()))       # 가방 무게 정보
     bags.sort()
 
     total_val, capable_jewels = 0, []
     for bag in bags :
         while jewels and bag >= jewels[0][0] :  # bag의 무게가 jewels의 무게보다 더 클 경우
-            m, v = heapq.heappop(jewels)        # 힙에서 팝
-            heapq.heappush(capable_jewels, -v)  # 힙에 푸시
+            m, v = heapq.heappop(jewels)        # 힙에 팝
+            heapq.heappush(capable_jewels, -v)  # 힙에 푸시 (-로 함으로써 큰 값으로 정렬)
         if capable_jewels :
-            total_val -= heapq.heappop(capable_jewels)  # total_val 갱신
+            total_val -= heapq.heappop(capable_jewels)  # total_val 갱신 (다시 - 함으로써 + 로 바꿔서 갱신)
         elif not jewels :
             break
+            
     print(total_val)
-
+    
 sol_1202()
