0134c44756b35bea505c856865dcdea97eec4a3c
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-03-06 08:29:37 +0900

    [Bronze I] Title: 세로 달력, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/26148. 세로 달력/세로 달력.py
+++ 백준/Bronze/26148. 세로 달력/세로 달력.py
@@ -1,7 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-cale = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 def sol_26148() :
     # 윤년의 조건
     # 4의 배수, not 100의 배수
@@ -13,9 +12,8 @@ def sol_26148() :
     if (N % 4 == 0 and N % 100 != 0) or (N % 400 == 0) :
         ans += 1
 
-    ans += sum(cale) - 28 * 12
+    ans += 365 - 28 * 12
 
     print(ans)
-
 
 sol_26148()
