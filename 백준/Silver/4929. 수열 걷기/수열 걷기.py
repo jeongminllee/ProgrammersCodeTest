03971a348708bdeb6f27f028d4cce3b8d5656bbf
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-03-06 20:18:13 +0900

    [Silver II] Title: 수열 걷기, Time: 104 ms, Memory: 110888 KB -BaekjoonHub

--- 백준/Silver/4929. 수열 걷기/수열 걷기.py
+++ 백준/Silver/4929. 수열 걷기/수열 걷기.py
@@ -1,39 +1,33 @@
 import sys
 input = sys.stdin.readline
 
-from bisect import bisect_left, bisect_right
 def sol_4929() :
     while True :
-        lst1 = list(map(int, input().split()))
-        if sum(lst1) == 0 :
+        n, *arr1 = map(int, input().split())
+        if n == 0 :
             break
-        n = lst1[0]
-        sequence1 = lst1[1:]
-        lst2 = list(map(int, input().split()))
-        m = lst2[0]
-        sequence2 = lst2[1:]
+        m, *arr2 = map(int, input().split())
+
+        arr1.reverse()
+        arr2.reverse()
 
         ans = 0
-        idx1 = 0    # 수열 1의 동일한 숫자의 index
-        idx2 = 0    # 수열 2의 동일한 숫자의 index
+        c = [0, 0]
 
-        for num in sequence1 :
-            # 동일한 숫자가 있으면
-            if bisect_right(sequence2, num) - bisect_left(sequence2, num) > 0 :
-                # 첫 번째 수열의 해당 숫자까지의 합
-                sum_seq1 = sum(sequence1[idx1:bisect_right(sequence1, num)])
-                # 두 번째 수열의 해당 숫자까지의 합
-                sum_seq2 = sum(sequence2[idx2:bisect_right(sequence2, num)])
+        while True :
+            if not arr1 or not arr2 :
+                ans += max(c[0] + sum(arr1), c[1] + sum(arr2))
+                break
 
-                # 각 수열의 누적합 중 큰 값을 ans에 누적
-                ans += max(sum_seq1, sum_seq2)
+            if arr1[-1] < arr2[-1] :
+                c[0] += arr1.pop()
+            elif arr1[-1] > arr2[-1] :
+                c[1] += arr2.pop()
+            else :
+                ans += max(c) + arr1.pop()
+                arr2.pop()
+                c = [0, 0]
 
-                # 각 수열의 index 갱신
-                idx1 = bisect_right(sequence1, num)
-                idx2 = bisect_right(sequence2, num)
-
-        # 마지막 길의 최댓값을 구해 ans에 추가
-        ans += max(sum(sequence1[idx1:]), sum(sequence2[idx2:]))
         print(ans)
 
 sol_4929()
