d8e194524139e722aa7b8c34e40ea24af86e2da0
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-06-12 21:37:19 +0900

    [Bronze I] Title: 약수, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.py
+++ 백준/Bronze/1037. 약수/약수.py
@@ -1,8 +1,4 @@
-N = int(input())
-lst = list(map(int, input().split()))
-lst.sort()
-# A : N의 진짜 약수가 되려면, 2 <= A <= 10 ** 6
-# 2 4 를 약수로 둔 자연수의 경우, 4와 8이 해당 될것인데, 4가 되려면 1이 포함되어야 한다.
-# 전제 조건에 위반하기 때문에 8이 정답이 될 것이다.
+A = int(input())            # N의 진짜 약수의 개수
+lst = list(map(int, input().split()))   # N의 진짜 약수
 
-print(lst[0] * lst[-1])
+print(min(lst) * max(lst))
