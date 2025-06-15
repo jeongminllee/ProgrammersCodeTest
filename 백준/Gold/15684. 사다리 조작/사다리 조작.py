2f6d20ea3ab79b2ea5067461c5409e710287937a
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-06-15 18:49:31 +0900

    [Gold III] Title: 사다리 조작, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.py
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.py
@@ -4,38 +4,50 @@ arr = [[0] * N for _ in range(H)]
 for i in range(M) :
     a, b = map(int, input().split())
     arr[a-1][b-1] = 1
+    arr[a-1][b] = 2
 
 def atoa() :
-    for i in range(N) :
-        start_num = i
+    same = 0
+    for s in range(N) :
+        now = s
         for j in range(H) :
-            if arr[j][start_num] == 1 :
-                start_num += 1
-            elif start_num > 0 and arr[j][start_num-1] == 1 :
-                start_num -= 1
+            if arr[j][now] == 1 :
+                now += 1
+            elif arr[j][now] == 2 :
+                now -= 1
+        if now == s :
+            same += 1
+    return same
 
-        if i != start_num :
-            return False
-    return True
-
-def dfs(cnt, x, y) :
+def dfs(n) :
     global res
-    if res <= cnt :
-        return
-    if atoa() :
-        res = min(res, cnt)
-        return
-    if cnt == 3 :
-        return
-    for i in range(x, H) :
-        for j in range(0, N - 1) :
-            if arr[i][j] == 0 :
-                arr[i][j] = 1
-                dfs(cnt + 1, i, j + 2)
-                arr[i][j] = 0
 
-res = 4
-dfs(0, 0, 0)
-if res > 3 :
-    res = -1
+    if res != -1 :
+        return
+
+    tmp = atoa()
+
+    if tmp + (cnt - n) * 2 < N :
+        return
+
+    if n == cnt :
+        if tmp == N :
+            res = cnt
+        return
+
+    for i in range(H) :
+        for j in range(N - 1) :
+            if arr[i][j] or arr[i][j+1] :
+                continue
+                
+            arr[i][j], arr[i][j+1] = 1, 2
+            dfs(n + 1)
+            arr[i][j], arr[i][j+1] = 0, 0
+
+res = -1
+for cnt in range(4) :
+    dfs(0)
+    if res != -1 :
+        break
+
 print(res)
