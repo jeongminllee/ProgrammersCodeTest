ce1703e0feca15cb53ca5f318cdecbc8a80ba5fe
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-06-17 07:38:37 +0900

    [Silver I] Title: 로봇, Time: 104 ms, Memory: 114712 KB -BaekjoonHub

--- 백준/Silver/13901. 로봇/로봇.py
+++ 백준/Silver/13901. 로봇/로봇.py
@@ -1,16 +1,16 @@
 # 방 크기
 R, C = map(int, input().split())
 
-# 방 설정
+# 방 세팅
 arr = [[0] * C for _ in range(R)]
 
-# 장애물 설치
-K = int(input())
-for _ in range(K) :
+# 장애물
+k = int(input())
+for _ in range(k) :
     br, bc = map(int, input().split())
-    arr[br][bc] = 1
+    arr[br][bc] = 1 # 장애물 초기 설정
 
-# 로봇 초기 설정
+# 로봇 시작 위치
 sr, sc = map(int, input().split())
 arr[sr][sc] = 1
 
@@ -22,28 +22,29 @@ dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
 # 로봇 현재 위치
-cr, cc = sr, sc
+r, c = sr, sc
 
 # 로봇이 더 이상 움직이지 않을 때 까지 반복
 while True :
     moved = False   # 이번 턴에 이동 여부 확인
 
-    for dir in direction :  # 이동 시도
+    for d in direction :    # 이동 시도
         while True :
-            nr, nc = cr + dr[dir - 1], cc + dc[dir - 1] # 다음 방향 이동 시도
+            nr, nc = r + dr[d - 1], c + dc[d - 1]   # 다음 방향 이동 시도
 
             # 방문 가능한지
             if 0 <= nr < R and 0 <= nc < C and arr[nr][nc] == 0 :
                 moved = True
                 arr[nr][nc] = 1
-                cr, cc = nr, nc
+                r, c = nr, nc
 
             # 만약 불가능하면 중단
             else :
                 break
+
     # 만약 한 번도 이동하지 못했다면 종료
     if not moved :
         break
 
 # 최종 위치 출력
-print(cr, cc)
+print(r, c)
