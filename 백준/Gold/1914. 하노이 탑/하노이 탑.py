88aa0d034080bc0ac3f509225e4f747ab0781cbc
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-08-12 02:03:57 +0900

    [Gold V] Title: 하노이 탑, Time: 428 ms, Memory: 149500 KB -BaekjoonHub

--- 백준/Gold/1914. 하노이 탑/하노이 탑.py
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -3,9 +3,9 @@ def hanoi(N, a, b, c):
         print(a, c)
     else :
         hanoi(N-1, a, c, b)
-        hanoi(1, a, b, c)
+        print(a, c)
         hanoi(N-1, b, a, c)
-        
+
 N = int(input())
 print(2**N - 1)
 if N <= 20 :
