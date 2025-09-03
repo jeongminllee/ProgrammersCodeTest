60206a81b18690b052a9993921338babfe4df062
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-09-03 16:40:28 +0900

    [Gold III] Title: 거울 설치, Time: 128 ms, Memory: 114800 KB -BaekjoonHub

--- 백준/Gold/2151. 거울 설치/거울 설치.py
+++ 백준/Gold/2151. 거울 설치/거울 설치.py
@@ -1,69 +1,58 @@
-import sys, collections
+from collections import deque
 
-# 입력부
-n = int(sys.stdin.readline())
-arr = [list(sys.stdin.readline().rstrip()) for _ in range(n)]
+di = [-1, 0, 1, 0]
+dj = [0, -1, 0, 1]
 
-# dx, dy : 방향배열(북, 남, 동, 서)
-dx = [-1,1,0,0]
-dy = [0,0,1,-1]
+def bfs(si, sj, ei, ej) :
+    INF = 10**9
+    # dist[dir][i][j] : (i, j)에 dir 방향으로 들어왔을 때 필요한 최소 거울 수
+    dist = [[[INF] * N for _ in range(N)] for _ in range(4)]
+    q = deque()
 
-# open_x, open_y : 시작하는 문의 (x, y)좌표
-open_x, open_y = -1, -1
-# close_x, close_y : 끝나는 문의 (x, y)좌표
-close_x, close_y = -1, -1
+    # 시작 칸에서 4방향 모두 비용 0으로 시작
+    for d in range(4) :
+        dist[d][si][sj] = 0
+        q.append((si, sj, d))
 
-for i in range(n):
-    for j in range(n):
-        if arr[i][j] == '#':
-            if open_x == -1 and open_y == -1:
-                open_x, open_y = i, j
-            else:
-                close_x, close_y = i, j
+    while q:
+        ci, cj, dr = q.popleft()
 
-# check : 현재 i행 j열이고 dir방향으로 들어올 때 필요한 최소 거울의 갯수
-check = [[[-1] * 4 for _ in range(n)] for _ in range(n)]
-q = collections.deque()
+        # 직진(0비용) : 현재 방향으로만 한 칸 전진
+        ni, nj = ci + di[dr], cj + dj[dr]
+        if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] != '*' :
+            # 0비용 갱신 -> appendleft
+            if dist[dr][ni][nj] > dist[dr][ci][cj] :
+                dist[dr][ni][nj] = dist[dr][ci][cj]
+                q.appendleft((ni, nj, dr))
 
-# 시작 지점에서 시작 방향 설정
-for a in range(4):
-    q.append((open_x, open_y, a))
-    check[open_x][open_y][a] = 0
+            # 거울 설치 간으 칸이면 1비용으로 직각 전환
+            if arr[ni][nj] == '!' :
+                # dr가 짝수면 홀수, 홀수면 짝수로 수직
+                turn_dirs = (1, 3) if dr % 2 == 0 else (0, 2)
+                for nd in turn_dirs :
+                    if dist[nd][ni][nj] > dist[dr][ci][cj] + 1 :
+                        dist[nd][ni][nj] = dist[dr][ci][cj] + 1
+                        q.append((ni, nj, nd))
+
+    res = min(dist[d][ei][ej] for d in range(4))
+    return res
     
-while q:
-    x, y, dir = q.popleft()
-    # 현재 지점이 닫는 문이면 정답 출력 후 바로 종료
-    if x == close_x and y == close_y:
-        print(check[x][y][dir])
-        break
-    nx, ny = x + dx[dir], y + dy[dir]
-    # 범위에 만족하는 경우
-    if 0 <= nx < n and 0 <= ny < n:
-    
-        # 벽이 아닌 경우
-        if arr[nx][ny] != '*':
-        
-            # 처음 방문하는 곳이거나 이전에 방문했던 점보다 더 적은 횟수로 갈 수 있다면
-            # 상태공간 갱신 및 큐의 앞부분에 삽입 
-            # 거울을 놓을 수 있어도 놓지 않는 경우거나 아예 거울을 놓을 수 없는 공간 두 가지 모두 고려가능
-            if check[nx][ny][dir] == -1  or check[nx][ny][dir] > check[x][y][dir]:
-                check[nx][ny][dir] = check[x][y][dir]
-                q.appendleft((nx, ny, dir))
-                
-            # 거울을 놓는 경우
-            if arr[nx][ny] == '!':
-                # 방향이 북이거나 남이면 동,서로 반사
-                # 이 때 거울을 설치하기 때문에 큐의 뒷부분에 삽입
-                if dir < 2:
-                    for n_dir in range(2, 4):
-                        if check[nx][ny][n_dir] == -1 or check[nx][ny][n_dir] > check[x][y][dir] + 1:
-                            check[nx][ny][n_dir] = check[x][y][dir] + 1
-                            q.append((nx, ny, n_dir))
-                            
-                # 방향이 동이거나 서라면 남,북으로 반사
-                # 이 때 거울을 설치하기 때문에 큐의 뒷부분에 삽입
-                else:
-                    for n_dir in range(2):
-                        if check[nx][ny][n_dir] == -1 or check[nx][ny][n_dir] > check[x][y][dir] + 1:
-                            check[nx][ny][n_dir] = check[x][y][dir] + 1
-                            q.append((nx, ny, n_dir))
+N = int(input())
+arr = [list(input().strip()) for _ in range(N)]
+
+# "#" : 문이 설치된 곳, 항상 2개
+# "." : 아무 것도 없는 곳으로 빛이 통과함
+# "!" : 거울 설치할 수 있는 위치
+# "*" : 빛이 통과할 수 없는 벽
+
+# 거울을 설치하면 무조건 직각으로 꺾여야함. 
+# 그 방향이 2개로 나누어짐.
+doors = []
+# 문 좌표 넣기
+for i in range(N) :
+    for j in range(N) :
+        if arr[i][j] == '#' :     # 문 좌표 
+            doors.append((i, j))
+            
+(si, sj), (ei, ej) = doors    # 문은 2개 확정이기 때문에
+print(bfs(si, sj, ei, ej))
