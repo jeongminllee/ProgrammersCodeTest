38269f80583c5f6adcef6b2508398b1b9f599e17
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-09-09 23:23:54 +0900

    [Silver IV] Title: 수 찾기, Time: 180 ms, Memory: 145304 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,40 +1,15 @@
-import sys
-input = sys.stdin.readline
+def main(N, M, num_set, num_list) :
+    for num in num_list :
+        if num in num_set :
+            print(1)
+        else :
+            print(0)
 
-N = int(input())
-a = set(map(int, input().split()))
+    return
 
-M = int(input())
-b = list(map(int, input().split()))
-
-for j in b :
-    if j in a :
-        print(1)
-    else :
-        print(0)
-
-# N = int(input())
-# A = list(map(int, input().split()))
-# M = int(input())
-# lst = list(map(int, input().split()))
-# A.sort()
-# 
-# # 이분 탐색
-# for num in lst :
-#     lt, rt = 0, N - 1
-#     isExist = False
-# 
-#     # 이분 탐색 시작
-#     while lt <= rt :    # lt가 rt보다 커지면 반복문 탈출
-#         mid = (lt + rt) // 2
-#         if num == A[mid] :  # num(목표값)이 A[mid]값과 같다면 (목표값 존재여부를 알았다면)
-#             isExist = True  # isExist True로 변경
-#             print(1)        # 1 출력
-#             break           # 반복문 탈출
-#         elif num > A[mid] : # A[mid]가 num보다 작으면
-#             lt = mid + 1    # lt를 높임
-#         else :              # A[mid]가 num보다 높으면
-#             rt = mid - 1    # rt를 낮춤
-
-#     if not isExist :        # 찾지 못한 경우
-#         print(0)            # 0 출력
+if __name__ == "__main__" :
+    N = int(input())
+    num_set = set(map(int, input().split()))
+    M = int(input())
+    num_list = list(map(int, input().split()))
+    main(N, M, num_set, num_list)
