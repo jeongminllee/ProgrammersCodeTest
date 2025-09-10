c42c4d8841d9fa27305037476ff0def663b204fd
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-09-10 15:17:28 +0900

    [Gold V] Title: 김밥천국의 계단, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.py
+++ 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.py
@@ -1,18 +1,20 @@
-def main(N, K) :
-    INF = 10 ** 18
-    dp = [INF] * (N + 1)
-    dp[0] = 0
-    for i in range(N) :
-        if dp[i] + 1 < dp[i + 1] :
-            dp[i + 1] = dp[i] + 1
+N, K = map(int, input().split())
+for _ in range(K) :
+    # 계산 steps = steps + 1, (steps + steps//2 = steps * 3 // 2)
+    # 결국 N == steps 가 되어야 하므로 N = steps + 1, (steps + steps//2 = steps * 3 // 2)
+    # steps = N - 1, N * 2 // 3
+    # 나누기 3을 하게 되면 오차가 발생하기 때문에 보정을 해주어야 함.
+    # i = 2t 면, i + i/2 = 2t + t = 3t = N 이기 때문에 i = 2n / 3
+    # i = 2t + 1 이면, i + i/2 = 2t+1 + t = 3t+1 = N 이기 때문에 i = 2n//3 + 1
+    if N % 3 <= 1 :
+        N = min(N - 1, 2 * N // 3 + N % 3)
+    else :
+        N -= 1
 
-        if i >= 2 :
-            j = i + i // 2
-            if j <= N and dp[i] + 1 < dp[j] :
-                dp[j] = dp[i] + 1
+    if N <= 0 :
+        break
 
-    return dp[N] <= K
-
-if __name__ == "__main__" :
-    N, K = map(int, input().split())
-    print("minigimbob" if main(N, K) else "water")
+if N <= 0:
+    print("minigimbob")
+else :
+    print("water")
