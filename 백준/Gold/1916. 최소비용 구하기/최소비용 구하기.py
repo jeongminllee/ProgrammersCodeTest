6fd663d630ce5d32d339d32dc0c9d9d896599491
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-09-23 05:28:01 +0900

    [Gold V] Title: 최소비용 구하기, Time: 248 ms, Memory: 122948 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,31 +1,36 @@
 import heapq
 
-n = int(input())
-m = int(input())
-graph = [[] for _ in range(n + 1)]
-dst = [float('inf')] * (n + 1
-                        )
-for _ in range(m) :
-    # a: 출발 도시 번호, b: 도착 도시 번호, c: 버스 비용
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-
-s, e = map(int, input().split())
-def dijkstra(start) :
+def deijkstra(start, end) :
     q = []
-    heapq.heappush(q, (0, start))
-    dst[start] = 0
+    distances = [INF] * (N + 1)
+
+    heapq.heappush(q, (start, 0))
+    distances[start] = 0
 
     while q :
-        dist, now = heapq.heappop(q)
-        if dst[now] < dist :
+        curr, dist = heapq.heappop(q)
+        if distances[curr] < dist :
             continue
 
-        for i in graph[now] :
-            cost = dist + i[1]
-            if cost < dst[i[0]] :
-                dst[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+        for node in edges[curr] :
+            nxt_node, nxt_dist = node
+            if distances[nxt_node] > dist + nxt_dist :
+                distances[nxt_node] = dist + nxt_dist
+                heapq.heappush(q, (nxt_node, distances[nxt_node]))
 
-dijkstra(s)
-print(dst[e])
+    return distances[end]
+#############
+# 입력
+N = int(input())    # N 개의 도시
+M = int(input())    # M 개의 버스
+edges = [[] for _ in range(N+1)]
+
+for _ in range(M) :
+    u, v, w = map(int, input().split())
+    edges[u].append((v, w))
+
+start, end = map(int, input().split())
+#############
+INF = 1<<32
+res = deijkstra(start, end)
+print(res)
