b2f9563d626b6ddc21a27b142a9f608c981b02b4
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-09-23 16:53:18 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 140 ms, Memory: 116236 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.py
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.py
@@ -2,37 +2,46 @@ from collections import deque
 
 def bfs(si, sj) :
     q = deque()
-    q.append((si, sj, 0, 0))   # si, sj, cnt
-    check = [[[0] * (1 << 6) for _ in range(M)] for _ in range(N)]
-    check[si][sj][0] = 1
+    q.append((si, sj, 0))
+
+    v = [[[-1] * (1<<6) for _ in range(M)] for _ in range(N)]
+    v[si][sj][0] = 0
 
     while q :
-        ci, cj, cnt, key = q.popleft()
+        ci, cj, key = q.popleft()
+
         if arr[ci][cj] == '1' :
-            return cnt
+            return v[ci][cj][key]
 
         for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)) :
             ni, nj = ci + di, cj + dj
-            if 0 <= ni < N and 0 <= nj < M :
-                if not check[ni][nj][key] :
-                    if arr[ni][nj] == '1' or arr[ni][nj] == '.' :
-                        check[ni][nj][key] = True
-                        q.append((ni, nj, cnt + 1, key))
-                    elif arr[ni][nj] in keys :
-                        tmp_key = key|(1 << (ord(arr[ni][nj]) - ord('a')))
-                        check[ni][nj][tmp_key] = 1
-                        q.append((ni, nj, cnt + 1, tmp_key))
-                    elif arr[ni][nj] in doors :
-                        if key & (1 << (ord(arr[ni][nj]) - ord('A'))) :
-                            check[ni][nj][key] = True
-                            q.append((ni, nj, cnt + 1, key))
+            if 0<=ni<N and 0<=nj<M and v[ni][nj][key] == -1 :
+                if arr[ni][nj] == '#' :
+                    continue
+                elif arr[ni][nj] in keys :
+                    nxt_key = key | (1<<(ord(arr[ni][nj]) - ord('a')))
+                    v[ni][nj][nxt_key] = v[ci][cj][key] + 1
+                    q.append((ni, nj, nxt_key))
+
+                elif arr[ni][nj] in doors :
+                    if key & (1<<(ord(arr[ni][nj]) - ord('A'))) :
+                        # arr[ni][nj] = '.'
+                        v[ni][nj][key] = v[ci][cj][key] + 1
+                        q.append((ni, nj, key))
+                    else :
+                        continue
+                else :
+                    v[ni][nj][key] = v[ci][cj][key] + 1
+                    q.append((ni, nj, key))
+
     return -1
 
-N, M = map(int, input().split())    # 세로, 가로
+N, M = map(int, input().split())
 arr = [list(input()) for _ in range(N)]
 
-keys = set(['a', 'b', 'c', 'd', 'e', 'f'])
-doors = set(['A', 'B', 'C', 'D', 'E', 'F'])
+keys = set(('a', 'b', 'c', 'd', 'e', 'f'))
+doors = set(('A', 'B', 'C', 'D', 'E', 'F'))
+
 
 for i in range(N) :
     for j in range(M) :
@@ -40,5 +49,4 @@ for i in range(N) :
             si, sj = i, j
             arr[i][j] = '.'
 
-res = bfs(si, sj)
-print(res)
+print(bfs(si, sj))
