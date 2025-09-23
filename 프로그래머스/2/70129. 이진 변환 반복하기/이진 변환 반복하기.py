a9ba569dabb2e8d030e0042de5a502b116742485
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-09-23 21:03:37 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.02 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,9 +1,21 @@
 def solution(s):
-    cnt_rm = cnt_2 = 0
+    answer = [0, 0] # 이진 변환 횟수, 제거된 모든 0의 개수
     
-    while s != "1" :
-        cnt_2 += 1
-        cnt_rm += s.count("0")
-        s = bin(s.count("1"))[2:]
+    cnt_1 = cnt_0 = 0
     
-    return [cnt_2, cnt_rm]
+    while s != '1' :
+        num = ''
+        cnt_1 = s.count('1')
+        cnt_0 = s.count('0')
+        
+        answer[1] += cnt_0  # 제거된 모든 0의 개수
+        
+        while cnt_1 != 0 :
+            num += str(cnt_1 % 2)
+            cnt_1 //= 2
+        
+        answer[0] += 1
+        s = num[::-1]
+        
+    
+    return answer
