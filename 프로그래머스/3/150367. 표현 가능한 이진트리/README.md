eec48a1e3ed97c18be262db75e92b85ac4a5b2d2
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-10-03 06:18:24 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 47.13 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/README.md
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/README.md
@@ -1,10 +1,10 @@
 # [level 3] 표현 가능한 이진트리 - 150367 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150367) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150367?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.69 MB, 시간: 56.33 ms
+메모리: 10.4 MB, 시간: 47.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 03일 01:09:00
+2025년 10월 03일 06:18:21
 
 ### 문제 설명
 
--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
@@ -1,23 +1,25 @@
 def solution(numbers):
-    def valid(s, left, right) :
-        if left >= right :
-            return True
-        
-        mid = (left + right) // 2
-        if s[mid] == '0' :
-            if '1' in s[left:mid] or '1' in s[mid+1:right+1] :
-                return False
-        
-        return valid(s, left, mid-1) and valid(s, mid+1, right)
-    
     answer = []
-    for num in numbers :
-        s = bin(num)[2:]
-        L = 1
-        while L < len(s) :
-            L = 2*L + 1
-        s = s.zfill(L)
-        
-        answer.append(int(valid(s, 0, len(s)-1)))
-    
+    for number in numbers :
+        bits = num_to_bin(number)
+        answer.append(int(trees(bits, 0, len(bits)-1)))
     return answer
+
+def num_to_bin(number) :
+    bins = bin(number)[2:]
+    n = 1
+    
+    while n < len(bins) :
+        n = 2*n + 1
+    return bins.zfill(n)
+
+def trees(number, left, right) :
+    if left >= right :
+        return True
+    
+    mid = (left + right) // 2
+    if number[mid] == '0' :
+        if '1' in number[left:mid] or '1' in number[mid+1:right+1] :
+            return False
+    
+    return trees(number, left, mid-1) and trees(number, mid+1, right)
