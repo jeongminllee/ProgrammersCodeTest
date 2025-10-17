e643d6d672cf85b1eb8176172d775f88fb8a80dd
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-10-17 17:04:09 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 865.86 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,30 +1,33 @@
 def solution(board, skill):
-    n, m, cnt = len(board), len(board[0]), 0
-    ac_sum = [[0] * (m+1) for _ in range(n+1)]
+    answer = 0
+    n, m = len(board), len(board[0])
+    prefix_sum = [[0 for _ in range(m+1)] for _ in range(n+1)]
     
-    for at, r1, c1, r2, c2, degree in skill :
-        val = 0
-        if at == 1 :
-            val = -degree
-        else :
-            val = degree
+    for a_or_h, r1, c1, r2, c2, degree in skill :
+        if a_or_h == 1 :
+            degree = -degree
         
-        ac_sum[r1][c1] += val
-        ac_sum[r2+1][c2+1] += val
-        ac_sum[r1][c2+1] -= val
-        ac_sum[r2+1][c1] -= val
+        prefix_sum[r1][c1] += degree
+        prefix_sum[r2+1][c2+1] += degree
+        prefix_sum[r2+1][c1] -= degree
+        prefix_sum[r1][c2+1] -= degree
         
+    # print(*prefix_sum, sep='\n')
+    # print('=' * 10)
     for i in range(n+1) :
         for j in range(m) :
-            ac_sum[i][j+1] += ac_sum[i][j]
+            prefix_sum[i][j+1] += prefix_sum[i][j]
+    
     for j in range(m+1) :
         for i in range(n) :
-            ac_sum[i+1][j] += ac_sum[i][j]
+            prefix_sum[i+1][j] += prefix_sum[i][j]
+            
+    # print(*prefix_sum, sep='\n')
     
     for i in range(n) :
         for j in range(m) :
-            board[i][j] += ac_sum[i][j]
+            board[i][j] += prefix_sum[i][j]
             if board[i][j] > 0 :
-                cnt += 1
+                answer += 1
                 
-    return cnt
+    return answer
