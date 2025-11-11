a7094ca46788f71d1f44f2b988b27e0530f9cec6
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-11-11 16:40:34 +0900

    [Gold II] Title: 스타트 택시, Time: 168 ms, Memory: 112144 KB -BaekjoonHub

--- 백준/Gold/19238. 스타트 택시/스타트 택시.py
+++ 백준/Gold/19238. 스타트 택시/스타트 택시.py
@@ -1,95 +1,96 @@
-from collections import deque
+import heapq
 
 di = [-1, 1, 0, 0]
 dj = [0, 0, -1, 1]
 
-def in_range(i, j, N) :
-    return 0<=i<N and 0<=j<N
+def find_passenger(ti, tj) :
+    global board, fuel
+    N = len(board)
+    q = []
+    visited = [[0] * N for _ in range(N)]
 
-def go_dest(i, j, N, board, passengers) :
-    dest_i, dest_j = passengers[(i, j)]
-    del passengers[(i, j)]
-    q = deque()
-    q.append((i, j, 0))
-    visited = set()
-    visited.add((i, j))
+    heapq.heappush(q, (0, ti, tj))
+    visited[ti][tj] = 1
 
     while q :
-        ci, cj, cnt = q.popleft()
-        if (ci, cj) == (dest_i, dest_j) :
-            return ci, cj, cnt
+        depth, ci, cj = heapq.heappop(q)
+
+        if depth > fuel :
+            return -1
+
+        if board[ci][cj] < 0 :
+            return ci, cj, depth
+
         for d in range(4) :
-            ni, nj, nxt = ci + di[d], cj + dj[d], cnt + 1
-            if (ni, nj) in visited :
-                continue
-            if in_range(ni, nj, N) and board[ni][nj] != 1 :
-                q.append((ni, nj, nxt))
-                visited.add((ni, nj))
+            ni, nj = ci + di[d], cj + dj[d]
+            if 0<=ni<N and 0<=nj<N and visited[ni][nj] == 0 :
+                if board[ni][nj] <= 0 :
+                    heapq.heappush(q, (depth+1, ni, nj))
+                    visited[ni][nj] = 1
 
-    return dest_i, dest_j, 10 ** 8
+    return -1
 
+def move_passenger(si, sj, ei, ej) :
+    global board, fuel
+    N = len(board)
+    q = []
+    visited = [[0] * N for _ in range(N)]
 
-def bfs(N, M, fuels, board, si, sj, passengers) :
-    q = deque()
-    q.append((si, sj, 0))
-    visited = set()
-    visited.add((si, sj))
-    candidate = []
-    min_distance = 10 ** 8
+    q.append((0, si, sj))
+    visited[si][sj] = 1
 
     while q :
-        ci, cj, cnt = q.popleft()
-        if cnt > min_distance :
-            break
+        depth, ci, cj = q.pop(0)
 
-        if (ci, cj) in passengers :
-            candidate.append((ci, cj))
-            min_distance = cnt
+        if depth > fuel :
+            return -1
+
+        if (ci, cj) == (ei, ej) :
+            return depth
 
         for d in range(4) :
-            ni, nj, nxt = ci + di[d], cj + dj[d], cnt + 1
-            if (ni, nj) in visited :
-                continue
+            ni, nj = ci + di[d], cj + dj[d]
+            if 0<=ni<N and 0<=nj<N and visited[ni][nj] == 0 :
+                if board[ni][nj] <= 0 :
+                    q.append((depth+1, ni, nj))
+                    visited[ni][nj] = 1
 
-            if in_range(ni, nj, N) and board[ni][nj] != 1 :
-                q.append((ni, nj, nxt))
-                visited.add((ni, nj))
-
-    return candidate, min_distance
+    return -1
 
 def main() :
-    N, M, fuels = map(int, input().split())
+    global board, fuel
+    N, M, fuel = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(N)]
-    si, sj = map(int, input().split())
-    si, sj = si-1, sj-1
-    passengers = {}
+    ti, tj = map(int, input().split())
+    ti, tj = ti-1, tj-1
+    destinations = []
+    for i in range(1, M+1) :
+        si, sj, ei, ej = map(int, input().split())
+        board[si-1][sj-1] = -i
+        destinations.append((ei-1, ej-1))
 
-    for _ in range(M) :
-        p1, p2, p3, p4 = map(int, input().split())
-        passengers[(p1-1, p2-1)] = (p3-1, p4-1)
+    passengers = M
 
-    answer = 0
+    while passengers > 0 :
+        result = find_passenger(ti, tj)
+        if result == -1 :
+            return -1
+        ti, tj, cost = result
+        fuel -= cost
 
-    while fuels > 0 and len(passengers) != 0 :
-        cand, used_feul = bfs(N, M, fuels, board, si, sj, passengers)
-        if used_feul > fuels or len(cand) == 0 :
-            answer = -1
-            break
+        person = -board[ti][tj] - 1
+        board[ti][tj] = 0
 
-        si, sj = sorted(cand)[0]
+        cost = move_passenger(ti, tj, destinations[person][0], destinations[person][1])
 
-        fuels -= used_feul
-        dest_i, dest_j, used_feul = go_dest(si, sj, N, board, passengers)
-        if used_feul > fuels :
-            answer -= 1
-            break
-        fuels += used_feul
-        si, sj = dest_i, dest_j
+        if cost == -1 :
+            return -1
+        ti, tj = destinations[person][0], destinations[person][1]
+        fuel += cost
 
-    if answer == -1 :
-        return -1
-    else :
-        return fuels
+        passengers -= 1
+
+    return fuel
 
 if __name__ == "__main__" :
     print(main())
