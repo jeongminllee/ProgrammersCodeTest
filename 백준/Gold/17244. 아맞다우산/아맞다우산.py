64b2c03eb65b9ef67ef9b767c304716fc3acafe2
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-11-24 20:21:23 +0900

    [Gold II] Title: 아맞다우산, Time: 124 ms, Memory: 115380 KB -BaekjoonHub

--- 백준/Gold/17244. 아맞다우산/아맞다우산.py
+++ 백준/Gold/17244. 아맞다우산/아맞다우산.py
@@ -1,48 +1,56 @@
 from collections import deque
 
+di = [-1, 1, 0, 0]
+dj = [0, 0, -1, 1]
+
 def bfs(si, sj, ei, ej, cnt) :
     q = deque()
-    v = [[[-1] * (1<<cnt) for _ in range(M)] for _ in range(N)]
-
-    q.append((si, sj, 0))
+    q.append((si, sj, 0))   # si, sj, 물건 갯수
     v[si][sj][0] = 0
 
     while q :
         ci, cj, bit = q.popleft()
-        if (ci, cj) == (ei, ej) and bit == 2 ** cnt - 1 :
-            return v[ci][cj][bit]
+        curr = v[ci][cj][bit]
 
-        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)) :
-            ni, nj = ci + di, cj + dj
+        if (ci, cj) == (ei, ej) and bit == 2 ** cnt - 1:
+            return curr
 
-            if 0<=ni<N and 0<=nj<M and v[ni][nj][bit] == -1 :
-                if arr[ni][nj] == '#' :
+        for d in range(4) :
+            ni, nj = ci + di[d], cj + dj[d]
+            if 0 <= ni < M and 0 <= nj < N and v[ni][nj][bit] == -1 :
+                if arr[ni][nj] == "#" :
                     continue
-                elif arr[ni][nj] == 'X' :
-                    nxt_cnt = bit | (1<<tbl[(ni, nj)])
-                    v[ni][nj][nxt_cnt] = v[ci][cj][bit] + 1
-                    q.append((ni, nj, nxt_cnt))
+                elif arr[ni][nj] == "X" :
+                    nxt_bit = bit | (1<<tbl[(ni, nj)])
+                    q.append((ni, nj, nxt_bit))
+                    v[ni][nj][nxt_bit] = curr + 1
                 else :
-                    v[ni][nj][bit] = v[ci][cj][bit] + 1
                     q.append((ni, nj, bit))
+                    v[ni][nj][bit] = curr + 1
 
     return -1
+\
+def main() :
+    global N, M, arr, v, tbl
+    N, M = map(int, input().split())
+    arr = [list(input().rstrip()) for _ in range(M)]
+    cnt = 0
+    si = sj = ei = ej = 0
+    tbl = {}    # 중복 처리를 위해 물건 위치를 담자.
+    for i in range(M) :
+        for j in range(N) :
+            if arr[i][j] == "S" :
+                si, sj = i, j
+                arr[si][sj] = "."
+            elif arr[i][j] == "E" :
+                ei, ej = i, j
+            elif arr[i][j] == "X" :
+                tbl[(i, j)] = cnt
+                cnt += 1
 
-M, N = map(int, input().split())
-arr = [list(input()) for _ in range(N)]
+    v = [[[-1 for _ in range(1<<cnt)] for _ in range(N)] for _ in range(M)]
 
-cnt = 0
-tbl = {}
-for i in range(N) :
-    for j in range(M) :
-        if arr[i][j] == 'S' :
-            si, sj = i, j
-            arr[i][j] = '.'
-        if arr[i][j] == 'E' :
-            ei, ej = i, j
-            arr[i][j] = '.'
-        if arr[i][j] == 'X' :
-            tbl[(i, j)] = cnt
-            cnt += 1
+    print(bfs(si, sj, ei, ej, cnt))
 
-print(bfs(si, sj, ei, ej, cnt))
+if __name__ == "__main__" :
+    main()
