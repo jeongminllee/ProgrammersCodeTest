0512c4697422d0829e5e7881877275503e69bad6
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-12-02 07:45:11 +0900

    [Bronze II] Title: Party Medley, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/33080. Party Medley/Party Medley.py
+++ 백준/Bronze/33080. Party Medley/Party Medley.py
@@ -1,23 +1,34 @@
 def main() :
     N, M = map(int, input().split())
     lst = list(map(int, input().split()))
+    lst.sort()
 
-    rating = 0
-    res = 0
+    cnt = 0         # 균형 잡힌 팀 개수
+    best_sum = -1   # 최대 팀 레이팅
+    l = 0
 
-    for i in range(len(lst)-2) :
-        for j in range(i+1, len(lst)-1) :
-            for k in range(j+1, len(lst)) :
-                team = [lst[i], lst[j], lst[k]]
-                if max(team) - min(team) <= M :
-                    res += 1
-                    if rating < sum(team) :
-                        rating = sum(team)
-    if res == 0 :
+    for r in range(N) :
+        # lst[r]를 최대값으로 가지는 구간을 만들기 위해 l 조정
+        while l < r and lst[r] - lst[l] > M :
+            l += 1
+
+        # 현재 r을 최대값으로 쓸 수 있는 사람 수
+        length = r - l  # [l ... r-1]에 있는 인원 수
+
+        # 이 중에서 2명을 고르면, (r포함 3명 팀)
+        if length >= 2 :
+            # 새로 생기는 팀 개수: 조합 C(length, 2)
+            cnt += length * (length - 1) // 2
+
+            # 최대 팀 레이팅 갱신 (이 구간에서 가장 큰 세 명)
+            candidate = lst[r] + lst[r-1] + lst[r-2]
+            if candidate > best_sum :
+                best_sum = candidate
+
+    if cnt == 0 :
         print(-1)
     else :
-        print(f"{res} {rating}")
-
+        print(cnt, best_sum)
 
 if __name__ == "__main__" :
     main()
