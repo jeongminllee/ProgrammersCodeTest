def27a5206bd7aea979640537aedd858f433dbe2
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-12-02 15:39:14 +0900

    [Silver V] Title: 잃어버린 섬, Time: 104 ms, Memory: 110896 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Silver/34146. 잃어버린 섬/잃어버린 섬.py
@@ -0,0 +1,32 @@
+def main() :
+    N, M = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    dct = {}
+
+    for i in range(N) :
+        for j in range(M) :
+            if arr[i][j] not in dct :
+                dct[arr[i][j]] = 0
+            dct[arr[i][j]] += 1
+
+    if M % 2 == 0 :
+        for val in dct.values() :
+            if val % 2 :
+                print("NO")
+                return
+
+    else :
+        cnt = 0
+        for val in dct.values() :
+            if val % 2 == 0 :
+                continue
+            cnt += 1
+        if cnt == len(dct.keys()) :
+            print("NO")
+            return
+        if cnt % 2 != N % 2 :
+            print("NO")
+            return
+    print("YES")
+if __name__ == "__main__" :
+    main()
