34fc54a9c7a61a3e9722d35dd04ba447417e9b38
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-12-03 19:33:16 +0900

    [Bronze V] Title: 공통교육과정, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/34813. 공통교육과정/공통교육과정.py
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.py
@@ -1,13 +1,4 @@
 def main() :
-    S = input()
-    if S[0] == 'F' :
-        print("Foundation")
-    elif S[0] == 'C' :
-        print("Claves")
-    elif S[0] == 'V' :
-        print("Veritas")
-    else :
-        print("Exploration")
-        
+    print([0,'Claves','Veritas','Exploration','Foundation'][ord(input()[0])%6])
 if __name__ == "__main__" :
     main()
