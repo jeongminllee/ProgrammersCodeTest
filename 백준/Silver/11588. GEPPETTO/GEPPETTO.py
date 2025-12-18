4f032b18201115f9a10ccb7db3b246faf1e18bd7
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-12-18 16:30:02 +0900

    [Silver II] Title: GEPPETTO, Time: 116 ms, Memory: 110720 KB -BaekjoonHub

--- 백준/Silver/11588. GEPPETTO/GEPPETTO.py
+++ 백준/Silver/11588. GEPPETTO/GEPPETTO.py
@@ -1,31 +1,13 @@
-
 N, M = map(int, input().split())
-forbidden = set()
-
-for _ in range(M) :
+forbidden = [0] * M
+for m in range(M) :
     a, b = map(int, input().split())
-    forbidden.add((min(a-1, b-1), max(a-1, b-1)))
+    forbidden[m] = (1<<(a-1) | 1<<(b-1))
 
-cnt = 0
-
-for mask in range(1<<N) :
-    valid = True
-
-    selected = []
-    for i in range(N) :
-        if mask & (1 << i) :
-            selected.append(i)
-
-    for i in range(len(selected)) :
-        for j in range(i + 1, len(selected)) :
-            pair = (min(selected[i], selected[j]), max(selected[i], selected[j]))
-            if pair in forbidden :
-                valid = False
-                break
-        if not valid :
+cnt = 2 ** N
+for n in range(1<<N) :
+    for banned in forbidden :
+        if n & banned == banned :
+            cnt -= 1
             break
-
-    if valid :
-        cnt += 1
-
 print(cnt)
