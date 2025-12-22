45777d6e4339dfa065a14974aa5a54386e7ede19
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-12-22 19:56:22 +0900

    [Platinum IV] Title: 경품 추첨, Time: 100 ms, Memory: 110720 KB -BaekjoonHub

---
+++ 백준/Platinum/22357. 경품 추첨/경품 추첨.py
@@ -0,0 +1,16 @@
+primes = [
+    2003, 2011, 2017, 2027, 2029, 2039, 2053, 2063, 2069, 2081,
+    2083, 2087, 2089, 2099, 2111, 2113, 2129, 2131, 2137, 2141,
+    2143, 2153, 2161, 2179, 2203, 2207, 2213, 2221, 2237, 2239
+]
+K, N = map(int, input().split())
+
+for i in range(K) :
+    p = primes[i]
+    box_numbers = []
+
+    for j in range(N) :
+        number = 1 + j * p
+        box_numbers.append(number)
+
+    print(" ".join(map(str, box_numbers)))
