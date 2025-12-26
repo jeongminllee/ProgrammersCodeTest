4ffb033a107061d2ebdfda1489dc1581f987066b
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2025-12-26 21:17:26 +0900

    [Silver IV] Title: Equal Schedules, Time: 132 ms, Memory: 111824 KB -BaekjoonHub

--- 백준/Silver/30949. Equal Schedules/Equal Schedules.py
+++ 백준/Silver/30949. Equal Schedules/Equal Schedules.py
@@ -1,58 +1,24 @@
 from collections import defaultdict
-schedules = [[] for _ in range(2)]
-d = 0
+
+schedules = defaultdict(int)
 while True :
     try :
-        schedule = input()
-        if schedule == "======" :
-            break
-        if schedule == "------" :
-            d = 1
-            continue
-
-        schedules[d].append(schedule)
-
+        x, y, z = input().split()
     except :
         break
-
-first = defaultdict(int)
-second = defaultdict(int)
-for i in range(2) :
-    for sche in schedules[i] :
-        s, e, name = sche.split()
-        if i == 0 :
-            first[name] += int(e) - int(s)
-        else :
-            second[name] += int(e) - int(s)
-
-res = defaultdict(int)
-diff_val = 0
-
-for idx, val in first.items() :
-    if idx not in second :
-        res[idx] = -val
-        diff_val += abs(val)
-
-    else :
-        res[idx] = second[idx] - val
-        diff_val += abs(second[idx] - val)
-
-for idx, val in second.items() :
-    if idx not in first :
-        res[idx] = val
-        diff_val += abs(val)
-
-ans = list(res.items())
-ans.sort()
-
-for a in ans :
-    if diff_val == 0 :
-        print("No differences found.")
+    schedules[z] += -(int(y) - int(x))
+while True:
+    try:
+        x, y, z = input().split()
+    except:
         break
+    schedules[z] += (int(y) - int(x))
 
-    if a[1] == 0 :
-        continue
-    elif a[1] > 0 :
-        print(f"{a[0]} +{a[1]}")
-    else :
-        print(f"{a[0]} {a[1]}")
+res = [name for name in sorted(schedules) if schedules[name]]
+
+if res :
+    for name in res :
+        print(name, schedules[name]\
+            if schedules[name] < 0 else '+' + str(schedules[name]))
+else :
+    print("No differences found.")
